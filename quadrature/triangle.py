fe8bf6c8016fa7fe88f385fb0a0f443e3966af85
Nico Schlömer <nico.schloemer@gmail.com>
2016-10-03 16:48:09 +0200

    add centroid rule, test framework

---
+++ quadrature/triangle.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+#
+import numpy
+
+
+def volume(triangle):
+    # It doesn't matter much which cross product we take for computing the
+    # triangle volumes; deliberately take
+    #
+    #   <e0 x e1, e0 x e1> = <e0, e0> <e1, e1> - <e0, e1>^2.
+    #
+    e0 = triangle[1] - triangle[0]
+    e1 = triangle[2] - triangle[1]
+    e0_dot_e0 = numpy.dot(e0, e0)
+    e0_dot_e1 = numpy.dot(e0, e1)
+    e1_dot_e1 = numpy.dot(e1, e1)
+    return 0.5 * numpy.sqrt(e0_dot_e0 * e1_dot_e1 - e0_dot_e1**2)
+
+
+def _transform_to_unit_triangle(f, triangle):
+    '''Transformation
+
+      x = x0 * N0(xi, eta) + x1 * N1(xi, eta) + x2 * N2(xi, eta)
+
+    with
+
+      N0(xi, eta) = 1 - xi - eta,
+      N1(xi, eta) = xi,
+      N2(xi, eta) = eta.
+    '''
+    return lambda xi: f(
+        + triangle[0] * (1.0 - xi[0] - xi[1])
+        + triangle[1] * xi[0]
+        + triangle[2] * xi[1]
+        )
+
+
+def _integrate(f, triangle, rule):
+    g = _transform_to_unit_triangle(f, triangle)
+    out = 0.0
+    for point, weight in zip(rule.points, rule.weights):
+        out += weight * g(point)
+    return volume(triangle) * out
+
+
+class Centroid(object):
+    def __init__(self):
+        self.weights = [1.0]
+        self.points = numpy.array([
+            [1.0/3.0, 1.0/3.0]
+            ])
+        self.order = 1
+        return
+
+
+def centroid(f, triangle):
+    return _integrate(f, triangle, Centroid())
---
+++ test/test.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+#
+import numpy
+import numpy.testing
+import quadrature
+import sympy
+
+
+def _integrate_exact(f, triangle):
+    #
+    # Note that
+    #
+    #     \int_T f(x) dx = \int_T0 |J(xi)| f(P(xi)) dxi
+    #
+    # with
+    #
+    #     P(xi) = x0 * (1-xi[0]-xi[1]) + x1 * xi[0] + x2 * xi[1].
+    #
+    # and T0 being the reference triangle [(0.0, 0.0), (1.0, 0.0), (0.0,
+    # 1.0)].
+    # The determinant of the transformation matrix J equals twice the volume of
+    # the triangle. (See, e.g.,
+    # <http://math2.uncc.edu/~shaodeng/TEACHING/math5172/Lectures/Lect_15.PDF>).
+    #
+    def g(xi):
+        pxi = triangle[0] * (1 - xi[0] - xi[1]) \
+            + triangle[1] * xi[0] \
+            + triangle[2] * xi[1]
+        return f(pxi)
+
+    x = sympy.DeferredVector('x')
+    exact = 2 * quadrature.triangle.volume(triangle) \
+        * sympy.integrate(
+            sympy.integrate(g(x), (x[1], 0, 1-x[0])),
+            (x[0], 0, 1)
+            )
+    return float(exact)
+
+
+def _create_test_polynomial(order):
+    '''The k-th order terms in polynomial have the form
+
+        alpha_{k,i} * x^{k-i} * y^k,
+
+    with i in {0,...,k}. Take
+
+      alpha_{k, i} = (i+1) / (k+2)
+
+    such that
+
+      p0(x) = 1/2,
+
+      p1(x) = 1/2 \
+            + 1/3 * x + 2/3 * y,
+
+      p1(x) = 1/2 \
+            + 1/3 * x + 2/3 * y \
+            + 1/4 * x^2 + 1/2 * x*y + 3/4 * y^2
+
+    etc.
+    '''
+    def f(x):
+        out = 0.0
+        for k in range(order+1):
+            i = 0
+            for i in range(k+1):
+                # This relies on Python's 0.0**0=1.
+                alpha = (i+1) / float(k+2) * x[0]**(k-i) * x[1]**i
+                out += alpha
+        return out
+
+    return f
+
+
+def test_centroid():
+
+    f = _create_test_polynomial(order=1)
+
+    triangle = numpy.array([
+        [0.0, 0.0],
+        [1.0, 0.0],
+        [0.6, 0.5]
+        ])
+    exact_val = _integrate_exact(f, triangle)
+
+    val = quadrature.triangle.centroid(f, triangle)
+
+    numpy.testing.assert_allclose(val, exact_val)
+    return
+
+
+if __name__ == '__main__':
+    test_centroid()
