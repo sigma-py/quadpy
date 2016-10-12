2ec6a531605323a2f115fba3722293cab5eb7f1b
Nico Schlömer <nico.schloemer@gmail.com>
2016-10-12 19:51:44 +0200

    add forth wandzura scheme

--- test/test_triangle.py
+++ test/test_triangle.py
@@ -72,6 +72,7 @@ def _create_monomial_exponents(degree):
     quadrature.triangle.WandzuraXiao(1),
     quadrature.triangle.WandzuraXiao(2),
     quadrature.triangle.WandzuraXiao(3),
+    quadrature.triangle.WandzuraXiao(4),
     quadrature.triangle.Centroid(),
     quadrature.triangle.Vertex(),
     quadrature.triangle.SevenPoint(),
