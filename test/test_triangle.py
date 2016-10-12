cba605fbedb78450be70aefc6e73ec5b6a67ecc1
Nico Schlömer <nico.schloemer@gmail.com>
2016-10-12 14:21:37 +0200

    much faster triangle tests

--- test/test_triangle.py
+++ test/test_triangle.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 #
+import math
 import numpy
 import numpy.testing
 import pytest
@@ -41,6 +42,24 @@ def _integrate_exact(f, triangle):
         (xi[0], 0, 1)
         )
     return float(exact)
+
+
+def _integrate_monomial_over_standard_triangle(k):
+    '''The integral of monomials over the standard triangle is given by
+
+    \int_T x_0^k0 * x1^k1 = (k0!*k1!) / (2+k0+k1)!,
+
+    see, e.g.,
+    A set of symmetric quadrature rules on triangles and tetrahedra,
+    Linbo Zhang, Tao Cui and Hui Liu,
+    Journal of Computational Mathematics,
+    Vol. 27, No. 1 (January 2009), pp. 89-96
+    '''
+    # exp-log to account for large values in numerator and denominator
+    return math.exp(
+        math.fsum([math.lgamma(kk+1) for kk in k])
+        - math.lgamma(3 + sum(k))
+        )
 
 
 def _create_monomial_exponents(degree):
@@ -96,7 +115,7 @@ def test_scheme(scheme):
     triangle = numpy.array([
         [0.0, 0.0],
         [1.0, 0.0],
-        [0.6, 0.5]
+        [0.0, 1.0]
         ])
     success = True
     degree = 0
@@ -105,7 +124,8 @@ def test_scheme(scheme):
         for k in _create_monomial_exponents(degree):
             def poly(x):
                 return x[0]**k[0] * x[1]**k[1]
-            exact_val = _integrate_exact(poly, triangle)
+            # exact_val = _integrate_exact(poly, triangle)
+            exact_val = _integrate_monomial_over_standard_triangle(k)
             val = quadrature.triangle.integrate(
                     poly, triangle, scheme
                     )
@@ -140,5 +160,7 @@ def test_show():
 
 
 if __name__ == '__main__':
-    test_show()
-    plt.show()
+    # test_show()
+    # plt.show()
+    scheme = quadrature.triangle.Dunavant(10)
+    test_scheme(scheme)
