876794a33bfb0a9ea0b4cf023c0e06a2d543413e
Nico Schlömer <nico.schloemer@gmail.com>
2016-10-15 00:12:35 +0200

    proper dtype for exponents array

--- test/helpers.py
+++ test/helpers.py
@@ -6,8 +6,10 @@ import numpy
 def create_monomial_exponents3(degree):
     '''Returns a list of all monomial exponents of degree :degree:.
     '''
-    return [
-        numpy.array([degree-i-j, i, j])
+    return numpy.array([
+        [degree-i-j, i, j]
         for i in range(degree+1)
         for j in range(degree-i+1)
-        ]
+        ],
+        dtype=int
+        )
