924968b1b7e33daff55ca867330b3202eb2f7538
Nico Schlömer <nico.schloemer@gmail.com>
2016-10-14 17:45:00 +0200

    add newton-cotes for triangle

--- quadrature/triangle.py
+++ quadrature/triangle.py
@@ -2,6 +2,7 @@
 #
 import math
 import numpy
+import sympy
 
 
 def volume(triangle):
@@ -1767,4 +1768,68 @@ class LynessJespersen(object):
             raise ValueError('Illegal Lyness-Jespersen index')
 
         self.points = bary[:, [1, 2]]
+        return
+
+
+def _newton_cotes(n, point_fun):
+    '''
+    Construction after
+
+    P. Silvester,
+    Symmetric quadrature formulae for simplexes
+    Math. Comp., 24, 95-100 (1970),
+    <http://www.ams.org/journals/mcom/1970-24-109/S0025-5718-1970-0258283-6/S0025-5718-1970-0258283-6.pdf>
+    '''
+    def get_poly(t, m, n):
+        f = 1
+        for k in range(m):
+            f *= (t - point_fun(k, n)) / (point_fun(m, n) - point_fun(k, n))
+        return f
+    degree = n
+    num_points = (n+1) * (n+2) / 2
+    bary = numpy.empty((num_points, 3))
+    weights = numpy.empty(num_points)
+    idx = 0
+    for i in range(n + 1):
+        for j in range(n + 1 - i):
+            k = n - i - j
+            bary[idx] = point_fun(
+                numpy.array([i, j, k], dtype=float), n
+                )
+            # Compute weight.
+            # Define the polynomial which to integrate over the
+            # tetrahedron.
+            t = sympy.DeferredVector('t')
+            g = sympy.expand(
+                get_poly(t[0], i, n)
+                * get_poly(t[1], j, n)
+                * get_poly(t[2], k, n)
+                )
+            # tranform it into a polynomial class
+            gpoly = sympy.poly_from_expr(
+                g, (t[0], t[1], t[2])
+                )[0]
+            # The integral of monomials over a tetrahedron are well-known,
+            # see Silvester.
+            weights[idx] = numpy.sum([
+                 c * numpy.prod([math.factorial(l) for l in m]) * 2
+                 / math.factorial(numpy.sum(m) + 2)
+                 for m, c in zip(gpoly.monoms(), gpoly.coeffs())
+                 ])
+            idx += 1
+    points = bary[:, [1, 2]]
+    return points, weights, degree
+
+
+class NewtonCotesClosed(object):
+    def __init__(self, n):
+        self.points, self.weights, self.degree = \
+            _newton_cotes(n, lambda k, n: k / float(n))
+        return
+
+
+class NewtonCotesOpen(object):
+    def __init__(self, n):
+        self.points, self.weights, self.degree = \
+            _newton_cotes(n, lambda k, n: (k+1) / float(n+3))
         return
