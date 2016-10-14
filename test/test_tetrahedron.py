c39d1aa82420f86b44227729c7c99a34ab098cfc
Nico Schlömer <nico.schloemer@gmail.com>
2016-10-14 16:28:43 +0200

    tet newton-cotes: compute integral explicitly

--- test/test_tetrahedron.py
+++ test/test_tetrahedron.py
@@ -77,12 +77,6 @@ def _create_monomial_exponents(degree):
 
 
 @pytest.mark.parametrize('scheme', [
-    quadrature.tetrahedron.NewtonCotesClosed(1),
-    quadrature.tetrahedron.NewtonCotesClosed(2),
-    quadrature.tetrahedron.NewtonCotesClosed(3),
-    quadrature.tetrahedron.NewtonCotesClosed(4),
-    # quadrature.tetrahedron.NewtonCotesClosed(5),
-    # quadrature.tetrahedron.NewtonCotesClosed(6),
     quadrature.tetrahedron.Keast(0),
     quadrature.tetrahedron.Keast(1),
     quadrature.tetrahedron.Keast(2),
@@ -93,6 +87,12 @@ def _create_monomial_exponents(degree):
     quadrature.tetrahedron.Keast(7),
     quadrature.tetrahedron.Keast(8),
     quadrature.tetrahedron.Keast(9),
+    quadrature.tetrahedron.NewtonCotesClosed(1),
+    quadrature.tetrahedron.NewtonCotesClosed(2),
+    quadrature.tetrahedron.NewtonCotesClosed(3),
+    quadrature.tetrahedron.NewtonCotesClosed(4),
+    quadrature.tetrahedron.NewtonCotesClosed(5),
+    quadrature.tetrahedron.NewtonCotesClosed(6),
     quadrature.tetrahedron.NewtonCotesOpen(0),
     quadrature.tetrahedron.NewtonCotesOpen(1),
     quadrature.tetrahedron.NewtonCotesOpen(2),
