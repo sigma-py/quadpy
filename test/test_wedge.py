61a2bd9d571e176e2f1dfa7a9a1c44645301bef2
Nico Schlömer <nico.schloemer@gmail.com>
2016-10-15 00:12:35 +0200

    proper dtype for exponents array

--- test/test_wedge.py
+++ test/test_wedge.py
@@ -79,7 +79,7 @@ def test_scheme(scheme):
     while success:
         for k in create_monomial_exponents3(degree):
             def poly(x):
-                return x[0]**k[0] + x[1]**k[1] + x[2]**k[2]
+                return x[0]**int(k[0]) + x[1]**int(k[1]) + x[2]**int(k[2])
             exact_val = _integrate_exact(poly, wedge)
             val = quadrature.wedge.integrate(
                     poly, wedge, scheme
@@ -115,5 +115,5 @@ def test_show():
 if __name__ == '__main__':
     # test_show()
     # plt.show()
-    scheme = quadrature.wedge.Felippa(1)
+    scheme = quadrature.wedge.Felippa(2)
     test_scheme(scheme)
