d3c9b7a6917690e9b5f9004dc53d4a6ddd4e1fcd
Nico Schlömer <nico.schloemer@gmail.com>
2016-10-15 22:48:36 +0200

    add some integration rules for the pyramid

---
+++ quadrature/pyramid.py
@@ -0,0 +1,138 @@
+# -*- coding: utf-8 -*-
+#
+import math
+import numpy
+
+
+def integrate(f, pyra, scheme):
+    xi = scheme.points.T
+    x = \
+        + numpy.outer(pyra[0], 0.125*(1.0-xi[0])*(1.0-xi[1])*(1-xi[2])) \
+        + numpy.outer(pyra[1], 0.125*(1.0+xi[0])*(1.0-xi[1])*(1-xi[2])) \
+        + numpy.outer(pyra[2], 0.125*(1.0+xi[0])*(1.0+xi[1])*(1-xi[2])) \
+        + numpy.outer(pyra[3], 0.125*(1.0-xi[0])*(1.0+xi[1])*(1-xi[2])) \
+        + numpy.outer(pyra[4], 0.50*(1.0+xi[2]))
+    J0 = \
+        - numpy.outer(pyra[0], 0.125*(1.0-xi[1])*(1-xi[2])) \
+        + numpy.outer(pyra[1], 0.125*(1.0-xi[1])*(1-xi[2])) \
+        + numpy.outer(pyra[2], 0.125*(1.0+xi[1])*(1-xi[2])) \
+        - numpy.outer(pyra[3], 0.125*(1.0+xi[1])*(1-xi[2]))
+    J1 = \
+        - numpy.outer(pyra[0], 0.125*(1.0-xi[0])*(1-xi[2])) \
+        - numpy.outer(pyra[1], 0.125*(1.0+xi[0])*(1-xi[2])) \
+        + numpy.outer(pyra[2], 0.125*(1.0+xi[0])*(1-xi[2])) \
+        + numpy.outer(pyra[3], 0.125*(1.0-xi[0])*(1-xi[2]))
+    J2 = \
+        - numpy.outer(pyra[0], 0.125*(1.0-xi[0])*(1.0-xi[1])) \
+        - numpy.outer(pyra[1], 0.125*(1.0+xi[0])*(1.0-xi[1])) \
+        - numpy.outer(pyra[2], 0.125*(1.0+xi[0])*(1.0+xi[1])) \
+        - numpy.outer(pyra[3], 0.125*(1.0-xi[0])*(1.0+xi[1])) \
+        + numpy.outer(pyra[4], 0.50*numpy.ones(1))
+    det = J0[0]*J1[1]*J2[2] + J1[0]*J2[1]*J0[2] + J2[0]*J0[1]*J1[2] \
+        - J0[2]*J1[1]*J2[0] - J1[2]*J2[1]*J0[0] - J2[2]*J0[1]*J1[0]
+
+    return math.fsum(scheme.weights * f(x).T * abs(det))
+
+
+class Felippa(object):
+    '''
+    Carlos Felippa,
+    A compendium of FEM integration formulas for symbolic work,
+    Engineering Computation,
+    Volume 21, Number 8, 2004, pages 867-890.
+
+    Abstract:
+    This paper presents a set of Mathematica modules that organizes numerical
+    integration rules considered useful for finite element work. Seven regions
+    are considered: line segments, triangles, quadrilaterals, tetrahedral,
+    wedges, pyramids and hexahedra. Information can be returned in
+    floating-point (numerical) form, or in exact symbolic form. The latter is
+    useful for computer-algebra aided FEM work that carries along symbolic
+    variables. A few quadrature rules were extracted from sources in the FEM
+    and computational mathematics literature, and placed in symbolic form using
+    Mathematica to generate own code. A larger class of formulas, previously
+    known only numerically, were directly obtained through symbolic
+    computations. Some unpublished non-product rules for pyramid regions were
+    found and included in the collection. For certain regions: quadrilaterals,
+    wedges and hexahedra, only product rules were included to economize
+    programming. The collection embodies most FEM-useful formulas of low and
+    moderate order for the seven regions noted above. Some gaps as regard
+    region geometries and omission of non-product rules are noted in the
+    conclusions. The collection may be used “as is” in support of symbolic FEM
+    work thus avoiding contamination with floating arithmetic that precludes
+    simplification. It can also be used as generator for low-level
+    floating-point code modules in Fortran or C. Floating point accuracy can be
+    selected arbitrarily. No similar modular collection applicable to a range
+    of FEM work, whether symbolic or numeric, has been published before.
+    '''
+    def __init__(self, index):
+        if index == 1:
+            self.weights = numpy.array([128.0/27.0])
+            self.points = numpy.array([
+                [0.0, 0.0, -0.5],
+                ])
+            self.degree = 1
+        elif index == 2:
+            self.weights = numpy.concatenate([
+                0.81 * numpy.ones(4),
+                125.0/27.0 * numpy.ones(1)
+                ])
+            self.points = numpy.concatenate([
+                self._s4(8 * numpy.sqrt(2.0/15.0) / 5, -2.0/3.0),
+                numpy.array([[0.0, 0.0, 0.4]]),
+                ])
+            self.degree = 2
+        elif index == 3:
+            self.weights = numpy.concatenate([
+                504.0/625.0 * numpy.ones(4),
+                576.0/625.0 * numpy.ones(1),
+                64.0/15.0 * numpy.ones(1),
+                ])
+            self.points = numpy.concatenate([
+                self._s4(numpy.sqrt(12.0/35.0), -2.0/3.0),
+                numpy.array([[0.0, 0.0, 1.0/6.0]]),
+                numpy.array([[0.0, 0.0, 0.5]]),
+                ])
+            self.degree = 2
+        elif index == 4:
+            w1 = 5 * (68.0 + 5*numpy.sqrt(10.0)) / 432.0
+            w2 = 85.0/54.0 - w1
+            self.weights = numpy.concatenate([
+                w1 * numpy.ones(4),
+                w2 * numpy.ones(4),
+                ])
+            g1 = numpy.sqrt(1.0/3.0)
+            g2 = (2*numpy.sqrt(10)-5) / 15.0
+            self.points = numpy.concatenate([
+                self._s4(g1, g2),
+                self._s4(g1, -2.0/3.0 - g2),
+                ])
+            self.degree = 3
+        elif index == 5:
+            w1 = (11764.0 - 461.0*numpy.sqrt(51.0)) / 15300.0
+            w2 = 346.0 / 225.0 - w1
+            self.weights = numpy.concatenate([
+                w1 * numpy.ones(4),
+                w2 * numpy.ones(4),
+                ])
+            g1 = numpy.sqrt(2.0/15.0 * (573 - 2*numpy.sqrt(51))) / 15.0
+            g2 = numpy.sqrt(2.0/15.0 * (573 + 2*numpy.sqrt(51))) / 15.0
+            g3 = -(2*numpy.sqrt(51.0) + 13) / 35.0
+            g4 = +(2*numpy.sqrt(51.0) - 13) / 35.0
+            self.points = numpy.concatenate([
+                self._s4(g1, g3),
+                self._s4(g2, g4),
+                ])
+            self.degree = 2
+        else:
+            raise ValueError('Illegal Felippa order')
+
+        return
+
+    def _s4(self, a, z):
+        return numpy.array([
+            [+a, +a, z],
+            [-a, +a, z],
+            [+a, -a, z],
+            [-a, -a, z],
+            ])
---
+++ test/test_pyramid.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+#
+from helpers import create_monomial_exponents3
+import numpy
+import numpy.testing
+import quadrature
+import pytest
+import sympy
+
+
+def _integrate_exact(f, pyra):
+    # map the reference hexahedron [-1,1]^3 to the pyramid
+    xi = sympy.DeferredVector('xi')
+    pxi = \
+        + pyra[0] * (1 - xi[0])*(1 - xi[1])*(1 - xi[2]) / 8 \
+        + pyra[1] * (1 + xi[0])*(1 - xi[1])*(1 - xi[2]) / 8 \
+        + pyra[2] * (1 + xi[0])*(1 + xi[1])*(1 - xi[2]) / 8 \
+        + pyra[3] * (1 - xi[0])*(1 + xi[1])*(1 - xi[2]) / 8 \
+        + pyra[4] * (1 + xi[2]) / 2
+
+    pxi = [
+        sympy.expand(pxi[0]),
+        sympy.expand(pxi[1]),
+        sympy.expand(pxi[2]),
+        ]
+    # determinant of the transformation matrix
+    J = sympy.Matrix([
+        [sympy.diff(pxi[0], xi[0]),
+         sympy.diff(pxi[0], xi[1]),
+         sympy.diff(pxi[0], xi[2])],
+        [sympy.diff(pxi[1], xi[0]),
+         sympy.diff(pxi[1], xi[1]),
+         sympy.diff(pxi[1], xi[2])],
+        [sympy.diff(pxi[2], xi[0]),
+         sympy.diff(pxi[2], xi[1]),
+         sympy.diff(pxi[2], xi[2])],
+        ])
+    det_J = sympy.det(J)
+    # we cannot use abs(), see <https://github.com/sympy/sympy/issues/4212>.
+    # abs_det_J = sympy.Piecewise((det_J, det_J >= 0), (-det_J, det_J < 0))
+    # This is quite the leap of faith, but sympy will cowardly bail out
+    # otherwise.
+    abs_det_J = det_J
+
+    exact = sympy.integrate(
+        sympy.integrate(
+            sympy.integrate(abs_det_J * f(pxi), (xi[2], -1, 1)),
+            (xi[1], -1, +1)
+            ),
+        (xi[0], -1, +1)
+        )
+
+    return float(exact)
+
+
+@pytest.mark.parametrize('scheme', [
+    quadrature.pyramid.Felippa(1),
+    quadrature.pyramid.Felippa(2),
+    quadrature.pyramid.Felippa(3),
+    quadrature.pyramid.Felippa(4),
+    quadrature.pyramid.Felippa(5),
+    ])
+def test_scheme(scheme):
+    # Test integration until we get to a polynomial degree `d` that can no
+    # longer be integrated exactly. The scheme's degree is `d-1`.
+    pyra = numpy.array([
+        [-1, -1, -1],
+        [+1, -1, -1],
+        [+1, +1, -1],
+        [-1, +1, -1],
+        [0, 0, 1],
+        ])
+    success = True
+    degree = 0
+    max_degree = scheme.degree + 1
+    while success:
+        for k in create_monomial_exponents3(degree):
+            def poly(x):
+                return x[0]**k[0] * x[1]**k[1] * x[2]**k[2]
+            exact_val = _integrate_exact(poly, pyra)
+            val = quadrature.pyramid.integrate(
+                    poly, pyra, scheme
+                    )
+            if abs(exact_val - val) > 1.0e-10:
+                success = False
+                break
+        if not success:
+            break
+        if degree >= max_degree:
+            break
+        degree += 1
+    numpy.testing.assert_equal(degree-1, scheme.degree)
+    return
