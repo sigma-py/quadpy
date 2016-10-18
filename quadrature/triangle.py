7a1f46d55fe0f84555a66e4299efbaa38795c36d
Nico Schlömer <nico.schloemer@gmail.com>
2016-10-18 10:27:43 +0200

    complete Liu-Vinokur schemes for triangle

--- quadrature/triangle.py
+++ quadrature/triangle.py
@@ -2292,8 +2292,6 @@ class LiuVinokur(object):
             self.degree = 3
         elif index == 6:
             self.weights = numpy.concatenate([
-                # Wrongly specified in the article as $/10$ (instead of
-                # $/120$).
                 (1.0 + numpy.sqrt(21.0)) / 120.0 * numpy.ones(3),
                 (39.0 - numpy.sqrt(21.0)) / 120.0 * numpy.ones(3),
                 ])
@@ -2341,6 +2339,59 @@ class LiuVinokur(object):
                 self._r_alpha(2.0/3.0),
                 ])
             self.degree = 4
+        elif index == 10:
+            self.weights = numpy.concatenate([
+                (11.0 - numpy.sqrt(13)) / 360.0 * numpy.ones(3),
+                (80.0 - 16*numpy.sqrt(13)) / 360.0 * numpy.ones(3),
+                (29.0 + 17*numpy.sqrt(13)) / 360.0 * numpy.ones(3),
+                ])
+            bary = numpy.concatenate([
+                self._r_alpha(1.0),
+                self._r_alpha(-0.5),
+                self._r_alpha((-1.0 + numpy.sqrt(13.0)) / 6.0),
+                ])
+            self.degree = 4
+        elif index == 11:
+            self.weights = numpy.concatenate([
+                0.45 * numpy.ones(1),
+                -1.0 / 60.0 * numpy.ones(3),
+                0.1 * numpy.ones(6),
+                ])
+            bary = numpy.concatenate([
+                _s3(),
+                self._r_alpha(1.0),
+                self._r_gamma_delta(
+                    (3.0 + numpy.sqrt(3.0)) / 6.0,
+                    (3.0 - numpy.sqrt(3.0)) / 6.0
+                    ),
+                ])
+            self.degree = 4
+        elif index == 12:
+            self.weights = numpy.concatenate([
+                9.0/40.0 * numpy.ones(1),
+                (155.0 - numpy.sqrt(15.0))/1200.0 * numpy.ones(3),
+                (155.0 + numpy.sqrt(15.0))/1200.0 * numpy.ones(3),
+                ])
+            bary = numpy.concatenate([
+                _s3(),
+                self._r_alpha((1.0 + numpy.sqrt(15.0)) / 7.0),
+                self._r_alpha((1.0 - numpy.sqrt(15.0)) / 7.0),
+                ])
+            self.degree = 5
+        elif index == 13:
+            self.weights = numpy.concatenate([
+                81.0/320.0 * numpy.ones(1),
+                1.0/90.0 * numpy.ones(3),
+                16.0/225.0 * numpy.ones(3),
+                2401.0/14400.0 * numpy.ones(3),
+                ])
+            bary = numpy.concatenate([
+                _s3(),
+                self._r_alpha(1.0),
+                self._r_alpha(-0.5),
+                self._r_alpha(4.0/7.0),
+                ])
+            self.degree = 5
         else:
             raise ValueError('Illegal Liu-Vinokur index')
 
@@ -2361,4 +2412,25 @@ class LiuVinokur(object):
             [a, b, b],
             [b, a, b],
             [b, b, a],
+            ])
+
+    def _r_gamma_delta(self, gamma, delta):
+        '''From the article:
+
+        mu_i = (1 + (n-1) gamma - delta) / n,
+        mu_j = (1 + (n-1) delta - gamma) / n,
+        mu_k = (1 - gamma - delta) / n    for k!=i, k!=j,
+
+        where n is the number of vertices
+        '''
+        a = (1.0 + 2*gamma - delta) / 3.0
+        b = (1.0 + 2*delta - gamma) / 3.0
+        c = (1.0 - gamma - delta ) / 3.0
+        return numpy.array([
+            [a, b, c],
+            [c, a, b],
+            [b, c, a],
+            [a, c, b],
+            [b, a, c],
+            [c, b, a],
             ])
