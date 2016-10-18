376b4babffdfd2e1c5362a3c82b72910e345ef82
Nico Schlömer <nico.schloemer@gmail.com>
2016-10-18 18:33:44 +0200

    add note on why not more Hillion schemes are implemented

--- quadrature/triangle.py
+++ quadrature/triangle.py
@@ -2443,6 +2443,11 @@ class Hillion(object):
     International Journal for Numerical Methods in Engineering,
     Vol. 11, 797-815 (1977).
     DOI:10.1002/nme.1620110504
+
+    Note that the schemes here are not fully symmetric. Also note that in the
+    article, the quadrature constants are specified with low precision such
+    that the tests are failing. What is needed here is a reimplementation of
+    Hillion's method to retrieve more digits.
     '''
     def __init__(self, index):
         if index == 1:
@@ -2493,6 +2498,64 @@ class Hillion(object):
                 2.0/3.0 - self._symm(0.591506351, 0.158493649),
                 ])
             self.degree = 2
+        # elif index == 6:
+        #     self.weights = 2.0 * numpy.concatenate([
+        #         1.0/8.0 * numpy.ones(4),
+        #         ])
+        #     lmbda = 0.655308609
+        #     mu = 0.247060398
+        #     self.points = numpy.concatenate([
+        #         self._symm(lmbda, mu),
+        #         2.0/3.0 - self._symm(lmbda, mu),
+        #         ])
+        #     self.degree = 2
+        # elif index == 7:
+        #     self.weights = 2.0 * numpy.concatenate([
+        #         0.159020691 * numpy.ones(2),
+        #         0.090979309 * numpy.ones(2),
+        #         ])
+        #     self.points = numpy.concatenate([
+        #         self._symm(0.666390246, 0.280019915),
+        #         self._symm(0.178558728, 0.075031109),
+        #         ])
+        #     self.degree = 3
+        # elif index == 8:
+        #     self.weights = 2.0 * numpy.concatenate([
+        #         0.065104166 * numpy.ones(2),
+        #         0.192191138 * numpy.ones(1),
+        #         0.177600528 * numpy.ones(1),
+        #         ])
+        #     lambda2 = 0.433949142
+        #     lambda3 = 0.175574667
+        #     self.points = numpy.concatenate([
+        #         self._symm(0.0, 0.8),
+        #         numpy.array([[lambda2, lambda2]]),
+        #         numpy.array([[lambda3, lambda3]]),
+        #         ])
+        #     self.degree = 3
+        # elif index == 9:
+        #     self.weights = 2.0 * numpy.concatenate([
+        #         9.0/32.0 * numpy.ones(1),
+        #         25.0/96.0 * numpy.ones(3),
+        #         ])
+        #     self.points = numpy.concatenate([
+        #         numpy.array([[1.0/3.0, 1.0/3.0]]),
+        #         self._symm(0.2, 0.6),
+        #         numpy.array([[0.2, 0.2]]),
+        #         ])
+        #     self.degree = 3
+        # elif index == 10:
+        #     self.weights = 2.0 * numpy.concatenate([
+        #         0.036232077 * numpy.ones(2),
+        #         0.083559589 * numpy.ones(2),
+        #         25.0/96.0 * numpy.ones(1),
+        #         ])
+        #     self.points = numpy.concatenate([
+        #         self._symm(0.939332590, 0.0),
+        #         self._symm(0.0, 0.340667409),
+        #         numpy.array([[0.4, 0.4]]),
+        #         ])
+        #     self.degree = 3
         else:
             raise ValueError('Illegal Hillion index')
 
