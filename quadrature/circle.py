925d6cab4da314583dca3123de39c5f678c393d1
Nico Schlömer <nico.schloemer@gmail.com>
2016-10-25 12:59:27 +0200

    added Lether schemes for circle

--- quadrature/circle.py
+++ quadrature/circle.py
@@ -55,3 +55,27 @@ class Peirce(object):
         # Gauss-Legendre weights, scaled according to the circle area.
         self.weights = numpy.tile(0.5 * numpy.pi / (k+1) * w, k+1)
         return
+
+
+class Lether(object):
+    '''
+    Frank G. Lether,
+    A Generalized Product Rule for the Circle,
+    SIAM Journal on Numerical Analysis,
+    Vol. 8, No. 2 (Jun., 1971), pp. 249-253,
+    <http://www.jstor.org/stable/2949473>.
+    '''
+    def __init__(self, n):
+        p, w = numpy.polynomial.legendre.leggauss(n)
+
+        mu = numpy.arange(1, n+1)
+        self.points = numpy.column_stack([
+            numpy.tile(numpy.cos(mu * numpy.pi / (n+1)), n),
+            numpy.outer(p, numpy.sin(mu * numpy.pi / (n+1))).flatten(),
+            ])
+
+        self.weights = numpy.pi/(n+1) \
+            * numpy.outer(w, numpy.sin(mu*numpy.pi/(n+1))**2).flatten()
+
+        self.degree = 2*n - 1
+        return
