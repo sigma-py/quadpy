cb2206d4f1468b1e5cf71bc6c162c64f18ddd592
Nico Schlömer <nico.schloemer@gmail.com>
2016-10-25 14:30:15 +0200

    fewer dependencies between tests

--- test/test_circle.py
+++ test/test_circle.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 #
+from helpers import create_monomial_exponents2
+
 import math
 import numpy
 import numpy.testing
 import pytest
 import quadrature
 import sympy
-
-from test_triangle import _create_monomial_exponents
 
 import os
 import matplotlib as mpl
@@ -89,7 +89,7 @@ def test_scheme(scheme):
     degree = 0
     max_degree = scheme.degree + 1
     while success:
-        for k in _create_monomial_exponents(degree):
+        for k in create_monomial_exponents2(degree):
             def poly(x):
                 return x[0]**k[0] * x[1]**k[1]
             exact_val = _integrate_exact(k)
