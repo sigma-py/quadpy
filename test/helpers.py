f14048bda5df740a3eca2aaa561d3d4f859b7222
Nico Schlömer <nico.schloemer@gmail.com>
2016-10-27 14:02:36 +0200

    share some test code between circle and disk

--- test/helpers.py
+++ test/helpers.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 #
+import math
 import numpy
+import sympy
 
 
 def create_monomial_exponents2(degree):
@@ -19,3 +21,53 @@ def create_monomial_exponents3(degree):
         ],
         dtype=int
         )
+
+
+def integrate_monomial_over_unit_circle(k):
+    '''The integral
+
+    I = \int_0^2pi cos(phi)**k[0] sin(phi)**k[1]
+
+    equals 0 if any of k[0], k[1] is odd. If both are even, then
+
+    I = \int_0^2pi sin**2m * cos**2n
+      = \int_0^2pi (1-cos**2)**m * cos**2n
+      = \sum_k=0^m (m over k) \int cos**2(k+n).
+
+    With
+      \int cos**n = (n-1)/n \int cos**(n-2)
+    one gets
+        \int_0^2pi cos**n
+      = (n-1)!! / n!! * 2*pi
+      = (2n)! / (2**n * n!)**2 * 2*pi,
+    so
+      I = \sum_k=0^m (-1)^i (m over k) (2(k+n))!
+                     / (2**(k+n) * (k+n)!)**2 * 2*pi
+
+    The quotient can be computed via log-gamma, i.e.,
+
+      (2d)! / (2**d * d!)**2
+    = exp(lgamma(2d) - 2d log(2) - 2 lgamma(d)).
+    '''
+    if any(numpy.array(k) % 2 == 1):
+        return 0.0
+
+    m = k[0] // 2
+    n = k[1] // 2
+    return 2*math.pi * sum([
+            (-1)**i
+            * sympy.binomial(m, i)
+            * math.factorial(2*(i+n))
+            / (2**(i+n) * math.factorial(i+n))**2
+            for i in range(m+1)
+            ])
+    # return 2*math.pi * sum([
+    #         (-1)**i
+    #         * sympy.binomial(m, i)
+    #         * math.exp(
+    #             + math.lgamma(2*(i+n))
+    #             - 2*math.lgamma(i+n)
+    #             - 2*(i+n)*math.log(2.0)
+    #             )
+    #         ] for i in range(m+1)
+    #         )
