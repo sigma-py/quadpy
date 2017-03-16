990f53d01501e69311106f5be3af195481938cda
Nico Schlömer <nico.schloemer@gmail.com>
2017-03-16 13:47:10 +0100

    pyramid cleanup

--- quadpy/pyramid.py
+++ quadpy/pyramid.py
@@ -133,28 +133,26 @@ class Felippa(object):
         wg9 = numpy.array([64.0, 40.0, 25.0]) / 81.0
 
         if index == 1:
-            self.weights = numpy.array([128.0/27.0])
-            self.points = numpy.array([
-                [0.0, 0.0, -0.5],
-                ])
+            self.weights = [128.0/27.0]
+            self.points = [[0.0, 0.0, -0.5]]
             self.degree = 1
         elif index == 2:
-            self.weights = numpy.array(
+            self.weights = (
                 [0.81] * 4 +
                 [125.0/27.0]
                 )
-            self.points = numpy.array(
+            self.points = (
                 self._s4(8 * numpy.sqrt(2.0/15.0) / 5, -2.0/3.0) +
                 [[0.0, 0.0, 0.4]]
                 )
             self.degree = 2
         elif index == 3:
-            self.weights = numpy.array(
+            self.weights = (
                 4 * [504.0/625.0] +
                 1 * [576.0/625.0] +
                 1 * [64.0/15.0]
                 )
-            self.points = numpy.array(
+            self.points = (
                 self._s4(numpy.sqrt(12.0/35.0), -2.0/3.0) +
                 [[0.0, 0.0, 1.0/6.0]] +
                 [[0.0, 0.0, 0.5]]
@@ -163,13 +161,13 @@ class Felippa(object):
         elif index == 4:
             w1 = 5 * (68.0 + 5*numpy.sqrt(10.0)) / 432.0
             w2 = 85.0/54.0 - w1
-            self.weights = numpy.array(
-                4 * [w1]  +
+            self.weights = (
+                4 * [w1] +
                 4 * [w2]
                 )
             g1 = numpy.sqrt(1.0/3.0)
             g2 = (2*numpy.sqrt(10)-5) / 15.0
-            self.points = numpy.array(
+            self.points = (
                 self._s4(g1, g2) +
                 self._s4(g1, -2.0/3.0 - g2)
                 )
@@ -177,7 +175,7 @@ class Felippa(object):
         elif index == 5:
             w1 = (11764.0 - 461.0*numpy.sqrt(51.0)) / 15300.0
             w2 = 346.0 / 225.0 - w1
-            self.weights = numpy.array(
+            self.weights = (
                 4 * [w1] +
                 4 * [w2]
                 )
@@ -185,7 +183,7 @@ class Felippa(object):
             g2 = numpy.sqrt(2.0/15.0 * (573 + 2*numpy.sqrt(51))) / 15.0
             g3 = -(2*numpy.sqrt(51.0) + 13) / 35.0
             g4 = +(2*numpy.sqrt(51.0) - 13) / 35.0
-            self.points = numpy.array(
+            self.points = (
                 self._s4(g1, g3) +
                 self._s4(g2, g4)
                 )
@@ -193,7 +191,7 @@ class Felippa(object):
         elif index == 6:
             w1 = 7.0*(11472415.0 - 70057.0*numpy.sqrt(2865.0)) / 130739500.0
             w2 = 84091.0/68450.0 - w1
-            self.weights = numpy.array(
+            self.weights = (
                 4 * [w1] +
                 4 * [w2] +
                 [3.6]
@@ -206,7 +204,7 @@ class Felippa(object):
             g2 = numpy.sqrt(2*(8025 + numpy.sqrt(2865.0)) / 35.0) / 37.0
             g3 = -(+87 + numpy.sqrt(2865.0)) / 168.0
             g4 = +(-87 + numpy.sqrt(2865.0)) / 168.0
-            self.points = numpy.array(
+            self.points = (
                 self._s4(g1, g3) +
                 self._s4(g2, g4) +
                 [[0.0, 0.0, 2.0/3.0]]
@@ -217,7 +215,7 @@ class Felippa(object):
             w2 = 276710106577408.0 / 1075923777052725.0
             w3 = 12827693806929.0 / 30577384040000.0
             w4 = 10663383340655070643544192.0 / 4310170528879365193704375.0
-            self.weights = numpy.array(
+            self.weights = (
                 [w1] * 4 +
                 [w2] * 4 +
                 [w3] * 4 +
@@ -229,7 +227,7 @@ class Felippa(object):
             g3 = numpy.sqrt(37043.0/35.0) / 56.0
             g4 = -127.0/153.0
             g5 = 1490761.0 / 2842826.0
-            self.points = numpy.array(
+            self.points = (
                 self._s4(g1, -1.0/7.0) +
                 self._s4_0(g2, -9.0/28.0) +
                 self._s4(g3, g4) +
@@ -239,7 +237,7 @@ class Felippa(object):
         elif index == 8:
             w1 = 5 * (68.0 + 5.0*numpy.sqrt(10)) / 432.0
             w2 = 85.0/54.0 - w1
-            self.weights = numpy.array(
+            self.weights = (
                 4 * [w1*wg9[2]] +
                 4 * [w1*wg9[1]] +
                 [w1*wg9[0]] +
@@ -251,7 +249,7 @@ class Felippa(object):
             g1 = numpy.sqrt(0.6)
             g2 = 1.0 - 2*(10.0 - numpy.sqrt(10)) / 15.0
             g3 = -2.0/3.0 - g2
-            self.points = numpy.array(
+            self.points = (
                 self._s4(g1, g2) +
                 self._s4_0(g1, g2) +
                 [[0.0, 0.0, g2]] +
@@ -266,7 +264,7 @@ class Felippa(object):
             g3 = -0.854011951853700535688324041975993416
             g4 = -0.305992467923296230556472913192103090
             g5 = +0.410004419776996766244796955168096505
-            self.points = numpy.array(
+            self.points = (
                 self._s4(g1, g3) +
                 self._s4_0(g1, g3) +
                 [[0.0, 0.0, g3]] +
@@ -281,7 +279,7 @@ class Felippa(object):
             w1 = (4.0/15.0)*(4+5*(g4+g5)+10*g4*g5)/((g3-g4)*(g3-g5)*(1-g3)**2)
             w2 = (4.0/15.0)*(4+5*(g3+g5)+10*g3*g5)/((g3-g4)*(g5-g4)*(1-g4)**2)
             w3 = (4.0/15.0)*(4+5*(g3+g4)+10*g3*g4)/((g3-g5)*(g4-g5)*(1-g5)**2)
-            self.weights = numpy.array(
+            self.weights = (
                 4 * [w1*wg9[2]] +
                 4 * [w1*wg9[1]] +
                 [w1*wg9[0]] +
@@ -295,6 +293,8 @@ class Felippa(object):
 
             self.degree = 5
 
+        self.weights = numpy.array(self.weights)
+        self.points = numpy.array(self.points)
         return
 
     def _s4(self, a, z):
