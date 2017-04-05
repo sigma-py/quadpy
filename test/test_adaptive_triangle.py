e604516a1b904617afb8c33f441dd2547ec6135f
Nico Schlömer <nico.schloemer@gmail.com>
2017-04-05 21:49:22 +0200

    more tests for adaptive triangle quadrature

--- test/test_adaptive_triangle.py
+++ test/test_adaptive_triangle.py
@@ -1,17 +1,19 @@
-from numpy import pi, sin
+from numpy import pi, sin, cos
+import pytest
 import quadpy
 
 
-def test_simple():
+@pytest.mark.parametrize('k', range(1, 6))
+def test_simple(k):
     val, _ = quadpy.triangle.adaptive_integrate(
-            lambda x: sin(pi * x[0]) * sin(pi * x[1]),
+            lambda x: sin(k * pi * x[0]) * sin(k * pi * x[1]),
             [[0.0, 0.0], [1.0, 0.0], [0.0, 1.0]],
             1.0e-10
             )
-    exact = 2 / pi**2
+    exact = (2 - pi*k*sin(pi*k) - 2*cos(pi*k)) / (2 * pi**2 * k**2)
 
     assert abs(exact - val) < 1.0e-10
 
 
 if __name__ == '__main__':
-    test_simple()
+    test_simple(1)
