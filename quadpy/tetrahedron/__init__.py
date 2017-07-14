b7168eab821eb60ac6241c8e065bff462bf575ea
Nico Schlömer <nico.schloemer@gmail.com>
2017-07-14 18:24:56 +0200

    add maeztu-sainz scheme

--- quadpy/tetrahedron/__init__.py
+++ quadpy/tetrahedron/__init__.py
@@ -4,6 +4,7 @@
 from .hammer_marlowe_stroud import *
 from .keast import *
 from .liu_vinokur import *
+from .maeztu_sainz import *
 from .newton_cotes import *
 from .shunn_ham import *
 from .xiao_gimbutas import *
