d06700add9776f58e4b6ad892d14ba8dd2e7dec9
Nico Schlömer <nico.schloemer@gmail.com>
2017-07-14 12:15:29 +0200

    more cools-haegemans for the disk

--- quadpy/disk/cools_haegemans.py
+++ quadpy/disk/cools_haegemans.py
@@ -23,8 +23,35 @@ class CoolsHaegemans(object):
                 _s4(0.459700843381),
                 _s40(0.125592606040e+01),
                 ])
+        elif index == 2:
+            self.degree = 9
+            self.weights = numpy.pi * numpy.concatenate([
+                 0.567209601536e-01 * numpy.ones(8),
+                 0.109948866164 * numpy.ones(4),
+                 0.261900192462e-01 * numpy.ones(4),
+                 0.419194282996e-03 * numpy.ones(4),
+                ])
+            self.points = numpy.concatenate([
+                _s8(0.243244191752, 0.809458260086),
+                _s4(0.302217386264),
+                _s4(0.664341348594),
+                _s40(0.134279080737e+01),
+                ])
         else:
-            assert False
+            assert index == 3
+            self.degree = 9
+            self.weights = numpy.pi * numpy.concatenate([
+                 0.123447696401e-01 * numpy.ones(8),
+                 0.932719633554e-01 * numpy.ones(4),
+                 0.589496783783e-01 * numpy.ones(4),
+                 0.730888189861e-01 * numpy.ones(4),
+                ])
+            self.points = numpy.concatenate([
+                _s8(0.343855345294, 0.944778017142),
+                _s4(0.277496500297),
+                _s4(0.592355387396),
+                _s40(0.778610819923),
+                ])
         # TODO There are more schemes in the techincal report
         return
 
