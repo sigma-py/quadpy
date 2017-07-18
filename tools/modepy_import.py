eac4a35badad15b17f3acd97dd8da1f2946928b7
Nico Schlömer <nico.schloemer@gmail.com>
2017-07-18 13:09:39 +0200

    update mode import tool

--- tools/modepy_import.py
+++ tools/modepy_import.py
@@ -2,7 +2,7 @@ import modepy
 import numpy
 
 
-def _get_symmetry_code(pts):
+def _get_symmetry_code_tri(pts):
     if len(pts) == 1:
         return '_s3()'
     elif len(pts) == 3:
@@ -22,34 +22,11 @@ def _get_symmetry_code(pts):
     return '_s111({:.15e}, {:.15e})'.format(pt0[2], pt0[1])
 
 
-def _main():
-    # # reference triangle
-    # tri = numpy.array([
-    #     [+1.0, -1.0],
-    #     [-1.0, +1.0],
-    #     [-1.0, -1.0],
-    #     ])
-
-    # Barycentric coordinates mu_i and Cartesian coordinates x_i are related by
-    # the relation
-    #
-    #    (A, 1) mu = (x, 1),
-    #
-    # where (A, 1) is the column matrix of the triangle corners with a row of
-    # 1s appended.
-    # A = numpy.vstack([tri.T, numpy.ones([1, 3])])
-
+def _import_tri():
     vr = modepy.quadrature.vioreanu_rokhlin
-    # q = vr.VioreanuRokhlinSimplexQuadrature(10, 2)
-    # import matplotlib.pyplot as plt
-    # plt.plot(q.nodes[0], q.nodes[1], 'x')
-    # plt.show()
 
     for k in range(20):
         q = vr.VioreanuRokhlinSimplexQuadrature(k, 2)
-        # x = numpy.vstack([q.nodes, numpy.ones((1, q.nodes.shape[1]))])
-        # bary = numpy.linalg.solve(A, x)
-        #
         # The reference triangle is simply
         #     [+1.0, -1.0],
         #     [-1.0, +1.0],
@@ -83,7 +60,84 @@ def _main():
         k = 0
         for c in counts:
             pts = bary[:, idx[k:k+c]].T
-            print(_get_symmetry_code(pts) + ',')
+            print(_get_symmetry_code_tri(pts) + ',')
+            # for p in pts:
+            #     print('%.15e, %.15e, %.15e' % (p[0], p[1], p[2]))
+            # print('#')
+            k += c
+
+    return
+
+
+def _get_symmetry_code_tet(pts):
+    if len(pts) == 1:
+        return '_s4()'
+    elif len(pts) == 4:
+        # Symmetry group [[a, a, a, b], [a, a, b, a], ...].
+        # Find the equal value `a`.
+        tol = 1.0e-12
+        beta = pts[0] - pts[0][0]
+        ct = numpy.count_nonzero(abs(beta) < tol)
+        if ct == 1:
+            return '_s31({:.15e})'.format(pts[0][1])
+        else:
+            assert ct == 3
+            return '_s31({:.15e})'.format(pts[0][0])
+    elif len(pts) == 6:
+        # Symmetry group [[a, a, b, b], [a, b, b, a], ...].
+        return '_s22({:.15e})'.format(pts[0][0])
+    elif len(pts) == 12:
+        # Symmetry group [[a, a, b, c], [a, c, a, b], ...].
+        # Find the double.
+        pt0 = numpy.sort(pts[0])
+        tol = 1.0e-12
+        for k in range(4):
+            beta = pt0 - pt0[k]
+            ct = numpy.count_nonzero(abs(beta) < tol)
+            if ct == 2:
+                return '_s211({:.15e}, {:.15e})'.format(pt0[k], pt0[k-1])
+
+    assert len(pts) == 24
+    pt0 = numpy.sort(pts[0])
+    return '_s111({:.15e}, {:.15e}, {:.15e})'.format(
+            pt0[-1], pt0[-2], pt0[-3]
+            )
+
+
+def _import_tet():
+    vr = modepy.quadrature.vioreanu_rokhlin
+
+    for k in range(10):
+        q = vr.VioreanuRokhlinSimplexQuadrature(k, 3)
+        # Sort the indices to make it easier to identify symmetry groups.
+        idx = numpy.argsort(q.weights)
+        print
+        print
+        print('k={}'.format(k))
+        print('weights')
+        # identify groups of equal weights and put them out as numpy.full(x, y)
+        tol = 1.0e-12
+        count = 0
+        counts = []
+        last_value = q.weights[idx][0]
+        for w in q.weights[idx]:
+            if abs(last_value - w) < tol:
+                count += 1
+            else:
+                print('numpy.full(%d, %.15e),' % (count, last_value))
+                last_value = w
+                counts.append(count)
+                count = 1
+        counts.append(count)
+        print('numpy.full(%d, %.15e)' % (count, last_value))
+        print
+
+        bary = (q.nodes + 1.0) / 2.0
+        bary = numpy.vstack([bary, 1.0 - numpy.sum(bary, axis=0)])
+        k = 0
+        for c in counts:
+            pts = bary[:, idx[k:k+c]].T
+            print(_get_symmetry_code_tet(pts) + ',')
             # for p in pts:
             #     print('%.15e, %.15e, %.15e' % (p[0], p[1], p[2]))
             # print('#')
@@ -93,4 +147,5 @@ def _main():
 
 
 if __name__ == '__main__':
-    _main()
+    # _import_tri()
+    _import_tet()
