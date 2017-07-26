a265d9cd4c6e4f5cf2d7d042a40dfbfb92ef0751
Nico Schlömer <nico.schloemer@gmail.com>
2017-07-26 12:52:07 +0200

    sphere plotting: fewer points for smaller spheres

--- quadpy/helpers.py
+++ quadpy/helpers.py
@@ -46,17 +46,29 @@ def plot_disks(plt, pts, weights, total_area):
 def plot_spheres(
         plt, ax, pts, weights, total_volume
         ):
-    # http://matplotlib.org/examples/mplot3d/surface3d_demo2.html
-    u = numpy.linspace(0, 2 * numpy.pi, 100)
-    v = numpy.linspace(0, numpy.pi, 100)
-    x = numpy.outer(numpy.cos(u), numpy.sin(v))
-    y = numpy.outer(numpy.sin(u), numpy.sin(v))
-    z = numpy.outer(numpy.ones(numpy.size(u)), numpy.cos(v))
-
-    alpha = 0.3
+    h = 1.0e-2
 
     sum_weights = math.fsum(weights)
     for tp, weight in zip(pts, weights):
+        # Choose radius such that the sum of volumes of the balls equals
+        # total_volume.
+        r = (
+            abs(weight)/sum_weights * total_volume/(4.0/3.0 * numpy.pi)
+            )**(1.0/3.0)
+
+        # http://matplotlib.org/examples/mplot3d/surface3d_demo2.html
+        # Compute sphere for every point anew. This is more costly on the
+        # numerical side, but gives the flexibility of drawing sphere of
+        # different size with different number of points. Another options would
+        # be to precomoute x, y, z before the loop, but this can be heavy on
+        # the graphics output. See
+        # <https://stackoverflow.com/q/45324258/353337>.
+        u = numpy.linspace(0, 2 * numpy.pi, int(2*numpy.pi/h*r) + 1)
+        v = numpy.linspace(0, numpy.pi, int(numpy.pi/h*r) + 1)
+        x = numpy.outer(numpy.cos(u), numpy.sin(v))
+        y = numpy.outer(numpy.sin(u), numpy.sin(v))
+        z = numpy.outer(numpy.ones(numpy.size(u)), numpy.cos(v))
+
         color = '#1f77b4' if weight >= 0 else '#d62728'
         # highlight ball center
         plt.plot(
@@ -64,17 +76,11 @@ def plot_spheres(
             linestyle='None', marker='.', color=color
             )
 
-        # Choose radius such that the sum of volumes of the balls equals
-        # total_volume.
-        r = (
-            abs(weight)/sum_weights * total_volume/(4.0/3.0 * numpy.pi)
-            )**(1.0/3.0)
-
         ax.plot_surface(
             r*x + tp[0], r*y + tp[1], r*z + tp[2],
             color=color,
-            alpha=alpha,
-            linewidth=0
+            alpha=0.3,
+            linewidth=1
             )
 
     ax.set_axis_off()
