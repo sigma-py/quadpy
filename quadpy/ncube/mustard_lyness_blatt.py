ec7f89f8f157af84b30872c2a75b2224a394aedc
Nico Schlömer <nico.schloemer@gmail.com>
2017-07-26 17:12:13 +0200

    more dissection in n-cube

---
+++ quadpy/ncube/mustard_lyness_blatt.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+#
+import numpy
+
+from .helpers import _fsd, _z, _pm
+
+
+class MustardLynessBlatt(object):
+    '''
+    D. Mustard, J.N. Lyness, J.M. Blatt,
+    Numerical quadrature in n dimensions,
+    Comput J (1963) 6 (1): 75-87,
+    <https://doi.org/10.1093/comjnl/6.1.75>.
+
+    Abstract
+    We investigate a selection of integration rules based on the combination of
+    third degree rules for elementary three-dimensional sub-domains. The
+    practical problems associated with the application of these rules to
+    domains bounded by planes of a certain type are discussed in detail. These
+    included integration rules for less symmetrical domains which may occur
+    near the boundary of the volume of integration, methods for combining
+    integration coefficients from adjacent sub-domains, and methods for
+    changing net size within the volume of integration.
+    Particular attention is paid to minimizing the number of points at which
+    the function has to be evaluated, and error estimates in terms of
+    computation time are given. A list of integration coefficients of general
+    interest for three-dimensional integrations is presented. The discussion is
+    generalized to n dimensions for hyper-cubic domains.
+    '''
+    def __init__(self, n):
+        reference_volume = 2.0**n
+        self.degree = 5
+        r = numpy.sqrt(2.0 / 5.0)
+        self.points = numpy.concatenate([
+            _z(n),
+            _fsd(n, r, 1),
+            _pm(n, 1.0),
+            ])
+        self.weights = numpy.concatenate([
+            numpy.full(1, (8 - 5*n)/9.0 * reference_volume),
+            numpy.full(2*n, 5.0/18.0 * reference_volume),
+            numpy.full(2**n, 1.0/9.0 / 2**n * reference_volume),
+            ])
+        return
--- quadpy/ncube/stroud.py
+++ quadpy/ncube/stroud.py
@@ -2,11 +2,14 @@
 #
 import numpy
 
-from .helpers import _fsd, _pm, _z, _s11, _s2
+from .helpers import _fsd, _pm
 from . import ewing
+from . import hammer_stroud
+from . import mustard_lyness_blatt
 from . import phillips
 from . import stroud57
 from . import stroud66
+from . import stroud68
 from . import thacher
 from . import tyler
 
@@ -85,70 +88,25 @@ class Stroud(object):
         # Cn 5-1 is not implemented because it's based on explicit values only
         # given for n=4,5,6.
         elif index == 'Cn 5-2':
-            # Preston C. Hammer and Arthur H. Stroud,
-            # Numerical Evaluation of Multiple Integrals II.
-            self.degree = 5
-            r = numpy.sqrt(3.0 / 5.0)
-            self.points = numpy.concatenate([
-                _z(n),
-                _fsd(n, r, 1),
-                _fsd(n, r, 2),
-                ])
-            self.weights = numpy.concatenate([
-                numpy.full(
-                    1, (25*n**2 - 115*n + 162)/162.0 * reference_volume
-                    ),
-                numpy.full(2*n, (70 - 25*n)/162.0 * reference_volume),
-                numpy.full(2*n*(n-1), 25.0/324.0 * reference_volume),
-                ])
+            scheme = hammer_stroud.HammerStroud(n)
+            self.degree = scheme.degree
+            self.points = scheme.points
+            self.weights = scheme.weights
         elif index == 'Cn 5-3':
-            # A. H. Stroud,
-            # Extensions of Symmetric Integration Formulas,
-            # Mathematics of Computation,
-            # Vol. 22, No. 102 (Apr., 1968), pp. 271-274,
-            # Published by: American Mathematical Society,
-            # DOI: 10.2307/2004655.
-            self.degree = 5
-            r = numpy.sqrt(7.0 / 15.0)
-            s = numpy.sqrt((7.0 + numpy.sqrt(24.0)) / 15.0)
-            t = numpy.sqrt((7.0 - numpy.sqrt(24.0)) / 15.0)
-            self.points = numpy.concatenate([
-                _z(n),
-                _s2(n, r),
-                _s2(n, -r),
-                _fsd(n, r, 1),
-                _s11(n, +s, -t),
-                _s11(n, -s, +t),
-                _fsd(n, s, 1),
-                _fsd(n, t, 1),
-                ])
-            self.weights = numpy.concatenate([
-                numpy.full(1, (5*n**2 - 15*n+14)/14.0 * reference_volume),
-                numpy.full(n*(n-1), 25.0/168.0 * reference_volume),
-                numpy.full(2*n, -25*(n-2)/168.0 * reference_volume),
-                numpy.full(2*n*(n-1), 5.0/48.0 * reference_volume),
-                numpy.full(4*n, -5*(n-2)/48.0 * reference_volume),
-                ])
+            scheme = stroud68.Stroud68(n)
+            self.degree = scheme.degree
+            self.points = scheme.points
+            self.weights = scheme.weights
         elif index == 'Cn 5-4':
             scheme = stroud66.Stroud66(n, 'a')
             self.degree = scheme.degree
             self.points = scheme.points
             self.weights = scheme.weights
         elif index == 'Cn 5-5':
-            # Mustard, Lyness, Blatt,
-            # Numerical quadrature in N dimensions.
-            self.degree = 5
-            r = numpy.sqrt(2.0 / 5.0)
-            self.points = numpy.concatenate([
-                _z(n),
-                _fsd(n, r, 1),
-                _pm(n, 1.0),
-                ])
-            self.weights = numpy.concatenate([
-                numpy.full(1, (8 - 5*n)/9.0 * reference_volume),
-                numpy.full(2*n, 5.0/18.0 * reference_volume),
-                numpy.full(2**n, 1.0/9.0 / 2**n * reference_volume),
-                ])
+            scheme = mustard_lyness_blatt.MustardLynessBlatt(n)
+            self.degree = scheme.degree
+            self.points = scheme.points
+            self.weights = scheme.weights
         elif index == 'Cn 5-6':
             scheme = stroud66.Stroud66(n, 'b')
             self.degree = scheme.degree
@@ -175,12 +133,11 @@ class Stroud(object):
             sqrt35 = numpy.sqrt(3.0/5.0)
             lst = n * [[-sqrt35, 0.0, sqrt35]]
             self.points = numpy.array(numpy.meshgrid(*lst)).T.reshape(-1, n)
-        elif index == 'Cn 7-1':
+        else:
+            assert index == 'Cn 7-1'
             scheme = phillips.Phillips(n)
             self.degree = scheme.degree
             self.points = scheme.points
             self.weights = scheme.weights
-        else:
-            assert False
 
         return
