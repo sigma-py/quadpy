5114c21fbdb903cdf6faf29aa95c8bfe75519bbb
Nico Schlömer <nico.schloemer@gmail.com>
2017-07-27 15:53:22 +0200

    update how you use hexahedra

--- quadpy/hexahedron/tools.py
+++ quadpy/hexahedron/tools.py
@@ -5,12 +5,19 @@ import numpy
 from .. import helpers
 
 
+def cube_points(x, y, z):
+    '''Given the end points of a cube aligned with the coordinate axes, this
+    returns the corner points of the cube in the correct data structure.
+    '''
+    return numpy.moveaxis(
+            numpy.array(numpy.meshgrid(x, y, z, indexing='ij')),
+            0, -1
+            )
+
+
 def show(
         scheme,
-        hexa=numpy.array([
-            [0, 0, 0], [1, 0, 0], [1, 1, 0], [0, 1, 0],
-            [0, 0, 1], [1, 0, 1], [1, 1, 1], [0, 1, 1],
-            ]),
+        hexa=cube_points([0.0, 1.0], [0.0, 1.0], [0.0, 1.0]),
         show_axes=False
         ):
     '''Shows the quadrature points on a given hexahedron. The size of the
@@ -28,20 +35,20 @@ def show(
         ax.set_axis_off()
 
     edges = numpy.array([
-        [hexa[0], hexa[1]],
-        [hexa[1], hexa[2]],
-        [hexa[2], hexa[3]],
-        [hexa[3], hexa[0]],
+        [hexa[0][0][0], hexa[1, 0, 0]],
+        [hexa[1][0][0], hexa[1, 1, 0]],
+        [hexa[1][1][0], hexa[0, 1, 0]],
+        [hexa[0][1][0], hexa[0, 0, 0]],
         #
-        [hexa[4], hexa[5]],
-        [hexa[5], hexa[6]],
-        [hexa[6], hexa[7]],
-        [hexa[7], hexa[4]],
+        [hexa[0][0][1], hexa[1, 0, 1]],
+        [hexa[1][0][1], hexa[1, 1, 1]],
+        [hexa[1][1][1], hexa[0, 1, 1]],
+        [hexa[0][1][1], hexa[0, 0, 1]],
         #
-        [hexa[0], hexa[4]],
-        [hexa[1], hexa[5]],
-        [hexa[2], hexa[6]],
-        [hexa[3], hexa[7]],
+        [hexa[0][0][0], hexa[0, 0, 1]],
+        [hexa[1][0][0], hexa[1, 0, 1]],
+        [hexa[1][1][0], hexa[1, 1, 1]],
+        [hexa[0][1][0], hexa[0, 1, 1]],
         ])
     for edge in edges:
         plt.plot(edge[:, 0], edge[:, 1], edge[:, 2], '-k')
@@ -50,14 +57,14 @@ def show(
     eta = scheme.points[:, 1]
     zeta = scheme.points[:, 2]
     transformed_pts = \
-        + numpy.outer(0.125 * (1.0 - xi)*(1.0 - eta)*(1.0 - zeta), hexa[0]) \
-        + numpy.outer(0.125 * (1.0 + xi)*(1.0 - eta)*(1.0 - zeta), hexa[1]) \
-        + numpy.outer(0.125 * (1.0 + xi)*(1.0 + eta)*(1.0 - zeta), hexa[2]) \
-        + numpy.outer(0.125 * (1.0 - xi)*(1.0 + eta)*(1.0 - zeta), hexa[3]) \
-        + numpy.outer(0.125 * (1.0 - xi)*(1.0 - eta)*(1.0 + zeta), hexa[4]) \
-        + numpy.outer(0.125 * (1.0 + xi)*(1.0 - eta)*(1.0 + zeta), hexa[5]) \
-        + numpy.outer(0.125 * (1.0 + xi)*(1.0 + eta)*(1.0 + zeta), hexa[6]) \
-        + numpy.outer(0.125 * (1.0 - xi)*(1.0 + eta)*(1.0 + zeta), hexa[7])
+        + numpy.outer(0.125 * (1 - xi)*(1 - eta)*(1 - zeta), hexa[0, 0, 0]) \
+        + numpy.outer(0.125 * (1 + xi)*(1 - eta)*(1 - zeta), hexa[1, 0, 0]) \
+        + numpy.outer(0.125 * (1 + xi)*(1 + eta)*(1 - zeta), hexa[1, 1, 0]) \
+        + numpy.outer(0.125 * (1 - xi)*(1 + eta)*(1 - zeta), hexa[0, 1, 0]) \
+        + numpy.outer(0.125 * (1 - xi)*(1 - eta)*(1 + zeta), hexa[0, 0, 1]) \
+        + numpy.outer(0.125 * (1 + xi)*(1 - eta)*(1 + zeta), hexa[1, 0, 1]) \
+        + numpy.outer(0.125 * (1 + xi)*(1 + eta)*(1 + zeta), hexa[1, 1, 1]) \
+        + numpy.outer(0.125 * (1 - xi)*(1 + eta)*(1 + zeta), hexa[0, 1, 1])
 
     vol = integrate(lambda x: 1.0, hexa, scheme)
     helpers.plot_spheres(
@@ -72,45 +79,45 @@ def integrate(f, hexa, scheme, sumfun=helpers.kahan_sum):
     xi = scheme.points.T
     mo = numpy.multiply.outer
     x = \
-        + mo(0.125*(1.0-xi[0])*(1.0-xi[1])*(1.0-xi[2]), hexa[0]) \
-        + mo(0.125*(1.0+xi[0])*(1.0-xi[1])*(1.0-xi[2]), hexa[1]) \
-        + mo(0.125*(1.0+xi[0])*(1.0+xi[1])*(1.0-xi[2]), hexa[2]) \
-        + mo(0.125*(1.0-xi[0])*(1.0+xi[1])*(1.0-xi[2]), hexa[3]) \
-        + mo(0.125*(1.0-xi[0])*(1.0-xi[1])*(1.0+xi[2]), hexa[4]) \
-        + mo(0.125*(1.0+xi[0])*(1.0-xi[1])*(1.0+xi[2]), hexa[5]) \
-        + mo(0.125*(1.0+xi[0])*(1.0+xi[1])*(1.0+xi[2]), hexa[6]) \
-        + mo(0.125*(1.0-xi[0])*(1.0+xi[1])*(1.0+xi[2]), hexa[7])
+        + mo(0.125*(1.0-xi[0])*(1.0-xi[1])*(1.0-xi[2]), hexa[0, 0, 0]) \
+        + mo(0.125*(1.0+xi[0])*(1.0-xi[1])*(1.0-xi[2]), hexa[1, 0, 0]) \
+        + mo(0.125*(1.0+xi[0])*(1.0+xi[1])*(1.0-xi[2]), hexa[1, 1, 0]) \
+        + mo(0.125*(1.0-xi[0])*(1.0+xi[1])*(1.0-xi[2]), hexa[0, 1, 0]) \
+        + mo(0.125*(1.0-xi[0])*(1.0-xi[1])*(1.0+xi[2]), hexa[0, 0, 1]) \
+        + mo(0.125*(1.0+xi[0])*(1.0-xi[1])*(1.0+xi[2]), hexa[1, 0, 1]) \
+        + mo(0.125*(1.0+xi[0])*(1.0+xi[1])*(1.0+xi[2]), hexa[1, 1, 1]) \
+        + mo(0.125*(1.0-xi[0])*(1.0+xi[1])*(1.0+xi[2]), hexa[0, 1, 1])
     x = x.T
 
     J0 = \
-        - numpy.multiply.outer(0.125*(1 - xi[1])*(1.0 - xi[2]), hexa[0]) \
-        + numpy.multiply.outer(0.125*(1 - xi[1])*(1.0 - xi[2]), hexa[1]) \
-        + numpy.multiply.outer(0.125*(1 + xi[1])*(1.0 - xi[2]), hexa[2]) \
-        - numpy.multiply.outer(0.125*(1 + xi[1])*(1.0 - xi[2]), hexa[3]) \
-        - numpy.multiply.outer(0.125*(1 - xi[1])*(1.0 + xi[2]), hexa[4]) \
-        + numpy.multiply.outer(0.125*(1 - xi[1])*(1.0 + xi[2]), hexa[5]) \
-        + numpy.multiply.outer(0.125*(1 + xi[1])*(1.0 + xi[2]), hexa[6]) \
-        - numpy.multiply.outer(0.125*(1 + xi[1])*(1.0 + xi[2]), hexa[7])
+        - numpy.multiply.outer(0.125*(1 - xi[1])*(1 - xi[2]), hexa[0, 0, 0]) \
+        + numpy.multiply.outer(0.125*(1 - xi[1])*(1 - xi[2]), hexa[1, 0, 0]) \
+        + numpy.multiply.outer(0.125*(1 + xi[1])*(1 - xi[2]), hexa[1, 1, 0]) \
+        - numpy.multiply.outer(0.125*(1 + xi[1])*(1 - xi[2]), hexa[0, 1, 0]) \
+        - numpy.multiply.outer(0.125*(1 - xi[1])*(1 + xi[2]), hexa[0, 0, 1]) \
+        + numpy.multiply.outer(0.125*(1 - xi[1])*(1 + xi[2]), hexa[1, 0, 1]) \
+        + numpy.multiply.outer(0.125*(1 + xi[1])*(1 + xi[2]), hexa[1, 1, 1]) \
+        - numpy.multiply.outer(0.125*(1 + xi[1])*(1 + xi[2]), hexa[0, 1, 1])
     J0 = J0.T
     J1 = \
-        - numpy.multiply.outer(0.125*(1 - xi[0])*(1.0 - xi[2]), hexa[0]) \
-        - numpy.multiply.outer(0.125*(1 + xi[0])*(1.0 - xi[2]), hexa[1]) \
-        + numpy.multiply.outer(0.125*(1 + xi[0])*(1.0 - xi[2]), hexa[2]) \
-        + numpy.multiply.outer(0.125*(1 - xi[0])*(1.0 - xi[2]), hexa[3]) \
-        - numpy.multiply.outer(0.125*(1 - xi[0])*(1.0 + xi[2]), hexa[4]) \
-        - numpy.multiply.outer(0.125*(1 + xi[0])*(1.0 + xi[2]), hexa[5]) \
-        + numpy.multiply.outer(0.125*(1 + xi[0])*(1.0 + xi[2]), hexa[6]) \
-        + numpy.multiply.outer(0.125*(1 - xi[0])*(1.0 + xi[2]), hexa[7])
+        - numpy.multiply.outer(0.125*(1 - xi[0])*(1 - xi[2]), hexa[0, 0, 0]) \
+        - numpy.multiply.outer(0.125*(1 + xi[0])*(1 - xi[2]), hexa[1, 0, 0]) \
+        + numpy.multiply.outer(0.125*(1 + xi[0])*(1 - xi[2]), hexa[1, 1, 0]) \
+        + numpy.multiply.outer(0.125*(1 - xi[0])*(1 - xi[2]), hexa[0, 1, 0]) \
+        - numpy.multiply.outer(0.125*(1 - xi[0])*(1 + xi[2]), hexa[0, 0, 1]) \
+        - numpy.multiply.outer(0.125*(1 + xi[0])*(1 + xi[2]), hexa[1, 0, 1]) \
+        + numpy.multiply.outer(0.125*(1 + xi[0])*(1 + xi[2]), hexa[1, 1, 1]) \
+        + numpy.multiply.outer(0.125*(1 - xi[0])*(1 + xi[2]), hexa[0, 1, 1])
     J1 = J1.T
     J2 = \
-        - numpy.multiply.outer(0.125*(1.0 - xi[0])*(1.0 - xi[1]), hexa[0]) \
-        - numpy.multiply.outer(0.125*(1.0 + xi[0])*(1.0 - xi[1]), hexa[1]) \
-        - numpy.multiply.outer(0.125*(1.0 + xi[0])*(1.0 + xi[1]), hexa[2]) \
-        - numpy.multiply.outer(0.125*(1.0 - xi[0])*(1.0 + xi[1]), hexa[3]) \
-        + numpy.multiply.outer(0.125*(1.0 - xi[0])*(1.0 - xi[1]), hexa[4]) \
-        + numpy.multiply.outer(0.125*(1.0 + xi[0])*(1.0 - xi[1]), hexa[5]) \
-        + numpy.multiply.outer(0.125*(1.0 + xi[0])*(1.0 + xi[1]), hexa[6]) \
-        + numpy.multiply.outer(0.125*(1.0 - xi[0])*(1.0 + xi[1]), hexa[7])
+        - numpy.multiply.outer(0.125*(1 - xi[0])*(1 - xi[1]), hexa[0, 0, 0]) \
+        - numpy.multiply.outer(0.125*(1 + xi[0])*(1 - xi[1]), hexa[1, 0, 0]) \
+        - numpy.multiply.outer(0.125*(1 + xi[0])*(1 + xi[1]), hexa[1, 1, 0]) \
+        - numpy.multiply.outer(0.125*(1 - xi[0])*(1 + xi[1]), hexa[0, 1, 0]) \
+        + numpy.multiply.outer(0.125*(1 - xi[0])*(1 - xi[1]), hexa[0, 0, 1]) \
+        + numpy.multiply.outer(0.125*(1 + xi[0])*(1 - xi[1]), hexa[1, 0, 1]) \
+        + numpy.multiply.outer(0.125*(1 + xi[0])*(1 + xi[1]), hexa[1, 1, 1]) \
+        + numpy.multiply.outer(0.125*(1 - xi[0])*(1 + xi[1]), hexa[0, 1, 1])
     J2 = J2.T
     det = J0[0]*J1[1]*J2[2] + J1[0]*J2[1]*J0[2] + J2[0]*J0[1]*J1[2] \
         - J0[2]*J1[1]*J2[0] - J1[2]*J2[1]*J0[0] - J2[2]*J0[1]*J1[0]
