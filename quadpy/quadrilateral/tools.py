b64f44edc3eebef4d9212bc96e58b673002e3df3
Nico Schlömer <nico.schloemer@gmail.com>
2017-07-28 19:34:41 +0200

    move transformation determinant code to ncube

--- quadpy/quadrilateral/tools.py
+++ quadpy/quadrilateral/tools.py
@@ -4,7 +4,7 @@ import numpy
 
 from .. import helpers
 from .stroud import Stroud
-from ..ncube import transform
+from ..ncube import transform, get_detJ
 
 
 def rectangle_points(x, y):
@@ -47,25 +47,7 @@ def show(
     return
 
 
-def _get_detJ(xi, quad):
-    '''Get the determinant of the transformation matrix.
-    '''
-    J0 = (
-        - numpy.multiply.outer(0.25*(1-xi[1]), quad[0, 0])
-        + numpy.multiply.outer(0.25*(1-xi[1]), quad[1, 0])
-        + numpy.multiply.outer(0.25*(1+xi[1]), quad[1, 1])
-        - numpy.multiply.outer(0.25*(1+xi[1]), quad[0, 1])
-        ).T
-    J1 = (
-        - numpy.multiply.outer(0.25*(1-xi[0]), quad[0, 0])
-        - numpy.multiply.outer(0.25*(1+xi[0]), quad[1, 0])
-        + numpy.multiply.outer(0.25*(1+xi[0]), quad[1, 1])
-        + numpy.multiply.outer(0.25*(1-xi[0]), quad[0, 1])
-        ).T
-    return J0[0]*J1[1] - J1[0]*J0[1]
-
-
 def integrate(f, quad, scheme, sumfun=helpers.kahan_sum):
     x = transform(scheme.points.T, quad).T
-    det = _get_detJ(scheme.points.T, quad)
+    det = get_detJ(scheme.points.T, quad)
     return sumfun(scheme.weights * f(x) * abs(det), axis=-1)
