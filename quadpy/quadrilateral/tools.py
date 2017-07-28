d20648bbcbe60fbf2953a65f55d9e389000f7288
Nico Schlömer <nico.schloemer@gmail.com>
2017-07-28 18:53:57 +0200

    more transform() to ncube, use it for hexahedra as well

--- quadpy/quadrilateral/tools.py
+++ quadpy/quadrilateral/tools.py
@@ -4,7 +4,7 @@ import numpy
 
 from .. import helpers
 from .stroud import Stroud
-# from ..ncube import transform
+from ..ncube import transform
 
 
 def rectangle_points(x, y):
@@ -45,33 +45,6 @@ def show(
         )
     plt.show()
     return
-
-
-def transform(xi, quad):
-    '''Transform the points xi from the reference quad to the quad `quad`.
-    '''
-    # For d==2, the result used to be computed with
-    #
-    # out = (
-    #     + outer(0.25*(1.0-xi[0])*(1.0-xi[1]), quad[0, 0])
-    #     + outer(0.25*(1.0+xi[0])*(1.0-xi[1]), quad[1, 0])
-    #     + outer(0.25*(1.0-xi[0])*(1.0+xi[1]), quad[0, 1])
-    #     + outer(0.25*(1.0+xi[0])*(1.0+xi[1]), quad[1, 1])
-    #     )
-    #
-    # This array of multiplications and additions is reminiscent of dot(), and
-    # indeed tensordot() can handle the situation. We just need to compute the
-    # `1+-xi` products and align them with `quad`.
-    one_mp_xi = numpy.stack([
-        0.5 * (1.0 - xi),
-        0.5 * (1.0 + xi),
-        ], axis=1)
-    a = helpers.n_outer(one_mp_xi)
-
-    # TODO kahan tensordot
-    # <https://stackoverflow.com/q/45372098/353337>
-    d = 2
-    return numpy.tensordot(a, quad, axes=(range(d), range(d)))
 
 
 def _get_detJ(xi, quad):
