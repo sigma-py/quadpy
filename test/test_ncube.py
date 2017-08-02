8977e6804a7fa68e6320dc9d97c7feddbe1efd72
Nico Schlömer <nico.schloemer@gmail.com>
2017-08-02 17:24:25 +0200

    add test for stroud 7-1 (phillips)

--- test/test_ncube.py
+++ test/test_ncube.py
@@ -14,18 +14,19 @@ stroud_idx = [
     'Cn 3-2', 'Cn 3-3', 'Cn 3-4', 'Cn 3-5', 'Cn 3-6',
     'Cn 5-2', 'Cn 5-3', 'Cn 5-4', 'Cn 5-5', 'Cn 5-6', 'Cn 5-7', 'Cn 5-8',
     'Cn 5-9',
-    # 'Cn 7-1',
     ]
 
 
 @pytest.mark.parametrize(
-    'scheme',
-    [quadpy.ncube.Stroud(3, k) for k in stroud_idx]
-    + [quadpy.ncube.Stroud(4, k) for k in stroud_idx]
-    + [quadpy.ncube.Stroud(5, k) for k in stroud_idx]
-    + [quadpy.ncube.Stroud(6, k) for k in stroud_idx]
+    'scheme, tol',
+    [(quadpy.ncube.Stroud(3, k), 1.0e-14) for k in stroud_idx]
+    + [(quadpy.ncube.Stroud(4, k), 1.0e-14) for k in stroud_idx]
+    + [(quadpy.ncube.Stroud(5, k), 1.0e-14) for k in stroud_idx]
+    + [(quadpy.ncube.Stroud(6, k), 1.0e-14) for k in stroud_idx]
+    #
+    + [(quadpy.ncube.Stroud(d, 'Cn 7-1'), 1.0e-7) for d in range(2, 7)]
     )
-def test_scheme(scheme):
+def test_scheme(scheme, tol):
     n = scheme.dim
     ncube_limits = [[0.0, 1.0]] * n
     ncube = quadpy.ncube.ncube_points(*ncube_limits)
@@ -33,7 +34,8 @@ def test_scheme(scheme):
             lambda poly: quadpy.ncube.integrate(poly, ncube, scheme),
             lambda exp: _integrate_monomial_over_ncube(ncube_limits, exp),
             lambda k: quadpy.helpers.partition(k, n),
-            scheme.degree + 1
+            scheme.degree + 1,
+            tol=tol
             )
     assert degree >= scheme.degree, \
         'observed: {}, expected: {}'.format(degree, scheme.degree)
