ab5430bb00f507a192463b5acc909da4947869ae
Nico Schlömer <nico.schloemer@gmail.com>
2017-08-02 20:13:58 +0200

    clean up pyramid tools

--- quadpy/pyramid/tools.py
+++ quadpy/pyramid/tools.py
@@ -7,6 +7,51 @@ from .felippa import Felippa
 from .. import helpers
 
 
+def integrate(f, pyra, scheme, sumfun=helpers.kahan_sum):
+    xi = scheme.points.T
+    x = _transform(xi, pyra)
+    det = _get_det_J(pyra, xi)
+    return sumfun(scheme.weights * f(x) * abs(det.T), axis=-1)
+
+
+def _transform(xi, pyra):
+    mo = numpy.multiply.outer
+    return (
+        + mo(0.125*(1.0-xi[0])*(1.0-xi[1])*(1-xi[2]), pyra[0])
+        + mo(0.125*(1.0+xi[0])*(1.0-xi[1])*(1-xi[2]), pyra[1])
+        + mo(0.125*(1.0+xi[0])*(1.0+xi[1])*(1-xi[2]), pyra[2])
+        + mo(0.125*(1.0-xi[0])*(1.0+xi[1])*(1-xi[2]), pyra[3])
+        + mo(0.500*(1.0+xi[2]), pyra[4])
+        ).T
+
+
+def _get_det_J(pyra, xi):
+    J0 = (
+        - numpy.multiply.outer(0.125*(1.0-xi[1])*(1-xi[2]), pyra[0])
+        + numpy.multiply.outer(0.125*(1.0-xi[1])*(1-xi[2]), pyra[1])
+        + numpy.multiply.outer(0.125*(1.0+xi[1])*(1-xi[2]), pyra[2])
+        - numpy.multiply.outer(0.125*(1.0+xi[1])*(1-xi[2]), pyra[3])
+        ).T
+    J1 = (
+        - numpy.multiply.outer(0.125*(1.0-xi[0])*(1-xi[2]), pyra[0])
+        - numpy.multiply.outer(0.125*(1.0+xi[0])*(1-xi[2]), pyra[1])
+        + numpy.multiply.outer(0.125*(1.0+xi[0])*(1-xi[2]), pyra[2])
+        + numpy.multiply.outer(0.125*(1.0-xi[0])*(1-xi[2]), pyra[3])
+        ).T
+    J2 = (
+        - numpy.multiply.outer(0.125*(1.0-xi[0])*(1.0-xi[1]), pyra[0])
+        - numpy.multiply.outer(0.125*(1.0+xi[0])*(1.0-xi[1]), pyra[1])
+        - numpy.multiply.outer(0.125*(1.0+xi[0])*(1.0+xi[1]), pyra[2])
+        - numpy.multiply.outer(0.125*(1.0-xi[0])*(1.0+xi[1]), pyra[3])
+        + numpy.multiply.outer(0.500*numpy.ones(1), pyra[4])
+        ).T
+    det = (
+        + J0[0]*J1[1]*J2[2] + J1[0]*J2[1]*J0[2] + J2[0]*J0[1]*J1[2]
+        - J0[2]*J1[1]*J2[0] - J1[2]*J2[1]*J0[0] - J2[2]*J0[1]*J1[0]
+        )
+    return det.T
+
+
 def show(*args, **kwargs):
     plot(*args, **kwargs)
     plt.show()
@@ -41,61 +86,14 @@ def plot(
         [pyra[2], pyra[4]],
         [pyra[3], pyra[4]],
         ])
+    edges = numpy.moveaxis(edges, 1, 2)
     for edge in edges:
-        plt.plot(edge[:, 0], edge[:, 1], edge[:, 2], '-k')
+        plt.plot(*edge, color='k')
 
-    xi = scheme.points[:, 0]
-    eta = scheme.points[:, 1]
-    zeta = scheme.points[:, 2]
-    transformed_pts = \
-        + numpy.outer(pyra[0], 0.125*(1.0-xi)*(1.0-eta)*(1-zeta)) \
-        + numpy.outer(pyra[1], 0.125*(1.0+xi)*(1.0-eta)*(1-zeta)) \
-        + numpy.outer(pyra[2], 0.125*(1.0+xi)*(1.0+eta)*(1-zeta)) \
-        + numpy.outer(pyra[3], 0.125*(1.0-xi)*(1.0+eta)*(1-zeta)) \
-        + numpy.outer(pyra[4], 0.500*(1.0+zeta))
-    transformed_pts = transformed_pts.T
+    transformed_pts = _transform(scheme.points.T, pyra).T
 
     vol = integrate(lambda x: 1.0, pyra, Felippa(1))
     helpers.plot_spheres(
         plt, ax, transformed_pts, scheme.weights, vol
         )
     return
-
-
-def _get_det_J(pyra, xi):
-    J0 = \
-        - numpy.multiply.outer(0.125*(1.0-xi[1])*(1-xi[2]), pyra[0]) \
-        + numpy.multiply.outer(0.125*(1.0-xi[1])*(1-xi[2]), pyra[1]) \
-        + numpy.multiply.outer(0.125*(1.0+xi[1])*(1-xi[2]), pyra[2]) \
-        - numpy.multiply.outer(0.125*(1.0+xi[1])*(1-xi[2]), pyra[3])
-    J0 = J0.T
-    J1 = \
-        - numpy.multiply.outer(0.125*(1.0-xi[0])*(1-xi[2]), pyra[0]) \
-        - numpy.multiply.outer(0.125*(1.0+xi[0])*(1-xi[2]), pyra[1]) \
-        + numpy.multiply.outer(0.125*(1.0+xi[0])*(1-xi[2]), pyra[2]) \
-        + numpy.multiply.outer(0.125*(1.0-xi[0])*(1-xi[2]), pyra[3])
-    J1 = J1.T
-    J2 = \
-        - numpy.multiply.outer(0.125*(1.0-xi[0])*(1.0-xi[1]), pyra[0]) \
-        - numpy.multiply.outer(0.125*(1.0+xi[0])*(1.0-xi[1]), pyra[1]) \
-        - numpy.multiply.outer(0.125*(1.0+xi[0])*(1.0+xi[1]), pyra[2]) \
-        - numpy.multiply.outer(0.125*(1.0-xi[0])*(1.0+xi[1]), pyra[3]) \
-        + numpy.multiply.outer(0.500*numpy.ones(1), pyra[4])
-    J2 = J2.T
-    det = J0[0]*J1[1]*J2[2] + J1[0]*J2[1]*J0[2] + J2[0]*J0[1]*J1[2] \
-        - J0[2]*J1[1]*J2[0] - J1[2]*J2[1]*J0[0] - J2[2]*J0[1]*J1[0]
-    return det.T
-
-
-def integrate(f, pyra, scheme, sumfun=helpers.kahan_sum):
-    xi = scheme.points.T
-    mo = numpy.multiply.outer
-    x = \
-        + mo(0.125*(1.0-xi[0])*(1.0-xi[1])*(1-xi[2]), pyra[0]) \
-        + mo(0.125*(1.0+xi[0])*(1.0-xi[1])*(1-xi[2]), pyra[1]) \
-        + mo(0.125*(1.0+xi[0])*(1.0+xi[1])*(1-xi[2]), pyra[2]) \
-        + mo(0.125*(1.0-xi[0])*(1.0+xi[1])*(1-xi[2]), pyra[3]) \
-        + mo(0.500*(1.0+xi[2]), pyra[4])
-    x = x.T
-    det = _get_det_J(pyra, xi)
-    return sumfun(scheme.weights * f(x) * abs(det.T), axis=-1)
