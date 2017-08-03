5d69977873624836fcadfeec56718f9411453268
Nico Schlömer <nico.schloemer@gmail.com>
2017-08-03 11:32:49 +0200

    unify backend selection

--- quadpy/hexahedron/tools.py
+++ quadpy/hexahedron/tools.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 #
-import matplotlib.pyplot as plt
 import numpy
 
 from .. import helpers
@@ -8,62 +7,20 @@ from ..ncube import transform, integrate
 from ..ncube import ncube_points as cube_points
 
 
-def show(*args, **kwargs):
-    plot(*args, **kwargs)
-    plt.show()
-    return
-
-
-def plot(
+def show(
         scheme,
         hexa=cube_points([0.0, 1.0], [0.0, 1.0], [0.0, 1.0]),
-        show_axes=False
+        backend='mpl'
         ):
     '''Shows the quadrature points on a given hexahedron. The size of the
     balls around the points coincides with their weights.
     '''
-    # pylint: disable=relative-import, unused-variable
-    from mpl_toolkits.mplot3d import Axes3D
-
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
-    ax.set_aspect('equal')
-
-    if not show_axes:
-        ax.set_axis_off()
-
-    edges = _get_edges(hexa)
-
-    for edge in edges:
-        plt.plot(*edge, color='k', linestyle='-')
-
-    transformed_pts = transform(scheme.points.T, hexa)
-
-    vol = integrate(lambda x: 1.0, hexa, scheme)
-    helpers.plot_spheres(
-        plt, ax, transformed_pts, scheme.weights, vol
-        )
-    return
-
-
-def show_mayavi(
-        scheme,
-        hexa=cube_points([0.0, 1.0], [0.0, 1.0], [0.0, 1.0]),
-        ):
-    helpers.show_mayavi(
-            transform(scheme.points.T, hexa),
-            scheme.weights,
-            integrate(lambda x: 1.0, hexa, scheme),
-            _get_edges(hexa)
-            )
-    return
-
-
-def show_vtk(
-        scheme,
-        hexa=cube_points([0.0, 1.0], [0.0, 1.0], [0.0, 1.0]),
-        ):
-    helpers.show_vtk(
+    backend_to_function = {
+        'mayavi': helpers.show_mayavi,
+        'mpl': helpers.show_mpl,
+        'vtk': helpers.show_vtk,
+        }
+    backend_to_function[backend](
             transform(scheme.points.T, hexa),
             scheme.weights,
             integrate(lambda x: 1.0, hexa, scheme),
