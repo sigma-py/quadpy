312235bebd98e6daf6d5468cddf4ed2f9f410319
Nico Schlömer <nico.schloemer@gmail.com>
2017-08-03 12:34:31 +0200

    reorder wedge points

--- quadpy/wedge/tools.py
+++ quadpy/wedge/tools.py
@@ -15,36 +15,36 @@ def integrate(f, wedge, scheme, sumfun=helpers.kahan_sum):
 def _transform(xi, wedge):
     mo = numpy.multiply.outer
     return (
-        + mo(0.5 * (1.0-xi[0]-xi[1]) * (1.0-xi[2]), wedge[0])
-        + mo(0.5 * xi[0] * (1.0-xi[2]), wedge[1])
-        + mo(0.5 * xi[1] * (1.0-xi[2]), wedge[2])
-        + mo(0.5 * (1.0-xi[0]-xi[1]) * (1.0+xi[2]), wedge[3])
-        + mo(0.5 * xi[0] * (1.0+xi[2]), wedge[4])
-        + mo(0.5 * xi[1] * (1.0+xi[2]), wedge[5])
+        + mo(0.5 * (1.0-xi[0]-xi[1]) * (1.0-xi[2]), wedge[0, 0])
+        + mo(0.5 * xi[0] * (1.0-xi[2]), wedge[0, 1])
+        + mo(0.5 * xi[1] * (1.0-xi[2]), wedge[0, 2])
+        + mo(0.5 * (1.0-xi[0]-xi[1]) * (1.0+xi[2]), wedge[1, 0])
+        + mo(0.5 * xi[0] * (1.0+xi[2]), wedge[1, 1])
+        + mo(0.5 * xi[1] * (1.0+xi[2]), wedge[1, 2])
         ).T
 
 
 def _get_detJ(xi, wedge):
     mo = numpy.multiply.outer
     J0 = (
-        - mo(0.5*(1.0 - xi[2]), wedge[0])
-        + mo(0.5*(1.0 - xi[2]), wedge[2])
-        - mo(0.5*(1.0 + xi[2]), wedge[3])
-        + mo(0.5*(1.0 + xi[2]), wedge[5])
+        - mo(0.5*(1.0 - xi[2]), wedge[0, 0])
+        + mo(0.5*(1.0 - xi[2]), wedge[0, 1])
+        - mo(0.5*(1.0 + xi[2]), wedge[1, 0])
+        + mo(0.5*(1.0 + xi[2]), wedge[1, 1])
         ).T
     J1 = (
-        - mo(0.5*(1.0 - xi[2]), wedge[0])
-        + mo(0.5*(1.0 - xi[2]), wedge[1])
-        - mo(0.5*(1.0 + xi[2]), wedge[3])
-        + mo(0.5*(1.0 + xi[2]), wedge[4])
+        - mo(0.5*(1.0 - xi[2]), wedge[0, 0])
+        + mo(0.5*(1.0 - xi[2]), wedge[0, 2])
+        - mo(0.5*(1.0 + xi[2]), wedge[1, 0])
+        + mo(0.5*(1.0 + xi[2]), wedge[1, 2])
         ).T
     J2 = (
-        - mo(0.5 * (1.0-xi[0]-xi[1]), wedge[0])
-        - mo(0.5 * xi[0], wedge[1])
-        - mo(0.5 * xi[1], wedge[2])
-        + mo(0.5 * (1.0-xi[0]-xi[1]), wedge[3])
-        + mo(0.5 * xi[0], wedge[4])
-        + mo(0.5 * xi[1], wedge[5])
+        - mo(0.5 * (1.0-xi[0]-xi[1]), wedge[0, 0])
+        - mo(0.5 * xi[0], wedge[0, 1])
+        - mo(0.5 * xi[1], wedge[0, 2])
+        + mo(0.5 * (1.0-xi[0]-xi[1]), wedge[1, 0])
+        + mo(0.5 * xi[0], wedge[1, 1])
+        + mo(0.5 * xi[1], wedge[1, 2])
         ).T
     det = (
         + J0[0]*J1[1]*J2[2] + J1[0]*J2[1]*J0[2] + J2[0]*J0[1]*J1[2]
@@ -56,23 +56,23 @@ def _get_detJ(xi, wedge):
 def show(
         scheme,
         wedge=numpy.array([
-            [0, 0, 0], [1, 0, 0], [0, 1, 0],
-            [0, 0, 1], [1, 0, 1], [0, 1, 1],
+            [[0, 0, 0], [1, 0, 0], [0, 1, 0]],
+            [[0, 0, 1], [1, 0, 1], [0, 1, 1]],
             ], dtype=float),
         backend='mpl'
         ):
     edges = numpy.array([
-        [wedge[0], wedge[1]],
-        [wedge[1], wedge[2]],
-        [wedge[0], wedge[2]],
+        [wedge[0, 0], wedge[0, 1]],
+        [wedge[0, 1], wedge[0, 2]],
+        [wedge[0, 2], wedge[0, 0]],
         #
-        [wedge[3], wedge[4]],
-        [wedge[4], wedge[5]],
-        [wedge[5], wedge[3]],
+        [wedge[1, 0], wedge[1, 1]],
+        [wedge[1, 1], wedge[1, 2]],
+        [wedge[1, 2], wedge[1, 0]],
         #
-        [wedge[0], wedge[3]],
-        [wedge[1], wedge[4]],
-        [wedge[2], wedge[5]],
+        [wedge[0, 0], wedge[1, 0]],
+        [wedge[0, 1], wedge[1, 1]],
+        [wedge[0, 2], wedge[1, 2]],
         ])
     edges = numpy.moveaxis(edges, 1, 2)
 
