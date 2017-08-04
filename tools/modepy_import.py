0f7bab7f27a6304734b45d5cc7bd1f06949b0eb3
Nico Schlömer <nico.schloemer@gmail.com>
2017-08-04 16:55:03 +0200

    fix modepy import

--- tools/modepy_import.py
+++ tools/modepy_import.py
@@ -34,37 +34,39 @@ def _import_tri():
         #
         # Sort the indices to make it easier to identify symmetry groups.
         idx = numpy.argsort(q.weights)
-        print
-        print
-        print('k={}'.format(k))
-        print('weights')
+        sorted_weights = q.weights[idx]
+
+        bary = (q.nodes + 1.0) / 2.0
+        bary = numpy.vstack([bary, 1.0 - numpy.sum(bary, axis=0)]).T
+        sorted_points = bary[idx]
+
+        print('elif index == {}:'.format(k))
+        print('    data = [')
         # identify groups of equal weights and put them out as numpy.full(x, y)
         tol = 1.0e-12
         count = 0
-        counts = []
-        last_value = q.weights[idx][0]
-        for w in q.weights[idx]:
+
+        kk = 0
+        last_value = sorted_weights[0]
+        for w in sorted_weights:
             if abs(last_value - w) < tol:
                 count += 1
             else:
-                print('numpy.full(%d, %.15e),' % (count, last_value))
+                pts = sorted_points[kk:kk+count]
+                kk += count
+                print(
+                    8*' ' + '(%.15e, %s),' %
+                    (last_value, _get_symmetry_code_tri(pts))
+                    )
                 last_value = w
-                counts.append(count)
                 count = 1
-        counts.append(count)
-        print('numpy.full(%d, %.15e)' % (count, last_value))
-        print
 
-        bary = (q.nodes + 1.0) / 2.0
-        bary = numpy.vstack([bary, 1.0 - numpy.sum(bary, axis=0)])
-        k = 0
-        for c in counts:
-            pts = bary[:, idx[k:k+c]].T
-            print(_get_symmetry_code_tri(pts) + ',')
-            # for p in pts:
-            #     print('%.15e, %.15e, %.15e' % (p[0], p[1], p[2]))
-            # print('#')
-            k += c
+        pts = sorted_points[kk:kk+count]
+        print(
+            8*' ' + '(%.15e, %s),' %
+            (last_value, _get_symmetry_code_tri(pts))
+            )
+        print(8*' ' + ']')
 
     return
 
@@ -99,7 +101,7 @@ def _get_symmetry_code_tet(pts):
 
     assert len(pts) == 24
     pt0 = numpy.sort(pts[0])
-    return '_s111({:.15e}, {:.15e}, {:.15e})'.format(
+    return '_s1111({:.15e}, {:.15e}, {:.15e})'.format(
             pt0[-1], pt0[-2], pt0[-3]
             )
 
@@ -111,41 +113,43 @@ def _import_tet():
         q = vr.VioreanuRokhlinSimplexQuadrature(k, 3)
         # Sort the indices to make it easier to identify symmetry groups.
         idx = numpy.argsort(q.weights)
-        print
-        print
-        print('k={}'.format(k))
-        print('weights')
-        # identify groups of equal weights and put them out as numpy.full(x, y)
+        sorted_weights = q.weights[idx]
+
+        bary = (q.nodes + 1.0) / 2.0
+        bary = numpy.vstack([bary, 1.0 - numpy.sum(bary, axis=0)]).T
+        sorted_points = bary[idx]
+
+        print('elif index == {}:'.format(k))
+        print('    data = [')
+        # identify groups of equal weights
         tol = 1.0e-12
         count = 0
-        counts = []
-        last_value = q.weights[idx][0]
-        for w in q.weights[idx]:
+        last_value = sorted_weights[0]
+
+        kk = 0
+        for w in sorted_weights:
             if abs(last_value - w) < tol:
                 count += 1
             else:
-                print('numpy.full(%d, %.15e),' % (count, last_value))
+                pts = sorted_points[k:k+count]
+                kk += count
+                print(
+                    8*' ' + '(%.15e, %s),'
+                    % (last_value, _get_symmetry_code_tet(pts))
+                    )
                 last_value = w
-                counts.append(count)
                 count = 1
-        counts.append(count)
-        print('numpy.full(%d, %.15e)' % (count, last_value))
-        print
 
-        bary = (q.nodes + 1.0) / 2.0
-        bary = numpy.vstack([bary, 1.0 - numpy.sum(bary, axis=0)])
-        k = 0
-        for c in counts:
-            pts = bary[:, idx[k:k+c]].T
-            print(_get_symmetry_code_tet(pts) + ',')
-            # for p in pts:
-            #     print('%.15e, %.15e, %.15e' % (p[0], p[1], p[2]))
-            # print('#')
-            k += c
+        pts = sorted_points[kk:kk+count]
+        print(
+            8*' ' + '(%.15e, %s),'
+            % (last_value, _get_symmetry_code_tet(pts))
+            )
+        print(8*' ' + ']')
 
     return
 
 
 if __name__ == '__main__':
-    # _import_tri()
-    _import_tet()
+    _import_tri()
+    # _import_tet()
