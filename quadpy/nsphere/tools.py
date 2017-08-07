10016c17cb5b5cdce8eceb17c86b9c5fe7ff3ad9
Nico Schlömer <nico.schloemer@gmail.com>
2017-08-07 12:13:27 +0200

    add n-sphere

---
+++ quadpy/nsphere/tools.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#
+import numpy
+
+from .. import helpers
+
+
+def integrate(f, center, radius, rule, sumfun=helpers.kahan_sum):
+    center = numpy.array(center)
+    rr = numpy.multiply.outer(radius, rule.points)
+    rr = numpy.swapaxes(rr, 0, -2)
+    ff = numpy.array(f((rr + center).T))
+    out = sumfun(rule.weights * ff, axis=-1)
+    return numpy.array(radius)**(rule.dim-1) * out
