eae6236e2a7669df35493576cb44ab76faf614c4
Nico Schlömer <nico.schloemer@gmail.com>
2017-08-07 12:13:27 +0200

    add n-sphere

---
+++ quadpy/nsphere/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+#
+# pylint: disable=wildcard-import
+from .dobrodeev1978 import *
+
+from .tools import *
---
+++ test/test_nsphere.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+#
+import numpy
+import pytest
+import quadpy
+from quadpy.nsphere.helpers import integrate_monomial_over_unit_nsphere
+
+from helpers import check_degree
+
+
+@pytest.mark.parametrize(
+    'scheme',
+    [quadpy.nsphere.Dobrodeev1978(n) for n in range(2, 7)]
+    )
+def test_scheme(scheme):
+    tol = 1.0e-14
+    n = scheme.dim
+    center = numpy.zeros(n)
+    rad = 1.0
+    degree = check_degree(
+            lambda poly: quadpy.nsphere.integrate(poly, center, rad, scheme),
+            integrate_monomial_over_unit_nsphere,
+            lambda k: quadpy.helpers.partition(k, n),
+            scheme.degree + 1,
+            tol=tol
+            )
+    assert degree >= scheme.degree, \
+        'observed: {}, expected: {}'.format(degree, scheme.degree)
+    return
+
+
+if __name__ == '__main__':
+    n_ = 5
+    scheme_ = quadpy.nsphere.Dobrodeev1978(n_)
+    test_scheme(scheme_)
