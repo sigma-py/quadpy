4d0a5eaea13f978b09ec960b290b876a9de4111e
Nico Schlömer <nico.schloemer@gmail.com>
2017-08-13 21:25:14 +0200

    small fixes

--- test/test_triangle.py
+++ test/test_triangle.py
@@ -72,6 +72,9 @@ def _integrate_exact(f, triangle):
     + [(quadpy.triangle.Walkington(k), 1.0e-14) for k in [1, 2, 3, 5, 'p5']]
     + [(quadpy.triangle.WandzuraXiao(k), 1.0e-14) for k in range(1, 7)]
     + [(quadpy.triangle.WilliamsShunnJameson(k), 1.0e-14) for k in range(1, 9)]
+    + [(quadpy.triangle.WitherdenVincent(k), 1.0e-14) for k in [
+        1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
+        ]]
     + [(quadpy.triangle.XiaoGimbutas(k), 1.0e-14) for k in range(1, 51)]
     + [(quadpy.triangle.ZhangCuiLiu(k), 1.0e-14) for k in [1, 2, 3]]
     )
