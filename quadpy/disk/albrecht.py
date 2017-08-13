ae280bca338cb267cef8328c43102d8237120b73
Nico Schlömer <nico.schloemer@gmail.com>
2017-08-13 21:22:24 +0200

    add triangle scheme from witherden-vincent

--- quadpy/disk/albrecht.py
+++ quadpy/disk/albrecht.py
@@ -2,7 +2,7 @@
 #
 import numpy
 
-from ..helpers import untangle, z
+from ..helpers import untangle, z, fsd
 
 
 class Albrecht(object):
@@ -10,35 +10,65 @@ class Albrecht(object):
     '''
     Albrecht [1]
     '''
-    def __init__(self):
+    def __init__(self, index):
         # Stroud claims degree 3, but really it's only order 1.
-        self.degree = 9
+        if index == 1:
+            self.degree = 9
 
-        rho1 = numpy.sqrt((96.0 - 4*numpy.sqrt(111.0)) / 155.0)
-        rho2 = numpy.sqrt((96.0 + 4*numpy.sqrt(111.0)) / 155.0)
+            rho1 = numpy.sqrt((96.0 - 4*numpy.sqrt(111.0)) / 155.0)
+            rho2 = numpy.sqrt((96.0 + 4*numpy.sqrt(111.0)) / 155.0)
 
-        k = numpy.arange(1, 7)
-        x = numpy.array([
-            numpy.cos(k*numpy.pi/3.0),
-            numpy.sin(k*numpy.pi/3.0),
-            ]).T
+            k = numpy.arange(1, 7)
+            x = numpy.array([
+                numpy.cos(k*numpy.pi/3.0),
+                numpy.sin(k*numpy.pi/3.0),
+                ]).T
 
-        uv = numpy.sqrt(0.8) * numpy.array([
-            numpy.cos((2*k-1)*numpy.pi / 6.0),
-            numpy.sin((2*k-1)*numpy.pi / 6.0),
-            ]).T
+            uv = numpy.sqrt(0.8) * numpy.array([
+                numpy.cos((2*k-1)*numpy.pi / 6.0),
+                numpy.sin((2*k-1)*numpy.pi / 6.0),
+                ]).T
 
-        B0 = 251.0 / 2304.0
-        B1 = (110297.0 + 5713.0*numpy.sqrt(111.0)) / 2045952.0
-        B2 = (110297.0 - 5713.0*numpy.sqrt(111.0)) / 2045952.0
-        C = 125.0 / 3072.0
+            B0 = 251.0 / 2304.0
+            B1 = (110297.0 + 5713.0*numpy.sqrt(111.0)) / 2045952.0
+            B2 = (110297.0 - 5713.0*numpy.sqrt(111.0)) / 2045952.0
+            C = 125.0 / 3072.0
 
-        data = [
-            (B0, z(2)),
-            (B1, rho1 * x),
-            (B2, rho2 * x),
-            (C, uv),
-            ]
+            data = [
+                (B0, z(2)),
+                (B1, rho1 * x),
+                (B2, rho2 * x),
+                (C, uv),
+                ]
+        else:
+            assert index == 2
+            r1 = 0.326655862701
+            r2 = 0.720984642976
+            r3 = 0.979798373636
+
+            B1 = 0.0655791415454
+            B2 = 0.0490399916287
+            B3 = 0.0104912371962
+
+            s1 = numpy.sqrt((125.0 - 10.0*numpy.sqrt(10.0)) / 366.0)
+            s2 = numpy.sqrt((125.0 + 10.0*numpy.sqrt(19.0)) / 366.0)
+
+            C1 = (7494893.0 + 1053263.0*numpy.sqrt(19)) / 205200000.0
+            C2 = (7494893.0 - 1053263.0*numpy.sqrt(19)) / 205200000.0
+            D = 81.0 / 3125.0
+
+            u = numpy.sqrt(5.0/6.0) * numpy.cos(numpy.pi/8.0)
+            v = numpy.sqrt(5.0/6.0) * numpy.sin(numpy.pi/8.0)
+
+            data = [
+                (B1, fsd(2, r1)),
+                (B2, fsd(2, r2)),
+                (B3, fsd(2, r3)),
+                (C1, pm(2, s1)),
+                (C2, pm(2, s2)),
+                (D,
+                ]
+
         self.points, self.weights = untangle(data)
         self.weights *= numpy.pi
         return
--- tools/import_modepy.py
+++ tools/import_modepy.py
@@ -1,25 +1,9 @@
+# -*- coding: utf-8 -*-
+#
 import modepy
 import numpy
 
-
-def _get_symmetry_code_tri(pts):
-    if len(pts) == 1:
-        return '_s3()'
-    elif len(pts) == 3:
-        # Symmetry group [[a, a, b], [a, b, a], [b, a, a]].
-        # Find the equal value `a`.
-        tol = 1.0e-12
-        beta = pts[0] - pts[0][0]
-        ct = numpy.count_nonzero(abs(beta) < tol)
-        assert ct in [1, 2], beta
-        val = pts[0][0] if ct == 2 else pts[0][1]
-        return '_s21({:.15e})'.format(val)
-
-    # Symmetry group [[a, b, c], [c, a, b], ...].
-    assert len(pts) == 6
-    # Take the two largest value from a, b, c.
-    pt0 = numpy.sort(pts[0])
-    return '_s111({:.15e}, {:.15e})'.format(pt0[2], pt0[1])
+from . import import_helpers
 
 
 def _import_tri():
@@ -56,7 +40,7 @@ def _import_tri():
                 kk += count
                 print(
                     8*' ' + '(%.15e, %s),' %
-                    (last_value, _get_symmetry_code_tri(pts))
+                    (last_value, import_helpers.get_symmetry_code_tri(pts))
                     )
                 last_value = w
                 count = 1
@@ -64,46 +48,11 @@ def _import_tri():
         pts = sorted_points[kk:kk+count]
         print(
             8*' ' + '(%.15e, %s),' %
-            (last_value, _get_symmetry_code_tri(pts))
+            (last_value, import_helpers.get_symmetry_code_tri(pts))
             )
         print(8*' ' + ']')
 
     return
-
-
-def _get_symmetry_code_tet(pts):
-    if len(pts) == 1:
-        return '_s4()'
-    elif len(pts) == 4:
-        # Symmetry group [[a, a, a, b], [a, a, b, a], ...].
-        # Find the equal value `a`.
-        tol = 1.0e-12
-        beta = pts[0] - pts[0][0]
-        ct = numpy.count_nonzero(abs(beta) < tol)
-        if ct == 1:
-            return '_s31({:.15e})'.format(pts[0][1])
-        else:
-            assert ct == 3
-            return '_s31({:.15e})'.format(pts[0][0])
-    elif len(pts) == 6:
-        # Symmetry group [[a, a, b, b], [a, b, b, a], ...].
-        return '_s22({:.15e})'.format(pts[0][0])
-    elif len(pts) == 12:
-        # Symmetry group [[a, a, b, c], [a, c, a, b], ...].
-        # Find the double.
-        pt0 = numpy.sort(pts[0])
-        tol = 1.0e-12
-        for k in range(4):
-            beta = pt0 - pt0[k]
-            ct = numpy.count_nonzero(abs(beta) < tol)
-            if ct == 2:
-                return '_s211({:.15e}, {:.15e})'.format(pt0[k], pt0[k-1])
-
-    assert len(pts) == 24
-    pt0 = numpy.sort(pts[0])
-    return '_s1111({:.15e}, {:.15e}, {:.15e})'.format(
-            pt0[-1], pt0[-2], pt0[-3]
-            )
 
 
 def _import_tet():
@@ -131,11 +80,11 @@ def _import_tet():
             if abs(last_value - w) < tol:
                 count += 1
             else:
-                pts = sorted_points[k:k+count]
+                pts = sorted_points[kk:kk+count]
                 kk += count
                 print(
                     8*' ' + '(%.15e, %s),'
-                    % (last_value, _get_symmetry_code_tet(pts))
+                    % (last_value, import_helpers.get_symmetry_code_tet(pts))
                     )
                 last_value = w
                 count = 1
@@ -143,7 +92,7 @@ def _import_tet():
         pts = sorted_points[kk:kk+count]
         print(
             8*' ' + '(%.15e, %s),'
-            % (last_value, _get_symmetry_code_tet(pts))
+            % (last_value, import_helpers.get_symmetry_code_tet(pts))
             )
         print(8*' ' + ']')
 
