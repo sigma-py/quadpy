49f514f709c3cac92ce2757736ea189d86c3eaaa
Nico Schlömer <nico.schloemer@gmail.com>
2017-08-15 00:42:10 +0200

    transform a few tet schemes to new layout

--- quadpy/tetrahedron/gatermann.py
+++ quadpy/tetrahedron/gatermann.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 #
-import numpy
-
 from .helpers import _s31, _s22
+
+from ..helpers import untangle
 
 
 class Gatermann(object):
@@ -16,16 +16,12 @@ class Gatermann(object):
     def __init__(self):
         self.name = 'Gatermann'
         self.degree = 5
-        self.weights = 6 * numpy.concatenate([
-            numpy.full(4, 9.73033316198362119165356216965707e-06),
-            numpy.full(4, 8.99031481668747219698547129902142e-03),
-            numpy.full(6, 2.17777476778781405656596945369837e-02),
-            ])
-        bary = numpy.concatenate([
-            _s31(0.656936552995394536166881327385593),
-            _s31(0.0801424420792727848879183805550907),
-            _s22(0.404475329343454044779549906725159),
-            ])
-
-        self.points = bary[:, 1:]
+        data = [
+            (9.73033316198362119165356216965707e-06, _s31(0.656936552995394536166881327385593)),
+            (8.99031481668747219698547129902142e-03, _s31(0.0801424420792727848879183805550907)),
+            (2.17777476778781405656596945369837e-02, _s22(0.404475329343454044779549906725159)),
+            ]
+        self.bary, self.weights = untangle(data)
+        self.weights *= 6
+        self.points = self.bary[:, 1:]
         return
