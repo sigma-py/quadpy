2bea8b334c5c21448b3887465b32352770bab4fe
Nico Schlömer <nico.schloemer@gmail.com>
2017-08-14 20:11:48 +0200

    rename Peirce -> Peirce1957

--- quadpy/disk/__init__.py
+++ quadpy/disk/__init__.py
@@ -8,7 +8,7 @@ from .cools_kim import *
 from .hammer_stroud import *
 from .lether import *
 from .mysovskih import *
-from .peirce import *
+from .peirce1957 import *
 from .stroud import *
 from .wissmann_becker import *
 
--- test/test_disk.py
+++ test/test_disk.py
@@ -12,7 +12,7 @@ from helpers import check_degree
     [(quadpy.disk.CoolsHaegemans(k), 1.0e-14) for k in range(1, 4)]
     + [(quadpy.disk.CoolsKim(k), 1.0e-14) for k in range(1, 4)]
     + [(quadpy.disk.Lether(k), 1.0e-14) for k in range(1, 6)]
-    + [(quadpy.disk.Peirce(k), 1.0e-14) for k in range(1, 6)]
+    + [(quadpy.disk.Peirce1957(k), 1.0e-14) for k in range(1, 6)]
     + [(quadpy.disk.Stroud(k), 1.0e-14) for k in [
         'S2 3-1', 'S2 3-2',
         'S2 4-1',
