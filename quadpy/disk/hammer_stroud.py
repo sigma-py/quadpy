3353c58a32295333f82b1e85d425c069494353c9
Nico Schlömer <nico.schloemer@gmail.com>
2017-08-15 17:06:17 +0200

    fix hammer-stroud for disk

--- quadpy/disk/hammer_stroud.py
+++ quadpy/disk/hammer_stroud.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-from math import pi
-import warnings
+from math import pi, sqrt
 
 from ..helpers import untangle, fsd
 
@@ -13,13 +12,16 @@ class HammerStroud(object):
     Math. Comp. 12 (1958), 272-280,
     <https://doi.org/10.1090/S0025-5718-1958-0102176-6>.
     '''
-    def __init__(self):
-        self.name = 'HammerStroud'
-        warnings.warn('Formula {} only has degree 1, not 3!'.format(self.name))
-        self.degree = 1
-        data = [
-            (0.25, fsd(2, 0.5, 1)),
-            ]
+    def __init__(self, index):
+        if index == '11-2':
+            self.degree = 3
+            # ERR Wrongly stated in Stroud with 0.5 instead of sqrt(0.5)
+            data = [
+                (0.25, fsd(2, sqrt(0.5), 1)),
+                ]
+        else:
+            assert False
+
         self.points, self.weights = untangle(data)
         self.weights *= pi
         return
