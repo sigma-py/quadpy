5d995ea27486354e09676872d7a4e842b56f2ba1
Nico Schlömer <nico.schloemer@gmail.com>
2017-08-16 16:40:51 +0200

    more stroud disk schemes

--- README.md
+++ README.md
@@ -186,7 +186,7 @@ val = quadpy.triangle.integrate(
  * via [Stroud](https://books.google.de/books/about/Approximate_calculation_of_multiple_inte.html?id=L_tQAAAAMAAJ&redir_esc=y):
    - [Radon](https://eudml.org/doc/176796) (1948, degree 5)
    - [Peirce](https://books.google.de/books/about/Numerical_integration_over_planar_region.html?id=WR9SAAAAMAAJ&redir_esc=y)
-     (1956, degree 7)
+     (1956, 2 schemes up to degree 9)
    - [Albrecht-Collatz](https://dx.doi.org/10.1002/zamm.19580380102) (1958, degree 3)
    - [Hammer-Stroud](https://doi.org/10.1090/S0025-5718-1958-0102176-6) (1958, 8 schemes up to degree 15)
    - [Albrecht](https://dx.doi.org/10.1002/zamm.19600401014) (1960, 2 schemes up to degree 11)
--- quadpy/disk/peirce1956.py
+++ quadpy/disk/peirce1956.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 #
 from __future__ import division
-import math
+from math import sqrt, cos, sin, pi
 
-from ..helpers import fsd, pm, untangle
+from ..helpers import fsd, pm, untangle, fs_array
 
 
 class Peirce1956(object):
@@ -13,27 +13,56 @@ class Peirce1956(object):
     PhD thesis, University of Wisconsin--Madison, 1956,
     <https://books.google.de/books/about/Numerical_integration_over_planar_region.html?id=WR9SAAAAMAAJ&redir_esc=y>.
 
-    Also: Formula 13-2 in Hammer-Stroud.
     '''
-    def __init__(self):
-        self.degree = 7
+    def __init__(self, index):
 
-        sqrt29 = math.sqrt(29.0)
-        r = math.sqrt(0.75)
-        s = math.sqrt((27 - 3 * sqrt29)/104.0)
-        t = math.sqrt((27 + 3 * sqrt29)/104.0)
+        if index == 1:
+            # Also: Formula 13-2 in Hammer-Stroud.
+            self.degree = 7
 
-        B1 = 2.0/27.0
-        # ERR Stroud falsely lists 4 instead of 41 here.
-        B2 = (551.0 + 41*sqrt29) / 6264.0
-        B3 = (551.0 - 41*sqrt29) / 6264.0
+            sqrt29 = sqrt(29.0)
+            r = sqrt(0.75)
+            s = sqrt((27 - 3 * sqrt29)/104.0)
+            t = sqrt((27 + 3 * sqrt29)/104.0)
 
-        data = [
-            (B1, fsd(2, r, 1)),
-            (B2, pm(2, s)),
-            (B3, pm(2, t)),
-            ]
+            B1 = 2.0/27.0
+            # ERR Stroud falsely lists 4 instead of 41 here.
+            B2 = (551.0 + 41*sqrt29) / 6264.0
+            B3 = (551.0 - 41*sqrt29) / 6264.0
+
+            data = [
+                (B1, fsd(2, r, 1)),
+                (B2, pm(2, s)),
+                (B3, pm(2, t)),
+                ]
+        else:
+            assert index == 2
+            self.degree = 9
+
+            sqrt15 = sqrt(15.0)
+            cos_pi8 = cos(pi/8)
+            sin_pi8 = sin(pi/8)
+
+            r = sqrt((5 + sqrt15) / 10.0)
+            u1 = sqrt((5.0 - sqrt15)/10.0) * cos_pi8
+            v1 = sqrt((5.0 - sqrt15)/10.0) * sin_pi8
+            u2 = sqrt(0.5) * cos_pi8
+            v2 = sqrt(0.5) * sin_pi8
+            u3 = sqrt((5 + sqrt15 + sqrt(185.0*sqrt15 - 700.0)) / 20.0)
+            v3 = sqrt((5 + sqrt15 - sqrt(185.0*sqrt15 - 700.0)) / 20.0)
+
+            B1 = (12060.0 - 1440.0*sqrt15) / 254088.0
+            B2 = 5.0 / 144.0
+            B3 = 1.0/18.0
+            B4 = (5585.0 + 1440.0*sqrt15) / 508176.0
+
+            data = [
+                (B1, fsd(2, r, 1)),
+                (B2, fs_array([u1, v1])),
+                (B3, fs_array([u2, v2])),
+                (B4, fs_array([u3, v3])),
+                ]
 
         self.points, self.weights = untangle(data)
-        self.weights *= math.pi
+        self.weights *= pi
         return
