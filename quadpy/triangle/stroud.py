807a225f6360d91d15b72e05af72a544cdf3d051
Nico Schlömer <nico.schloemer@gmail.com>
2017-08-24 11:18:48 +0200

    correct T3 schemes from Stroud

--- quadpy/triangle/stroud.py
+++ quadpy/triangle/stroud.py
@@ -50,11 +50,11 @@ class Stroud(object):
             self.bary, self.weights = untangle(data)
             self.points = self.bary[1:]
 
-        self.points = self.bary[:, 1:]
         return
 
     def set_data(self, scheme):
         self.degree = scheme.degree
         self.weights = scheme.weights
+        self.points = scheme.points
         self.bary = scheme.bary
         return
