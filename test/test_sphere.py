5a250f30871cc58190e8e058d3f1698101448e5e
Nico Schlömer <nico.schloemer@gmail.com>
2017-08-28 13:01:00 +0200

    improve sphere tests even more

--- test/test_sphere.py
+++ test/test_sphere.py
@@ -3,10 +3,21 @@
 import numpy
 import pytest
 import quadpy
-from quadpy.nball.helpers import integrate_monomial_over_unit_nsphere
 import specialpy
 
-from helpers import check_degree
+# Note
+# ====
+# Instead of testing exact integration against of all monomials of degree at
+# most l, one can instead test exact integration of all _spherical harmonics_
+# of degree at most l. While there are 2**l monomials, there are only l**2
+# spherical harmonics.
+
+
+def _cartesian_to_spherical(X):
+    return numpy.stack([
+        numpy.arctan2(X[:, 1], X[:, 0]),
+        numpy.arccos(X[:, 2])
+        ], axis=1)
 
 
 @pytest.mark.parametrize(
@@ -25,30 +36,48 @@ from helpers import check_degree
         'U3 14-1',
         ]]
     )
-def test_scheme(scheme):
-    # Test integration until we get to a polynomial degree `d` that can no
-    # longer be integrated exactly. The scheme's degree is `d-1`.
-    midpoint = numpy.array([0.0, 0.0, 0.0])
-    radius = 1.0
-    degree = check_degree(
-            lambda poly: quadpy.sphere.integrate(
-                poly, midpoint, radius, scheme, sumfun=numpy.sum
-                ),
-            integrate_monomial_over_unit_nsphere,
-            lambda n: quadpy.helpers.partition(n, 3),
-            min(30, scheme.degree + 1)
+def test_scheme_cartesian(scheme, tol=1.0e-11):
+    exact_val = numpy.zeros(scheme.degree + 1)
+    exact_val[0] = numpy.sqrt(4*numpy.pi)
+
+    def sph_tree_cartesian(x):
+        print(x.shape)
+        phi_theta = _cartesian_to_spherical(x.T).T
+        return specialpy.sph_tree(
+            scheme.degree+1, phi_theta[1], phi_theta[0]
             )
-    assert degree >= min(30, scheme.degree), \
+
+    vals = quadpy.sphere.integrate(
+        sph_tree_cartesian,
+        center=numpy.array([0.0, 0.0, 0.0]),
+        radius=1.0, rule=scheme, sumfun=numpy.sum
+        )
+
+    exact = numpy.zeros((scheme.degree+2, scheme.degree+2))
+    exact[0, 0] = numpy.sqrt(4 * numpy.pi)
+
+    # The exact value is sqrt(4*pi) for the Y_0^0, and 0 otherwise.
+    err = vals
+    err[0, 0] -= numpy.sqrt(4.0 * numpy.pi)
+
+    # check in which level the first significant errors occur
+    first_error_level = numpy.min(
+        numpy.max(numpy.vstack(numpy.where(abs(err) > tol)), axis=0)
+        )
+
+    degree = first_error_level - 1
+
+    assert degree == scheme.degree, \
         'Observed: {}, expected: {}'.format(degree, scheme.degree)
     return
 
 
+# Test a few schemes with integrate_spherical. -- This is basically the same as
+# above, no need to repeat it all in detail.
 @pytest.mark.parametrize(
     'scheme',
     [quadpy.sphere.Lebedev(degree) for degree in [
-        3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 35, 41, 47, 53,
-        # TODO speed up
-        # 59, 65, 71, 77, 83, 89, 95, 101, 107, 113, 119, 125, 131
+        3, 5, 7, 9, 11, 13, 15, 17, 19,
         ]]
     + [quadpy.sphere.Stroud(k) for k in [
         'U3 3-1',
@@ -60,10 +89,6 @@ def test_scheme(scheme):
         'U3 14-1',
         ]]
     )
-# Instead of testing exact integration against of all monomials of degree at
-# most l, one can instead test exact integration of all _spherical harmonics_
-# of degree at most l. While there are 2**l monomials, there are only l**2
-# spherical harmonics.
 def test_scheme_spherical(scheme, tol=1.0e-11):
     exact_val = numpy.zeros(scheme.degree + 1)
     exact_val[0] = numpy.sqrt(4*numpy.pi)
@@ -107,5 +132,5 @@ if __name__ == '__main__':
     # scheme_ = quadpy.sphere.Stroud('U3 11-3')
     scheme_ = quadpy.sphere.Lebedev(131)
     # test_scheme(scheme_)
-    test_scheme_spherical(scheme_)
+    test_scheme_cartesian(scheme_)
     # test_show(scheme_)
