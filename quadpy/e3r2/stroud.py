7caeff08da871bf3f5c642ac0b017d5ca93c4202
Nico Schlömer <nico.schloemer@gmail.com>
2017-08-29 14:57:42 +0200

    all schemes from Stroud/Secrest for E3r2

--- quadpy/e3r2/stroud.py
+++ quadpy/e3r2/stroud.py
@@ -13,6 +13,20 @@ class Stroud(object):
     def __init__(self, index):
         if index == 'E3r2 5-1':
             self.set_data(StroudSecrest('VII'))
+        elif index == 'E3r2 5-2a':
+            self.set_data(StroudSecrest('VIIIa'))
+        elif index == 'E3r2 5-2b':
+            self.set_data(StroudSecrest('VIIIb'))
+        elif index == 'E3r2 5-3':
+            self.set_data(StroudSecrest('IX'))
+        elif index == 'E3r2 7-1a':
+            self.set_data(StroudSecrest('Xa'))
+        elif index == 'E3r2 7-1b':
+            self.set_data(StroudSecrest('Xb'))
+        elif index == 'E3r2 7-2a':
+            self.set_data(StroudSecrest('XIa'))
+        elif index == 'E3r2 7-2b':
+            self.set_data(StroudSecrest('XIb'))
         else:
             assert False
         return
