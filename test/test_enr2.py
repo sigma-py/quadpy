ba377615e41c8d8295e90203d6d64740aab281c8
Nico Schlömer <nico.schloemer@gmail.com>
2017-09-04 17:26:47 +0200

    test and correct Stenger's schemes

--- test/test_enr2.py
+++ test/test_enr2.py
@@ -14,12 +14,15 @@ from helpers import check_degree, integrate_monomial_over_enr2
         '5-1a',
         '5-2',
         ]]
-    + [(quadpy.enr2.Stroud(n, index), 1.0e-14) for n in [3, 5, 6]
-        for index in [
-        '5-1b',
-        ]]
+    + [(quadpy.enr2.Stroud(n, '5-1b'), 1.0e-14) for n in [3, 5, 6]]
     + [(quadpy.enr2.Stroud(n, '7-1a'), 1.0e-14) for n in [2, 3, 4, 6, 7]]
     + [(quadpy.enr2.Stroud(n, '7-1a'), 1.0e-14) for n in [3, 4]]
+    + [(quadpy.enr2.Stroud(n, index), 1.0e-14) for n in [3, 4, 5, 6]
+        for index in ['7-3b', '9-1a']]
+    + [(quadpy.enr2.Stroud(n, '7-3b'), 1.0e-14) for n in [3, 4, 6]]
+    + [(quadpy.enr2.Stroud(n, '9-1b'), 1.0e-14) for n in [4, 5, 6]]
+    + [(quadpy.enr2.Stroud(n, '11-1a'), 1.0e-14) for n in [3, 4]]
+    + [(quadpy.enr2.Stroud(n, '11-1b'), 1.0e-14) for n in [3, 4, 5]]
     + [(quadpy.enr2.StroudSecrest(n, 'II'), 1.0e-14) for n in range(2, 6)]
     )
 def test_scheme(scheme, tol):
@@ -39,5 +42,5 @@ def test_scheme(scheme, tol):
 if __name__ == '__main__':
     dim_ = 5
     # quadpy.e3r2.show(quadpy.enr2.Stroud(dim_, '5-1a'), backend='vtk')
-    scheme_ = quadpy.enr2.Stroud(dim_, '7-2')
+    scheme_ = quadpy.enr2.Stroud(dim_, '11-1b')
     test_scheme(scheme_, 1.0e-14)
