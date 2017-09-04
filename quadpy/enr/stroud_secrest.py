81e76ced34c767d5e18446317026cd01bf3fa218
Nico Schlömer <nico.schloemer@gmail.com>
2017-09-04 21:51:22 +0200

    more stroud-secrest schems for Enr

--- quadpy/enr/stroud_secrest.py
+++ quadpy/enr/stroud_secrest.py
@@ -4,7 +4,9 @@ from __future__ import division
 
 from math import sqrt, pi, lgamma, exp
 
-from ..helpers import untangle, fsd
+import numpy
+
+from ..helpers import untangle, fsd, pm
 from ..enr2.stroud_secrest import _nsimplex
 
 
@@ -28,8 +30,27 @@ class StroudSecrest(object):
             data = [
                 (1/(2*n), fsd(n, (nu, 1))),
                 ]
+        elif index == 'III':
+            self.degree = 3
+            nu = sqrt(n+1)
+            data = [
+                (1/2**n, pm(n, nu)),
+                ]
         else:
-            assert False
+            assert index == 'IV'
+            self.degree = 5
+
+            nu = sqrt((n+2) * (n+3))
+            xi = sqrt((n+2) * (n+3) / 2)
+            A = 2*(2*n+3) / (n+2) / (n+3)
+            B = (4-n)*(n+1) / 2 / (n+2)**2 / (n+3)
+            C = (n+1) / (n+2)**2 / (n+3)
+
+            data = [
+                (A, numpy.full((1, n), 0.0)),
+                (B, fsd(n, (nu, 1))),
+                (C, fsd(n, (xi, 2))),
+                ]
 
         self.points, self.weights = untangle(data)
         self.weights *= 2 * sqrt(pi)**n * exp(lgamma(n) - lgamma(n/2))
