4c6e23dab1ead36e78b8e9a2bf54c6bced2cd8e1
Nico Schlömer <nico.schloemer@gmail.com>
2017-09-04 21:51:22 +0200

    more stroud-secrest schems for Enr

--- test/test_enr.py
+++ test/test_enr.py
@@ -8,15 +8,15 @@ from helpers import check_degree, integrate_monomial_over_enr
 
 @pytest.mark.parametrize(
     'scheme,tol',
-    [(quadpy.enr2.StroudSecrest(n, index), 1.0e-14) for n in range(2, 6)
+    [(quadpy.enr.StroudSecrest(n, index), 1.0e-14) for n in range(2, 6)
         for index in [
-            'I', 'II',
+            'I', 'II', 'III', 'IV',
             ]]
     )
 def test_scheme(scheme, tol):
     n = scheme.dim
     degree = check_degree(
-            lambda poly: quadpy.enr2.integrate(poly, scheme),
+            lambda poly: quadpy.enr.integrate(poly, scheme),
             integrate_monomial_over_enr,
             lambda k: quadpy.helpers.partition(k, n),
             scheme.degree + 1,
@@ -28,7 +28,7 @@ def test_scheme(scheme, tol):
 
 
 if __name__ == '__main__':
-    dim_ = 5
-    # quadpy.e3r2.show(quadpy.enr2.Stroud(dim_, '5-1a'), backend='vtk')
-    scheme_ = quadpy.enr.StroudSecrest(dim_, 'II')
+    dim_ = 2
+    # quadpy.e3r2.show(quadpy.enr.Stroud(dim_, '5-1a'), backend='vtk')
+    scheme_ = quadpy.enr.StroudSecrest(dim_, 'I')
     test_scheme(scheme_, 1.0e-14)
