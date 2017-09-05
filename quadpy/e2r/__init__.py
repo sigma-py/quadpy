566b0f7415847989d39d5aa8e9c27c600e4342e8
Nico Schlömer <nico.schloemer@gmail.com>
2017-09-05 15:33:37 +0200

    add Stroud's E_2^r schemes

--- quadpy/e2r/__init__.py
+++ quadpy/e2r/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 from .rabinowitz_richter import RabinowitzRichter
+from .stroud import Stroud
 from .stroud_secrest import StroudSecrest
 
 # pylint: disable=wildcard-import
