d89fc91c08e7e722efff6c2f4a834e485236e361
Nico Schlömer <nico.schloemer@gmail.com>
2017-09-05 21:44:56 +0200

    increase test coverage

--- test/test_enr2.py
+++ test/test_enr2.py
@@ -11,15 +11,19 @@ from helpers import check_degree, integrate_monomial_over_enr2
     [(quadpy.enr2.Stroud(n, index), 1.0e-14) for n in range(2, 8)
         for index in [
         '3-1', '3-2',
-        '5-1a',
-        '5-2',
+        '5-1a', '5-2', '5-4',
+        '5-5a',
         ]]
+    + [(quadpy.enr2.Stroud(n, '5-3'), 1.0e-14) for n in range(3, 8)]
+    + [(quadpy.enr2.Stroud(n, '5-5b'), 1.0e-14) for n in [2]]
+    + [(quadpy.enr2.Stroud(n, '5-6'), 1.0e-14) for n in range(5, 8)]
     + [(quadpy.enr2.Stroud(n, '5-1b'), 1.0e-14) for n in [3, 5, 6]]
     + [(quadpy.enr2.Stroud(n, '7-1a'), 1.0e-14) for n in [2, 3, 4, 6, 7]]
-    + [(quadpy.enr2.Stroud(n, '7-1a'), 1.0e-14) for n in [3, 4]]
+    + [(quadpy.enr2.Stroud(n, '7-1b'), 1.0e-14) for n in [3, 4]]
+    + [(quadpy.enr2.Stroud(n, '7-2'), 1.0e-14) for n in [3, 4, 5, 6]]
     + [(quadpy.enr2.Stroud(n, index), 1.0e-14) for n in [3, 4, 5, 6]
         for index in ['7-3b', '9-1a']]
-    + [(quadpy.enr2.Stroud(n, '7-3b'), 1.0e-14) for n in [3, 4, 6]]
+    + [(quadpy.enr2.Stroud(n, '7-3a'), 1.0e-14) for n in [3, 4, 6]]
     + [(quadpy.enr2.Stroud(n, '9-1b'), 1.0e-14) for n in [4, 5, 6]]
     + [(quadpy.enr2.Stroud(n, '11-1a'), 1.0e-14) for n in [3, 4]]
     + [(quadpy.enr2.Stroud(n, '11-1b'), 1.0e-14) for n in [3, 4, 5]]
