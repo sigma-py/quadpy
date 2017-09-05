bbcd7941d1f93014cd6b3924d331797568d59ec8
Nico Schlömer <nico.schloemer@gmail.com>
2017-09-05 10:20:06 +0200

    reorganize stroud-secrest e3r

--- quadpy/e3r/stroud_secrest.py
+++ quadpy/e3r/stroud_secrest.py
@@ -18,97 +18,107 @@ class StroudSecrest(object):
     <https://doi.org/10.1090/S0025-5718-1963-0161473-0>.
     '''
     def __init__(self, index):
-        if index == 'VII':
-            self.degree = 5
-
-            p_m = numpy.array([+1, -1])
-
-            nu, xi = numpy.sqrt(15 - p_m * 3 * sqrt(5))
-            A = 3/5
-            B = 1/30
-
-            data = [
-                (A, numpy.array([[0.0, 0.0, 0.0]])),
-                (B, pm_roll(3, [xi, nu])),
-                ]
-        elif index == 'VIII':
-            self.degree = 5
-
-            nu = sqrt(30)
-            eta = sqrt(10)
-            A = 3/5
-            B = 2/75
-            C = 3/100
-
-            data = [
-                (A, numpy.array([[0.0, 0.0, 0.0]])),
-                (B, fsd(3, (nu, 1))),
-                (C, pm(3, eta)),
-                ]
-        elif index == 'IX':
-            self.degree = 5
-
-            p_m = numpy.array([+1, -1])
-
-            eta = sqrt(10)
-            xi, nu = numpy.sqrt(15 - p_m * 5 * sqrt(5))
-            A = 3/5
-            B = 1/50
-
-            data = [
-                (A, numpy.array([[0.0, 0.0, 0.0]])),
-                (B, pm(3, eta)),
-                (B, pm_roll(3, [xi, nu])),
-                ]
-        elif index == 'X':
-            # ERR
-            # TODO find out what's wrong
-            warnings.warn(
-                'Stroud-Secrest\'s scheme X for E_3^r has degree 3, not 7.'
-                )
-            self.degree = 3
-
-            sqrt130 = sqrt(130.0)
-
-            nu = sqrt((720 - 24*sqrt130) / 11)
-            xi = sqrt(288 + 24*sqrt130)
-            eta = sqrt((-216 + 24*sqrt130) / 7)
-            A = (5175 - 13*sqrt130) / 8820
-            B = (3870 + 283*sqrt130) / 493920
-            C = (3204 - 281*sqrt130) / 197568
-            D = (4239 + 373*sqrt130) / 197568
-
-            data = [
-                (A, numpy.array([[0.0, 0.0, 0.0]])),
-                (B, fsd(3, (nu, 1))),
-                (C, fsd(3, (xi, 2))),
-                (D, pm(3, eta)),
-                ]
-        else:
-            assert index == 'XI'
-            self.degree = 7
-
-            p_m = numpy.array([+1, -1])
-            sqrt5 = sqrt(5.0)
-            sqrt39 = sqrt(39.0)
-            sqrt195 = sqrt(195.0)
-
-            nu, xi = numpy.sqrt(-50 + p_m*10*sqrt5 + 10*sqrt39 - p_m*2*sqrt195)
-            eta = sqrt(36 + 4*sqrt39)
-            mu, lmbda = numpy.sqrt(
-                54 + p_m * 18*sqrt5 + 6*sqrt39 + p_m * 2*sqrt195
-                )
-            A = (1725 - 26*sqrt39) / 2940
-            B = (1065 + 171*sqrt39) / 54880
-            C = (297 - 47*sqrt39) / 32928
-
-            data = [
-                (A, numpy.array([[0.0, 0.0, 0.0]])),
-                (B, pm_roll(3, [xi, nu])),
-                (C, pm(3, eta)),
-                (C, pm_roll(3, [lmbda, mu])),
-                ]
-
+        gen = {
+            'VII': _vii,
+            'VIII': _viii,
+            'IX': _ix,
+            'X': _x,
+            'XI': _xi,
+            }
+        self.degree, data = gen[index]()
         self.points, self.weights = untangle(data)
         self.weights *= 8 * pi
         return
+
+
+def _vii():
+    p_m = numpy.array([+1, -1])
+
+    nu, xi = numpy.sqrt(15 - p_m * 3 * sqrt(5))
+    A = 3/5
+    B = 1/30
+
+    data = [
+        (A, numpy.array([[0.0, 0.0, 0.0]])),
+        (B, pm_roll(3, [xi, nu])),
+        ]
+    return 5, data
+
+
+def _viii():
+    nu = sqrt(30)
+    eta = sqrt(10)
+    A = 3/5
+    B = 2/75
+    C = 3/100
+
+    data = [
+        (A, numpy.array([[0.0, 0.0, 0.0]])),
+        (B, fsd(3, (nu, 1))),
+        (C, pm(3, eta)),
+        ]
+    return 5, data
+
+
+def _ix():
+    p_m = numpy.array([+1, -1])
+
+    eta = sqrt(10)
+    xi, nu = numpy.sqrt(15 - p_m * 5 * sqrt(5))
+    A = 3/5
+    B = 1/50
+
+    data = [
+        (A, numpy.array([[0.0, 0.0, 0.0]])),
+        (B, pm(3, eta)),
+        (B, pm_roll(3, [xi, nu])),
+        ]
+    return 5, data
+
+
+def _x():
+    sqrt130 = sqrt(130.0)
+
+    nu = sqrt((720 - 24*sqrt130) / 11)
+    xi = sqrt(288 + 24*sqrt130)
+    eta = sqrt((-216 + 24*sqrt130) / 7)
+    A = (5175 - 13*sqrt130) / 8820
+    B = (3870 + 283*sqrt130) / 493920
+    C = (3204 - 281*sqrt130) / 197568
+    D = (4239 + 373*sqrt130) / 197568
+
+    data = [
+        (A, numpy.array([[0.0, 0.0, 0.0]])),
+        (B, fsd(3, (nu, 1))),
+        (C, fsd(3, (xi, 2))),
+        (D, pm(3, eta)),
+        ]
+
+    # ERR
+    # TODO find out what's wrong
+    warnings.warn('Stroud-Secrest\'s scheme X for E_3^r has degree 3, not 7.')
+    return 3, data
+
+
+def _xi():
+    p_m = numpy.array([+1, -1])
+    sqrt5 = sqrt(5.0)
+    sqrt39 = sqrt(39.0)
+    sqrt195 = sqrt(195.0)
+
+    nu, xi = numpy.sqrt(-50 + p_m*10*sqrt5 + 10*sqrt39 - p_m*2*sqrt195)
+    eta = sqrt(36 + 4*sqrt39)
+    mu, lmbda = numpy.sqrt(
+        54 + p_m * 18*sqrt5 + 6*sqrt39 + p_m * 2*sqrt195
+        )
+    A = (1725 - 26*sqrt39) / 2940
+    B = (1065 + 171*sqrt39) / 54880
+    C = (297 - 47*sqrt39) / 32928
+
+    data = [
+        (A, numpy.array([[0.0, 0.0, 0.0]])),
+        (B, pm_roll(3, [xi, nu])),
+        (C, pm(3, eta)),
+        (C, pm_roll(3, [lmbda, mu])),
+        ]
+    return 7, data
