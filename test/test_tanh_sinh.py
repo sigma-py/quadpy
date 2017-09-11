b3f3285b01e7462600e50ab46245e550874a803c
Nico Schlömer <nico.schloemer@gmail.com>
2017-09-11 18:18:06 +0200

    tanh-sinh: support for arbitrary intervals

--- test/test_tanh_sinh.py
+++ test/test_tanh_sinh.py
@@ -1,28 +1,34 @@
 # -*- coding: utf-8 -*-
 #
+import pytest
 import quadpy
 
 
-def test_tanh_sinh():
+@pytest.mark.parametrize(
+    'f, f_derivatives, a, b, exact',
     # test problem 1 from
     # <http://crd-legacy.lbl.gov/~dhbailey/dhbpapers/quadrature.pdf>
-    def f(t):
-        # return t**2
-        return 1
-        # return t * mpmath.log(1 + t)
+    [(lambda t: 1, {1: lambda t: 0, 2: lambda t: 0}, -1, +1, 2)]
+    + [(lambda t: 1, {1: lambda t: 0, 2: lambda t: 0}, 0, +1, 1)]
+    )
+def test_tanh_sinh(f, f_derivatives, a, b, exact):
+    # test fine error estimate
+    tol = 1.0e-50
+    value, error_estimate = quadpy.line_segment.tanh_sinh_quadrature(
+                f, a, b, tol,
+                f_derivatives=f_derivatives
+                )
+    assert abs(value - exact) < tol
 
+    # test crude error estimate
     tol = 1.0e-50
     value, error_estimate = \
-        quadpy.line_segment.tanh_sinh_quadrature(
-                f, -1, +1, tol,
-                # f_derivatives={
-                #     1: lambda t: 0,
-                #     2: lambda t: 0,
-                #     }
-                )
-    assert abs(value - 2) < tol
+        quadpy.line_segment.tanh_sinh_quadrature(f, a, b, tol)
+    assert abs(value - exact) < tol
     return
 
 
 if __name__ == '__main__':
-    test_tanh_sinh()
+    test_tanh_sinh(
+        lambda t: 1, {1: lambda t: 0, 2: lambda t: 0}, 2
+        )
