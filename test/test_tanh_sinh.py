335d624408112bea920ab3d66e22468b7ae67c6c
Nico Schlömer <nico.schloemer@gmail.com>
2017-09-12 08:34:04 +0200

    more pylint fixes

--- test/test_tanh_sinh.py
+++ test/test_tanh_sinh.py
@@ -36,11 +36,17 @@ import sympy
         1: lambda t: t/(t+1) + mp.log(t+1),
         2: lambda t: (t+2) / (t+1)**2,
         }, 0, +1, sympy.Rational(1, 4))]
+    # Test problem 2:
+    # + [({
+    #     0: lambda t: t**2 * mp.atan(t),
+    #     1: lambda t: t * (t/(t**2+1) + 2*mp.atan(t)),
+    #     2: lambda t: 2 * (t * (t**2+2) / (t**2+1)**2 + mp.atan(t)),
+    #     }, 0, +1, (sympy.pi - 2 + 2*sympy.log(2))/12)]
     )
 def test_tanh_sinh(f, a, b, exact):
     # test fine error estimate
     tol = 1.0e-50
-    value, error_estimate = quadpy.line_segment.tanh_sinh_quadrature(
+    value, _ = quadpy.line_segment.tanh_sinh_quadrature(
                 f[0], a, b, tol,
                 f_derivatives={1: f[1], 2: f[2]}
                 )
@@ -55,41 +61,10 @@ def test_tanh_sinh(f, a, b, exact):
 
 
 if __name__ == '__main__':
-    # test_tanh_sinh(
-    #     {
-    #         0: lambda t: 1,
-    #         1: lambda t: 0,
-    #         2: lambda t: 0
-    #     }, -0, +1, 1
-    #     )
-    # test_tanh_sinh(
-    #     {
-    #         0: lambda t: (t+1)/2,
-    #         1: lambda t: sympy.Rational(1)/2,
-    #         2: lambda t: 0,
-    #     },
-    #     -1, +1, 1
-    #     )
-    # test_tanh_sinh(
-    #     {
-    #         0: lambda t: t,
-    #         1: lambda t: 1,
-    #         2: lambda t: 0,
-    #     },
-    #     0, +1, sympy.Rational(1, 2)
-    #     )
-    # test_tanh_sinh(
-    #     {
-    #         0: lambda t: t**2,
-    #         1: lambda t: 2*t,
-    #         2: lambda t: 2,
-    #     },
-    #     -1, +1, sympy.Rational(2, 3)
-    #     )
     test_tanh_sinh(
         {
-            0: lambda t: t * mp.log(1+t),
-            1: lambda t: t/(t+1) + mp.log(t+1),
-            2: lambda t: (t+2) / (t+1)**2,
-        }, 0, +1, 0.25
+            0: lambda t: t**2 * mp.atan(t),
+            1: lambda t: t * (t/(t**2+1) + mp.atan(t)),
+            2: lambda t: 2 * (t * (t**2+2) / (t**2+1)**2 + mp.atan(t)),
+        }, 0, +1, (sympy.pi - 2 + 2*sympy.log(2))/12
         )
