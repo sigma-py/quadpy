b883e640dbbeb23100236641299ee593aa8d60d9
Nico Schlömer <nico.schloemer@gmail.com>
2017-09-20 00:55:27 +0200

    tanh-sinh: further simplification

--- quadpy/line_segment/__init__.py
+++ quadpy/line_segment/__init__.py
@@ -12,7 +12,7 @@ from .midpoint import Midpoint
 from .newton_cotes import NewtonCotesOpen, NewtonCotesClosed
 from .trapezoidal import Trapezoidal
 
-from .tanh_sinh import tanh_sinh_regular, tanh_sinh0
+from .tanh_sinh import tanh_sinh
 
 # import pylint: disable=wildcard-import
 from .tools import *
