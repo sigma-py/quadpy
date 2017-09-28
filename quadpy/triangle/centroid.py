14fcd3f05870225bbe0e091196fa9a2cf619f36f
Nico Schlömer <nico.schloemer@gmail.com>
2017-09-28 15:57:20 +0200

    more symbolic values in triangles

--- quadpy/triangle/centroid.py
+++ quadpy/triangle/centroid.py
@@ -6,7 +6,7 @@ from .helpers import _s3
 
 class Centroid(object):
     def __init__(self):
-        self.weights = numpy.array([1.0])
+        self.weights = numpy.array([1])
         self.bary = _s3()
         self.points = self.bary[:, 1:]
         self.degree = 1
