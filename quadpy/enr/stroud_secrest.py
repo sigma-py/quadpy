0ab97b6cdf7008954995df2f548486168383e52a
Nico Schlömer <nico.schloemer@gmail.com>
2017-09-29 19:39:47 +0200

    sympyfy enr

--- quadpy/enr/stroud_secrest.py
+++ quadpy/enr/stroud_secrest.py
@@ -8,7 +8,7 @@ Math. Comp. 17 (1963), 105-135,
 '''
 from __future__ import division
 
-from math import sqrt, pi, lgamma, exp
+from sympy import sqrt, pi, gamma, Rational as fr
 
 import numpy
 
@@ -18,7 +18,7 @@ from ..enr2.stroud_secrest import _nsimplex
 
 def i(n):
     data = [
-        (1/(n+1), sqrt(n+1) * _nsimplex(n))
+        (fr(1, n+1), sqrt(n+1) * _nsimplex(n))
         ]
     return 2, data
 
@@ -26,7 +26,7 @@ def i(n):
 def ii(n):
     nu = sqrt(n * (n+1))
     data = [
-        (1/(2*n), fsd(n, (nu, 1))),
+        (fr(1, 2*n), fsd(n, (nu, 1))),
         ]
     return 3, data
 
@@ -34,20 +34,20 @@ def ii(n):
 def iii(n):
     nu = sqrt(n+1)
     data = [
-        (1/2**n, pm(n, nu)),
+        (fr(1, 2**n), pm(n, nu)),
         ]
     return 3, data
 
 
 def iv(n):
     nu = sqrt((n+2) * (n+3))
-    xi = sqrt((n+2) * (n+3) / 2)
-    A = 2*(2*n+3) / (n+2) / (n+3)
-    B = (4-n)*(n+1) / 2 / (n+2)**2 / (n+3)
-    C = (n+1) / (n+2)**2 / (n+3)
+    xi = sqrt(fr((n+2) * (n+3), 2))
+    A = fr(2*(2*n+3), (n+2) * (n+3))
+    B = fr((4-n)*(n+1), 2 * (n+2)**2 * (n+3))
+    C = fr(n+1, (n+2)**2 * (n+3))
 
     data = [
-        (A, numpy.full((1, n), 0.0)),
+        (A, numpy.full((1, n), 0)),
         (B, fsd(n, (nu, 1))),
         (C, fsd(n, (xi, 2))),
         ]
@@ -69,5 +69,5 @@ class StroudSecrest(object):
         self.dim = n
         self.degree, data = _gen[key](n)
         self.points, self.weights = untangle(data)
-        self.weights *= 2 * sqrt(pi)**n * exp(lgamma(n) - lgamma(n/2))
+        self.weights *= 2 * sqrt(pi)**n * gamma(n) / gamma(fr(n, 2))
         return
