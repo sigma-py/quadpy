8169ead6aac60af3449c04df0eef1b8b0b47e11a
Nico Schlömer <nico.schloemer@gmail.com>
2017-09-29 15:53:52 +0200

    sympyfication of some spherical schemes

--- quadpy/sphere/helpers.py
+++ quadpy/sphere/helpers.py
@@ -1,10 +1,19 @@
 # -*- coding: utf-8 -*-
 #
 import numpy
+import sympy
 
 
 def cartesian_to_spherical(X):
     return numpy.stack([
         numpy.arctan2(X[:, 1], X[:, 0]),
         numpy.arccos(X[:, 2])
+        ], axis=1)
+
+
+def cartesian_to_spherical_sympy(X):
+    vacos = numpy.vectorize(sympy.acos)
+    return numpy.stack([
+        [sympy.atan2(X[k, 1], X[k, 0]) for k in range(len(X))],
+        vacos(X[:, 2])
         ], axis=1)
