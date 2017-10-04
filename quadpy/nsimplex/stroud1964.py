ebc065949816b83b13609dd5df2cf154e85c2e56
Nico Schlömer <nico.schloemer@gmail.com>
2017-10-04 14:54:39 +0200

    fix n-simplex scheme by stroud (1964)

--- quadpy/nsimplex/stroud1964.py
+++ quadpy/nsimplex/stroud1964.py
@@ -2,9 +2,9 @@
 #
 from __future__ import division
 
-import warnings
-
 import numpy
+from mpmath import mp
+from sympy import Rational as fr
 
 from ..helpers import untangle, rd
 
@@ -18,34 +18,46 @@ class Stroud1964(object):
     '''
     def __init__(self, n, variant):
         assert variant in ['a', 'b']
+
         self.dim = n
-        self.degree = 0
+        self.degree = 3
 
-        # TODO find out what's wrong
-        warnings.warn('Stroud\'s scheme has degree 0, not 3.')
+        if n == 2:
+            # The roots sum up to 1
+            r, s, t = mp.polyroots([1, -1, fr(1, 4), -fr(1, 60)])
+            data = [
+                (fr(1, n*(n+1)), rd(n+1, [(r, 1), (s, 1), (t, 1)])),
+                ]
+        else:
+            assert n > 2
 
-        # Stroud's book only gives numerical values for certain n, the article
-        # explains it in more detail.
-        # r is a root of a polynomial of degree 3.
-        p = numpy.array([
-            n+1, -3, 3/(n+2), -1/(n+2)/(n+3)
-            ])
-        roots = numpy.sort(numpy.roots(p))
-        # all roots are real-valued
-        if n > 8:
-            assert variant == 'b', 'Choose variant b for n >= 9.'
+            # Stroud's book only gives numerical values for certain n, the
+            # article explains it in more detail.
+            # r is a root of a polynomial of degree 3.
+            p = [n+1, -3, fr(3, n+2), -fr(1, (n+2)*(n+3))]
 
-        r = roots[0] if variant == 'a' else roots[1]
+            # mpmath polyroots takes really long, unfortunately.
+            # from mpmath import mp
+            # roots = mp.polyroots(p)
 
-        # s and t are zeros of a polynomial of degree 2
-        p = numpy.array([
-            1, -(1-(n-1)*r), n/2/(n+2) - (n-1)*r + n*(n-1)/2*r**2
-            ])
-        s, t = numpy.sort(numpy.roots(p))
+            roots = numpy.sort(numpy.roots(p))
+            # all roots are real-valued
+            if n > 8:
+                assert variant == 'b', 'Choose variant b for n >= 9.'
 
-        data = [
-            (1/n/(n+1), rd(n+1, [(r, 1), (s, 1), (t, n-1)])),
-            ]
+            r = roots[0] if variant == 'a' else roots[1]
+
+            # s and t are zeros of a polynomial of degree 2
+            # b = 1 - (n-1) * r
+            p = [
+                1, -(1-(n-1)*r), fr(n, 2*(n+2)) - (n-1)*r + fr(n*(n-1), 2)*r**2
+                ]
+            s, t = numpy.sort(numpy.roots(p))
+
+            # TODO check what's given wrong in Stroud's book
+            data = [
+                (fr(1, n*(n+1)), rd(n+1, [(r, n-1), (s, 1), (t, 1)])),
+                ]
 
         self.bary, self.weights = untangle(data)
         self.points = self.bary[:, 1:]
