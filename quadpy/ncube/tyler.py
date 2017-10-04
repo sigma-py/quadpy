9535fc4f5606248fbee8909ff9443c36dd915236
Nico Schlömer <nico.schloemer@gmail.com>
2017-10-04 13:11:38 +0200

    sympyfy for all n-cube schemes

--- quadpy/ncube/tyler.py
+++ quadpy/ncube/tyler.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 #
+from sympy import Rational as fr
+
 from ..helpers import untangle, fsd, z
 
 
@@ -11,13 +13,14 @@ class Tyler(object):
     <https://dx.doi.org/10.4153/CJM-1953-044-1>.
     '''
     def __init__(self, n):
-        reference_volume = 2.0**n
         self.degree = 3
+
         data = [
-            ((3.0 - n)/3.0, z(n)),
-            (1.0/6.0, fsd(n, (1.0, 1))),
+            (fr(3-n, 3), z(n)),
+            (fr(1, 6), fsd(n, (1, 1))),
             ]
 
         self.points, self.weights = untangle(data)
+        reference_volume = 2**n
         self.weights *= reference_volume
         return
