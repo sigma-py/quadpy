f20d2ee81262fc40145c77f1033efd5067239b6d
Nico Schlömer <nico.schloemer@gmail.com>
2017-10-04 17:52:38 +0200

    more sympyfication for n-simplex schemes

--- quadpy/nsimplex/walkington.py
+++ quadpy/nsimplex/walkington.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
-from math import factorial
 import numpy
+from sympy import factorial, Rational as fr, sqrt
 
 from ..helpers import untangle
 
@@ -19,7 +19,7 @@ class Walkington(object):
         self.dim = d
         if index == 1:
             self.degree = 1
-            data = [(1.0 / factorial(d), _c(d))]
+            data = [(fr(1, factorial(d)), _c(d))]
         elif index == 2:
             # The article claims order 2, but tests really only show order 1.
             # Also, the article says:
@@ -31,40 +31,40 @@ class Walkington(object):
             # the simplex.
             self.degree = 1
             data = [
-                (1.0/factorial(d+1), _xi1(d, 1.0 / numpy.sqrt(d + 1.0)))
+                (fr(1, factorial(d+1)), _xi1(d, 1/sqrt(d+1)))
                 ]
         elif index == 3:
             self.degree = 3
             data = [
-                ((-1.0*(d+1)**3) / (4.0 * factorial(d+2)), _c(d)),
-                ((d+3)**3 / (4.0 * factorial(d+3)), _xi1(d, 1.0 / (d + 3.0))),
+                (fr(-(d+1)**3, 4 * factorial(d+2)), _c(d)),
+                (fr(+(d+3)**3, 4 * factorial(d+3)), _xi1(d, fr(1, (d+3)))),
                 ]
         elif index == 5:
             self.degree = 5
-            w0 = +(d+1)**5 / (32.0 * factorial(d+3))
-            w1 = -(d+3)**5 / (16.0 * factorial(d+4))
-            w2 = +(d+5)**5 / (16.0 * factorial(d+5))
+            w0 = fr(+(d+1)**5, 32 * factorial(d+3))
+            w1 = fr(-(d+3)**5, 16 * factorial(d+4))
+            w2 = fr(+(d+5)**5, 16 * factorial(d+5))
             data = [
                 (w0, _c(d)),
-                (w1, _xi1(d, 1.0 / (d + 3.0))),
-                (w2, _xi1(d, 1.0 / (d + 5.0))),
-                (w2, _xi11(d, 1.0 / (d + 5.0))),
+                (w1, _xi1(d, fr(1, d+3))),
+                (w2, _xi1(d, fr(1, d+5))),
+                (w2, _xi11(d, fr(1, d+5))),
                 ]
         else:
             assert index == 7
             self.degree = 7
-            w0 = -1.0/384.0 * (d+1)**7 / factorial(d+4)
-            w1 = +1.0/128.0 * (d+3)**7 / factorial(d+5)
-            w2 = -1.0/64.0 * (d+5)**7 / factorial(d+6)
-            w3 = +1.0/64.0 * (d+7)**7 / factorial(d+7)
+            w0 = -fr(1, 384) * fr((d+1)**7, factorial(d+4))
+            w1 = +fr(1, 128) * fr((d+3)**7, factorial(d+5))
+            w2 = -fr(1, 64) * fr((d+5)**7, factorial(d+6))
+            w3 = +fr(1, 64) * fr((d+7)**7, factorial(d+7))
             data = [
                 (w0, _c(d)),
-                (w1, _xi1(d, 1.0 / (d + 3))),
-                (w2, _xi1(d, 1.0 / (d + 5))),
-                (w2, _xi11(d, 1.0 / (d + 5))),
-                (w3, _xi1(d, 1.0 / (d + 7))),
-                (w3, _xi21(d, 1.0 / (d + 7))),
-                (w3, _xi111(d, 1.0 / (d + 7))),
+                (w1, _xi1(d, fr(1, d+3))),
+                (w2, _xi1(d, fr(1, d+5))),
+                (w2, _xi11(d, fr(1, d+5))),
+                (w3, _xi1(d, fr(1, d+7))),
+                (w3, _xi21(d, fr(1, d+7))),
+                (w3, _xi111(d, fr(1, d+7))),
                 ]
 
         self.bary, self.weights = untangle(data)
@@ -76,20 +76,20 @@ class Walkington(object):
 
 def _c(d):
     return numpy.array([
-        numpy.full(d+1, 1.0/(d+1))
+        numpy.full(d+1, fr(1, d+1))
         ])
 
 
 def _xi1(d, a):
     out = numpy.full((d+1, d+1), a)
-    b = 1.0 - d*a
+    b = 1 - d*a
     numpy.fill_diagonal(out, b)
     return out
 
 
 def _xi11(d, a):
     assert d > 1
-    b = (1.0 - (d-1) * a) / 2.0
+    b = fr(1 - (d-1) * a,  2)
     if d == 2:
         out = numpy.array([
             [b, b, a],
@@ -111,9 +111,9 @@ def _xi11(d, a):
 
 def _xi21(d, a):
     assert d > 1
-    b = (1.0 - (d-2) * a) / 3.0
+    b = fr(1 - (d-2) * a, 3)
     # ERR Note that the article wrongly states (d-2) the the expression for c.
-    c = 1.0 - (d-1) * a - b
+    c = 1 - (d-1) * a - b
     if d == 2:
         out = numpy.array([
             [b, c, a],
@@ -145,7 +145,7 @@ def _xi21(d, a):
 
 def _xi111(d, a):
     assert d == 3
-    b = (1.0 - (d-2) * a) / 3.0
+    b = fr(1 - (d-2) * a, 3)
     out = numpy.array([
         [b, b, b, a],
         [b, b, a, b],
