21d9fc080a1517b84c69948ff550d32326aa9c0f
Nico Schlömer <nico.schloemer@gmail.com>
2017-10-06 17:37:44 +0200

    more sympyfy for quads

--- quadpy/quadrilateral/irwin.py
+++ quadpy/quadrilateral/irwin.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 #
-from .helpers import _symm_s, _symm_s_t
+from sympy import Rational as fr
 
+from .helpers import _symm_s, _symm_s_t
 from ..helpers import untangle
 
 
@@ -17,19 +18,19 @@ class Irwin(object):
         if index == 1:
             self.degree = 3
             data = [
-                (14.0/48.0, _symm_s(1.0)),
-                (-1.0/48.0, _symm_s_t(3.0, 1.0))
+                (fr(14, 48), _symm_s(1)),
+                (-fr(1, 48), _symm_s_t(3, 1))
                 ]
         else:
             assert index == 2
             self.degree = 5
             data = [
-                (889.0/2880.0, _symm_s(1.0)),
-                (-98.0/2880.0, _symm_s_t(3.0, 1.0)),
-                (5.0/2880.0, _symm_s(3.0)),
-                (11.0/2880.0, _symm_s_t(5.0, 1.0)),
+                (fr(889, 2880), _symm_s(1)),
+                (-fr(98, 2880), _symm_s_t(3, 1)),
+                (fr(5, 2880), _symm_s(3)),
+                (fr(11, 2880), _symm_s_t(5, 1)),
                 ]
 
         self.points, self.weights = untangle(data)
-        self.weights *= 4.0
+        self.weights *= 4
         return
