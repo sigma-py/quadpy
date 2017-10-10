f1df2666c457cd2bf73cb302d34da03bdeec559c
Nico Schlömer <nico.schloemer@gmail.com>
2017-10-10 13:30:48 +0200

    hillion: provide barycentric coordinates

--- quadpy/triangle/hillion.py
+++ quadpy/triangle/hillion.py
@@ -3,6 +3,7 @@
 import numpy
 from sympy import Rational as fr, sqrt
 
+from .helpers import _s21
 from ..helpers import untangle
 
 
@@ -18,38 +19,38 @@ class Hillion(object):
     Note that the schemes here are not fully symmetric.
     '''
     def __init__(self, index):
+        # ENH in the article, most schemes are given only in single precision.
+        # quadpy adds symbolic expressions
         self.name = 'Hillion(%d)' % index
 
         if index == 1:
             self.degree = 1
             data = [
-                (fr(1, 2), [[fr(1, 3), fr(1, 3)]])
+                (fr(1, 2), numpy.full((1, 3), fr(1, 3))),
                 ]
         elif index == 2:
             self.degree = 2
             data = [
-                (fr(1, 6), _symm(0, fr(1, 2))),
-                (fr(1, 6), numpy.full((1, 2), fr(1, 2))),
+                (fr(1, 6), _s21(fr(1, 2))),
                 ]
         elif index == 3:
             self.degree = 2
             data = [
-                (fr(1, 6), fr(2, 3) - _symm(0, fr(1, 2))),
-                (fr(1, 6), fr(2, 3) - numpy.full((1, 2), fr(1, 2))),
+                (fr(1, 6), _s21(fr(1, 6))),
                 ]
         elif index == 4:
             self.degree = 2
             a0, a1 = [(3 + i*sqrt(3)) / 8 for i in [+1, -1]]
             data = [
-                (fr(1, 18), numpy.array([[0, 0]])),
+                (fr(1, 18), numpy.array([[0, 0, 1]])),
                 (fr(2, 9), _symm(a0, a1)),
                 ]
         elif index == 5:
             self.degree = 2
             a0, a1 = [(3 + i*sqrt(3)) / 8 for i in [+1, -1]]
             data = [
-                (fr(1, 18), fr(2, 3) - numpy.array([[0, 0]])),
-                (fr(2, 9), fr(2, 3) - _symm(a0, a1))
+                (fr(1, 18), numpy.array([[fr(2, 3), fr(2, 3), -fr(1, 3)]])),
+                (fr(2, 9), _symm(fr(2, 3) - a0, fr(2, 3) - a1))
                 ]
         elif index == 6:
             self.degree = 2
@@ -57,7 +58,7 @@ class Hillion(object):
             mu = (2 - sqrt(2 - sqrt(3))) / 6
             data = [
                 (fr(1, 8), _symm(lmbda, mu)),
-                (fr(1, 8), fr(2, 3) - _symm(lmbda, mu)),
+                (fr(1, 8), _symm(fr(2, 3) - lmbda, fr(2, 3) - mu)),
                 ]
         # Not working
         # elif index == 7:
@@ -72,16 +73,16 @@ class Hillion(object):
             w1, w2 = [(3266 + i * 19*sqrt(46)) / 17664 for i in [+1, -1]]
             data = [
                 (fr(25, 384), _symm(0, fr(4, 5))),
-                (w1, numpy.array([[lambda2, lambda2]])),
-                (w2, numpy.array([[lambda3, lambda3]])),
+                (w1, numpy.array([[lambda2, lambda2, 1 - 2*lambda2]])),
+                (w2, numpy.array([[lambda3, lambda3, 1 - 2*lambda3]])),
                 ]
         # Not working. The weights don't even add up.
         # elif index == 9:
         #     self.degree = 3
         #     data = [
-        #         (9.0/32.0, numpy.array([[1.0/3.0, 1.0/3.0]])),
-        #         (25.0/96.0, _symm(0.2, 0.6)),
-        #         (25.0/96.0, numpy.array([[0.2, 0.2]])),
+        #         (fr(9, 32), numpy.full((1, 3), fr(1, 3))),
+        #         (fr(25, 96), _symm(fr(1, 5), fr(3, 5))),
+        #         (fr(25, 96), numpy.array([[fr(1, 5), fr(1, 5), fr(3, 5)]])),
         #         ]
         else:
             assert index == 10
@@ -91,16 +92,18 @@ class Hillion(object):
             data = [
                 (w2, _symm(lambda1, 0)),
                 (w1, _symm(0, lambda2)),
-                (fr(25, 96), numpy.array([[fr(2, 5), fr(2, 5)]])),
+                (fr(25, 96), numpy.array([[fr(2, 5), fr(2, 5), fr(1, 5)]])),
                 ]
 
-        self.points, self.weights = untangle(data)
+        self.bary, self.weights = untangle(data)
+        self.points = self.bary[:, 1:]
         self.weights *= 2
         return
 
 
 def _symm(a, b):
+    c = 1 - a - b
     return numpy.array([
-        [a, b],
-        [b, a],
+        [a, b, c],
+        [b, a, c],
         ])
