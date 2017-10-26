4af4fb30a1d95dafca8aa48676c43b8f784db637
Nico Schlömer <nico.schloemer@gmail.com>
2017-10-26 11:11:06 +0200

    modernize more triangle schemes

--- quadpy/triangle/seven_point.py
+++ quadpy/triangle/seven_point.py
@@ -2,19 +2,20 @@
 #
 from sympy import Rational as fr
 
-from .helpers import _s3, _s21
-from ..helpers import untangle
+from .helpers import untangle2
 
 
 class SevenPoint(object):
     def __init__(self):
         self.name = 'seven-point'
         self.degree = 3
-        data = [
-            (fr(9, 20), _s3()),
-            (fr(1, 20), _s21(0)),
-            (fr(2, 15), _s21(fr(1, 2))),
-            ]
-        self.bary, self.weights = untangle(data)
+        data = {
+            's3': [fr(9, 20)],
+            's2': [
+                [fr(1, 20), 0],
+                [fr(2, 15), fr(1, 2)],
+                ],
+            }
+        self.bary, self.weights = untangle2(data)
         self.points = self.bary[:, 1:]
         return
