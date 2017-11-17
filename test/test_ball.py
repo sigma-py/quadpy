92c1a46e4bdc97eefd1136ffdf5cfe5bb38e6dac
Nico Schlömer <nico.schloemer@gmail.com>
2017-11-17 17:09:28 +0100

    remove debug statement

--- test/test_ball.py
+++ test/test_ball.py
@@ -20,7 +20,6 @@ from helpers import check_degree
         ]]
     )
 def test_scheme(scheme, tol):
-    out = quadpy.helpers.partition(0, 3)
     degree = check_degree(
             lambda poly: quadpy.ball.integrate(
                 poly, [0.0, 0.0, 0.0], 1.0, scheme
