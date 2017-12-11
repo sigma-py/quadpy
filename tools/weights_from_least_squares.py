dce1ce3f3d98da07807ff5ba7bd6ede5832b6860
Nico Schlömer <nico.schloemer@gmail.com>
2017-12-11 13:03:48 +0100

    clean up weights_from_points

--- tools/weights_from_least_squares.py
+++ tools/weights_from_least_squares.py
@@ -1,100 +1,74 @@
+# -*- coding: utf-8 -*-
+#
+from __future__ import print_function
+
 import numpy
 import orthopy
 import quadpy
 from quadpy.nsimplex.helpers import integrate_monomial_over_unit_simplex
 from quadpy.triangle.helpers import _s3, _s21, _s111ab
-import scipy.special
 
 
-# s = quadpy.triangle.Dunavant(20)
-s = quadpy.triangle.Cubtri()
+def with_monomials(degree):
+    exponents = numpy.concatenate([
+        quadpy.helpers.partition(d, 2)
+        for d in range(degree+1)
+        ])
 
+    exact_vals = numpy.array([
+        integrate_monomial_over_unit_simplex(k) for k in exponents
+        ])
 
-# WITH MONOMIALS
-# ==============
-# exponents = numpy.concatenate([
-#     quadpy.helpers.partition(d, 2)
-#     for d in range(s.degree+1)
-#     ])
-#
-# exact_vals = numpy.array([
-#     integrate_monomial_over_unit_simplex(k) for k in exponents
-#     ])
-#
-#
-# def fun(x):
-#     k = exponents.T
-#     # <https://stackoverflow.com/a/46689653/353337>
-#     s = x.shape[1:] + k.shape[1:]
-#     return (
-#         x.reshape(x.shape[0], -1, 1)**k.reshape(k.shape[0], 1, -1)
-#         ).prod(0).reshape(s)
-
-
-# WITH ORTHOGONAL POLYNOMIALS
-# ===========================
-exponents = numpy.concatenate([
-    quadpy.helpers.partition(d, 2)
-    for d in range(s.degree+1)
-    ])
-# The exact integrals of the orthogonal polynomials over the triangle are 0,
-# except for the one with degree 0 for which we have sqrt(2)/2.
-exact_vals = numpy.zeros(len(exponents))
-exact_vals[0] = numpy.sqrt(2) / 2
-
-
-def fun(x):
-    def f(i, j, x, y):
-        alpha1, beta1 = \
-            orthopy.recurrence_coefficients.jacobi(i, 0, 0, mode='numpy')
-        a = orthopy.tools.evaluate_orthogonal_polynomial(
-                alpha1, beta1, (x-y)/(x+y)
-                )
-        # a = numpy.polyval(scipy.special.jacobi(i, 0, 0), (x-y)/(x+y))
-
-        alpha2, beta2 = \
-            orthopy.recurrence_coefficients.jacobi(j, 2*i+1, 0, mode='numpy')
-        b = orthopy.tools.evaluate_orthogonal_polynomial(
-                alpha2, beta2, 1-2*(x+y)
-                )
-        # b = numpy.polyval(scipy.special.jacobi(j, 2*i+1, 0), 1-2*(x+y))
-
+    def fun(x):
+        k = exponents.T
+        # <https://stackoverflow.com/a/46689653/353337>
+        s = x.shape[1:] + k.shape[1:]
         return (
-            numpy.sqrt(2*i + 1) * a * (x+y)**i
-            * numpy.sqrt(2*j + 2*i + 2) * b
+            x.reshape(x.shape[0], -1, 1)**k.reshape(k.shape[0], 1, -1)
+            ).prod(0).reshape(s)
+
+    return fun, exact_vals
+
+
+if __name__ == '__main__':
+    scheme = quadpy.triangle.Cubtri()
+
+    def eval_orthopolys(x):
+        bary = numpy.array([x[0], x[1], 1.0-x[0]-x[1]])
+        out = numpy.concatenate(
+            orthopy.triangle.orth_tree(scheme.degree+1, bary, 'normal')
             )
+        return out
 
-    return numpy.array([
-        f(ij[0], ij[1], x[0], x[1])
-        for ij in exponents
-        ]).T
+    fun = eval_orthopolys
 
+    a_data = []
+    if 's3' in scheme.data:
+        a_data.append(fun(_s3().T[1:]))
 
-a_data = []
-if 's3' in s.data:
-    a_data.append(fun(_s3().T[1:]))
+    if 's2' in scheme.data:
+        s2_data = _s21(scheme.data['s2'][1])
+        a_data.append(numpy.sum(fun(s2_data[1:]), axis=1))
 
-if 's2' in s.data:
-    s2_data = _s21(s.data['s2'][1])
-    a_data.append(numpy.sum(fun(s2_data[1:]), axis=1))
+    if 's1' in scheme.data:
+        s1_data = _s111ab(*scheme.data['s1'][1:])
+        a_data.append(numpy.sum(fun(s1_data[1:]), axis=1))
 
-if 's1' in s.data:
-    s1_data = _s111ab(*s.data['s1'][1:])
-    a_data.append(numpy.sum(fun(s1_data[1:]), axis=1))
+    A = numpy.column_stack(a_data)
 
-A = numpy.concatenate(a_data).T
+    exact_vals = numpy.zeros(len(A))
+    exact_vals[0] = 1.0
 
-print(A.shape)
+    x, res, rank, sv = numpy.linalg.lstsq(A, exact_vals)
 
-x, res, rank, sv = numpy.linalg.lstsq(A, exact_vals)
+    print('A.shape = {} x {}'.format(*A.shape))
+    print('rank: {}'.format(rank))
+    print('residual: {}'.format(res))
+    print('singular values:')
+    for val in sv:
+        print('  {:.15e}'.format(val))
 
-print
-print(x*2)
-print(res)
-print(rank)
-print(sv)
-print(numpy.dot(A, x) - exact_vals)
-
-print('{:.15e}'.format(x[0]*2))
-print('{:.15e}'.format(x[1]*2))
-print('{:.15e}'.format(x[-1]*2))
+    print()
+    print('solution:')
+    for val in x:
+        print('  {:.15e}'.format(2*val))
