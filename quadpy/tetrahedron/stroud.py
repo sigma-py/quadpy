c47de394f75c7a48f5ddad354408c37ff6537ec4
Nico Schlömer <nico.schloemer@gmail.com>
2017-12-16 14:53:51 +0100

    hardcode another two schemes

--- quadpy/tetrahedron/stroud.py
+++ quadpy/tetrahedron/stroud.py
@@ -31,16 +31,44 @@ class Stroud(object):
             A = gl4.weights / 2
 
             # Generate Gauss formula for int_0^1 (1-s) * f(s) ds.
-            k = numpy.arange(8)
-            moments = 1 / (k**2 + 3*k + 2)
-            alpha, beta = orthopy.line.chebyshev(moments)
-            s, B = orthopy.line.schemes.custom(alpha, beta, mode='numpy')
+            # ```
+            # k = numpy.arange(8)
+            # moments = 1 / (k**2 + 3*k + 2)
+            # alpha, beta = orthopy.line.chebyshev(moments)
+            # s, B = orthopy.line.schemes.custom(alpha, beta, mode='numpy')
+            # ```
+            s = numpy.array([
+                5.710419611452533e-02,
+                2.768430136381415e-01,
+                5.835904323689318e-01,
+                8.602401356562251e-01,
+                ])
+            B = numpy.array([
+                1.355069134315012e-01,
+                2.034645680102685e-01,
+                1.298475476082247e-01,
+                3.118097095000554e-02,
+                ])
 
             # Generate Gauss formula for int_0^1 (1-t)^2 * f(t) ds.
-            k = numpy.arange(8)
-            moments = 2 / (k**3 + 6*k**2 + 11*k + 6)
-            alpha, beta = orthopy.line.chebyshev(moments)
-            t, C = orthopy.line.schemes.custom(alpha, beta, mode='numpy')
+            # ```
+            # k = numpy.arange(8)
+            # moments = 2 / (k**3 + 6*k**2 + 11*k + 6)
+            # alpha, beta = orthopy.line.chebyshev(moments)
+            # t, C = orthopy.line.schemes.custom(alpha, beta, mode='numpy')
+            # ```
+            t = numpy.array([
+                4.850054944699245e-02,
+                2.386007375518456e-01,
+                5.170472951043522e-01,
+                7.958514178967657e-01,
+                ])
+            C = numpy.array([
+                1.108884156112685e-01,
+                1.434587897992167e-01,
+                6.863388717292915e-02,
+                1.035224074991912e-02,
+                ])
 
             data = [
                 (6*A[i]*B[j]*C[k], numpy.array([[
