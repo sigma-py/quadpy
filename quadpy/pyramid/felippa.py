a66fb70bbd5b491361b9ccdaf41918d73d390485
Nico Schlömer <nico.schloemer@gmail.com>
2018-01-02 21:45:53 +0100

    numpyfy wedge and pyramid schemes

--- quadpy/pyramid/felippa.py
+++ quadpy/pyramid/felippa.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 #
+from __future__ import division
+
 import numpy
-from sympy import Rational as fr, sqrt
+import sympy
 
 from .helpers import _s4, _s4_0
 from ..helpers import untangle
@@ -39,47 +41,52 @@ class Felippa(object):
     selected arbitrarily. No similar modular collection applicable to a range
     of FEM work, whether symbolic or numeric, has been published before.
     '''
-    def __init__(self, index):
+    def __init__(self, index, symbolic=False):
+        frac = sympy.Rational if symbolic else lambda x, y: x/y
+        sqrt = numpy.vectorize(sympy.sqrt) if symbolic else numpy.sqrt
+
+        self.name = 'Felippa({})'.format(index)
+
         wg9 = numpy.array([
-            fr(64, 81),
-            fr(40, 81),
-            fr(25, 81),
+            frac(64, 81),
+            frac(40, 81),
+            frac(25, 81),
             ])
 
         if index == 1:
             self.degree = 1
             data = [
-                (fr(128, 27), numpy.array([[0, 0, -fr(1, 2)]])),
+                (frac(128, 27), numpy.array([[0, 0, -frac(1, 2)]])),
                 ]
         elif index == 2:
             self.degree = 2
             data = [
-                (fr(81, 100), _s4(8 * sqrt(fr(2, 15)) / 5, -fr(2, 3))),
-                (fr(125, 27), numpy.array([[0, 0, fr(2, 5)]])),
+                (frac(81, 100), _s4(8 * sqrt(frac(2, 15)) / 5, -frac(2, 3))),
+                (frac(125, 27), numpy.array([[0, 0, frac(2, 5)]])),
                 ]
         elif index == 3:
             self.degree = 2
             data = [
-                (fr(504, 625), _s4(sqrt(fr(12, 35)), -fr(2, 3))),
-                (fr(576, 625), numpy.array([[0, 0, fr(1, 6)]])),
-                (fr(64, 15), numpy.array([[0, 0, fr(1, 2)]])),
+                (frac(504, 625), _s4(sqrt(frac(12, 35)), -frac(2, 3))),
+                (frac(576, 625), numpy.array([[0, 0, frac(1, 6)]])),
+                (frac(64, 15), numpy.array([[0, 0, frac(1, 2)]])),
                 ]
         elif index == 4:
             self.degree = 3
             w1 = 5 * (68 + 5*sqrt(10)) / 432
-            w2 = fr(85, 54) - w1
-            g1 = sqrt(fr(1, 3))
+            w2 = frac(85, 54) - w1
+            g1 = sqrt(frac(1, 3))
             g2 = (2*sqrt(10)-5) / 15
             data = [
                 (w1, _s4(g1, g2)),
-                (w2, _s4(g1, -fr(2, 3) - g2)),
+                (w2, _s4(g1, -frac(2, 3) - g2)),
                 ]
         elif index == 5:
             self.degree = 2
             w1 = (11764 - 461*sqrt(51)) / 15300
-            w2 = fr(346, 225) - w1
+            w2 = frac(346, 225) - w1
             g1, g2 = [
-                sqrt(fr(2, 15) * (573 - i * 2*sqrt(51))) / 15
+                sqrt(frac(2, 15) * (573 - i * 2*sqrt(51))) / 15
                 for i in [+1, -1]
                 ]
             g3, g4 = [
@@ -93,7 +100,7 @@ class Felippa(object):
         elif index == 6:
             self.degree = 2
             w1 = 7*(11472415 - 70057*sqrt(2865)) / 130739500
-            w2 = fr(84091, 68450) - w1
+            w2 = frac(84091, 68450) - w1
 
             g1 = 8 * sqrt(
                 (573 + 5*sqrt(2865))
@@ -108,32 +115,32 @@ class Felippa(object):
             data = [
                 (w1, _s4(g1, g3)),
                 (w2, _s4(g2, g4)),
-                (fr(18, 5), numpy.array([[0, 0, fr(2, 3)]])),
+                (frac(18, 5), numpy.array([[0, 0, frac(2, 3)]])),
                 ]
         elif index == 7:
             self.degree = 2
-            w1 = fr(170569, 331200)
-            w2 = fr(276710106577408, 1075923777052725)
-            w3 = fr(12827693806929, 30577384040000)
-            w4 = fr(10663383340655070643544192, 4310170528879365193704375)
-            g1 = 7 * sqrt(fr(35, 59)) / 8
-            g2 = 224 * sqrt(fr(336633710, 33088740423)) / 37
-            g3 = sqrt(fr(37043, 35)) / 56
-            g4 = -fr(127, 153)
-            g5 = fr(1490761, 2842826)
+            w1 = frac(170569, 331200)
+            w2 = frac(276710106577408, 1075923777052725)
+            w3 = frac(12827693806929, 30577384040000)
+            w4 = frac(10663383340655070643544192, 4310170528879365193704375)
+            g1 = 7 * sqrt(frac(35, 59)) / 8
+            g2 = 224 * sqrt(frac(336633710, 33088740423)) / 37
+            g3 = sqrt(frac(37043, 35)) / 56
+            g4 = -frac(127, 153)
+            g5 = frac(1490761, 2842826)
             data = [
-                (w1, _s4(g1, -fr(1, 7))),
-                (w2, _s4_0(g2, -fr(9, 28))),
+                (w1, _s4(g1, -frac(1, 7))),
+                (w2, _s4_0(g2, -frac(9, 28))),
                 (w3, _s4(g3, g4)),
                 (w4, numpy.array([[0, 0, g5]])),
                 ]
         elif index == 8:
             self.degree = 3
             w1 = 5 * (68 + 5*sqrt(10)) / 432
-            w2 = fr(85, 54) - w1
-            g1 = sqrt(fr(3, 5))
+            w2 = frac(85, 54) - w1
+            g1 = sqrt(frac(3, 5))
             g2 = 1 - 2*(10 - sqrt(10)) / 15
-            g3 = -fr(2, 3) - g2
+            g3 = -frac(2, 3) - g2
             data = [
                 (w1*wg9[2], _s4(g1, g2)),
                 (w1*wg9[1], _s4_0(g1, g2)),
@@ -145,13 +152,13 @@ class Felippa(object):
         else:
             assert index == 9
             self.degree = 5
-            g1 = sqrt(fr(3, 5))
+            g1 = sqrt(frac(3, 5))
             g3 = -0.854011951853700535688324041975993416
             g4 = -0.305992467923296230556472913192103090
             g5 = +0.410004419776996766244796955168096505
-            w1 = fr(4, 15)*(4+5*(g4+g5)+10*g4*g5)/((g3-g4)*(g3-g5)*(1-g3)**2)
-            w2 = fr(4, 15)*(4+5*(g3+g5)+10*g3*g5)/((g3-g4)*(g5-g4)*(1-g4)**2)
-            w3 = fr(4, 15)*(4+5*(g3+g4)+10*g3*g4)/((g3-g5)*(g4-g5)*(1-g5)**2)
+            w1 = frac(4, 15)*(4+5*(g4+g5)+10*g4*g5)/((g3-g4)*(g3-g5)*(1-g3)**2)
+            w2 = frac(4, 15)*(4+5*(g3+g5)+10*g3*g5)/((g3-g4)*(g5-g4)*(1-g4)**2)
+            w3 = frac(4, 15)*(4+5*(g3+g4)+10*g3*g4)/((g3-g5)*(g4-g5)*(1-g5)**2)
             data = [
                 (w1*wg9[2], _s4(g1, g3)),
                 (w1*wg9[1], _s4_0(g1, g3)),
