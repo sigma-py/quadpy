200e78331371ac6c13e6be40b13d7b791121437b
Nico Schlömer <nico.schloemer@gmail.com>
2018-01-02 14:34:28 +0100

    numpyfy a few disk schemes

--- quadpy/disk/albrecht_collatz.py
+++ quadpy/disk/albrecht_collatz.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 #
-from sympy import pi, Rational as fr
+from __future__ import division
+
+import numpy
+import sympy
 
 from ..helpers import untangle, pm
 
@@ -12,12 +15,15 @@ class AlbrechtCollatz(object):
     ZAMM, Volume 38, Issue 1-2, 1958, Pages 1–15,
     <https://doi.org/10.1002/zamm.19580380102>
     '''
-    def __init__(self):
+    def __init__(self, symbolic=True):
+        frac = sympy.Rational if symbolic else lambda x, y: x/y
+        pi = sympy.pi if symbolic else numpy.pi
+
         self.name = 'AlbrechtCollatz'
         self.degree = 3
         data = [
-            # ERR Wrongly stated in Stroud as sqrt(0.5) instead of 0.5
-            (fr(1, 4), pm(2, fr(1, 2))),
+            # ERR Wrongly stated in Stroud as sqrt(1/2) instead of 1/2
+            (frac(1, 4), pm(2, frac(1, 2))),
             ]
         self.points, self.weights = untangle(data)
         self.weights *= pi
