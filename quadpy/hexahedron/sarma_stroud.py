7de85aefd830d46c6f7916694a7451a1649e92e5
Nico Schlömer <nico.schloemer@gmail.com>
2018-01-02 21:41:20 +0100

    numpyfy hexahedron

--- quadpy/hexahedron/sarma_stroud.py
+++ quadpy/hexahedron/sarma_stroud.py
@@ -11,7 +11,7 @@ class SarmaStroud(object):
     Vol. 23, No. 108 (Oct., 1969), pp. 781-784,
     <https://doi.org/10.2307/2004963>.
     '''
-    def __init__(self):
+    def __init__(self, symbolic=False):
         # Hammer-Wymore is a one-parameter family of schemes, and the
         # parameters lambda is chosen to minimize the standard deviation of
         # Sarma's error functional. The particular value of lambda is not
@@ -19,7 +19,7 @@ class SarmaStroud(object):
         # values. Note that it is only given in single precision, so the
         # minimum is only approximated.
         lmbda = 1.0329785305
-        hw = HammerWymore(lmbda=lmbda)
+        hw = HammerWymore(lmbda=lmbda, symbolic=symbolic)
         self.degree = hw.degree
         self.points = hw.points
         self.weights = hw.weights
