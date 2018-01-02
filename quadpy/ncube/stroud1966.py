5e018ac749663d42d3e9412e5700c7a55999a90a
Nico Schlömer <nico.schloemer@gmail.com>
2018-01-02 18:12:12 +0100

    numpyfy ncube

--- quadpy/ncube/stroud1966.py
+++ quadpy/ncube/stroud1966.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 import numpy
-from sympy import sqrt, Rational as fr, S
+import sympy
 
 from .helpers import _fs11
 from ..helpers import untangle, fsd, z, pm
@@ -16,34 +16,38 @@ class Stroud1966(object):
     Published by: American Mathematical Society,
     <https://doi.org/10.1090/S0025-5718-1966-0191094-8>.
     '''
-    def __init__(self, n, variant):
+    def __init__(self, n, variant, symbolic=True):
+        frac = sympy.Rational if symbolic else lambda x, y: x/y
+        sqrt = sympy.sqrt if symbolic else numpy.sqrt
+        zero = sympy.S.Zero if symbolic else 0.0
+
         self.name = 'Stroud66{}'.format(variant)
         self.degree = 5
 
         if variant == 'a':
-            r = sqrt(fr(5*n + 4, 30))
-            s = sqrt(fr(5*n + 4, 15*n - 12))
+            r = sqrt(frac(5*n + 4, 30))
+            s = sqrt(frac(5*n + 4, 15*n - 12))
             data = [
-                (fr(40, (5*n+4)**2), fsd(n, (r, 1))),
-                (fr(5*n - 4, (5*n + 4))**2 / 2**n, pm(n, s)),
+                (frac(40, (5*n+4)**2), fsd(n, (r, 1))),
+                (frac(5*n - 4, (5*n + 4))**2 / 2**n, pm(n, s)),
                 ]
         elif variant == 'b':
             s = 1 / sqrt(3)
-            data = [(fr(4, 5*n + 4), z(n))]
+            data = [(frac(4, 5*n + 4), z(n))]
             for k in range(1, n+1):
-                r = sqrt(fr(5*k + 4, 15))
-                arr = numpy.full((2**(n-k+1), n), S.Zero)
+                r = sqrt(frac(5*k + 4, 15))
+                arr = numpy.full((2**(n-k+1), n), zero)
                 arr[:, k-1:] = pm(n-k+1, 1)
                 arr[:, k-1] *= r
                 arr[:, k:] *= s
-                b = fr(5 * 2**(k-n+1), (5*k-1) * (5*k+4))
+                b = frac(5 * 2**(k-n+1), (5*k-1) * (5*k+4))
                 data.append((b, arr))
         elif variant == 'c':
             r = sqrt((5*n + 4 + 2*(n-1)*sqrt(5*n+4)) / (15*n))
             s = sqrt((5*n + 4 - 2*sqrt(5*n+4)) / (15*n))
             data = [
-                (fr(4, 5*n+4), z(n)),
-                (fr(5, (5*n+4) * 2**n), _fs11(n, r, s)),
+                (frac(4, 5*n+4), z(n)),
+                (frac(5, (5*n+4) * 2**n), _fs11(n, r, s)),
                 ]
         else:
             assert variant == 'd'
@@ -52,7 +56,7 @@ class Stroud1966(object):
             # This sqrt() is imaginary for negative for n=2.
             s = sqrt((5*n - 2*sqrt(5) - 2*sqrt(5*n+5)) / (15*n))
             t = sqrt((5 + 2*sqrt(5)) / 15)
-            w = fr(1, 2**n * (n+1))
+            w = frac(1, 2**n * (n+1))
             data = [
                 (w, _fs11(n, r, s)),
                 (w, pm(n, t)),
