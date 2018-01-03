2342196c2a7309c451ac182bde5684d4a2598c19
Nico Schlömer <nico.schloemer@gmail.com>
2018-01-03 16:49:56 +0100

    numpyfy nsimplex

--- quadpy/nsimplex/lauffer.py
+++ quadpy/nsimplex/lauffer.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 #
-from sympy import Rational as fr
+from __future__ import division
+
+import sympy
 
 from ..helpers import untangle, rd
 
@@ -13,29 +15,31 @@ class Lauffer(object):
     <https://doi.org/10.1007/BF01900222>.
     '''
     # pylint: disable=too-many-locals
-    def __init__(self, n, degree):
+    def __init__(self, n, degree, symbolic=False):
+        frac = sympy.Rational if symbolic else lambda x, y: x/y
+
         self.dim = n
         self.degree = degree
         if degree == 1:
             data = [
-                (fr(1, n+1), rd(n+1, [(1, 1)]))
+                (frac(1, n+1), rd(n+1, [(1, 1)]))
                 ]
         elif degree == 2:
-            B = fr(2-n, (n+1) * (n+2))
-            C = fr(4, (n+1) * (n+2))
+            B = frac(2-n, (n+1) * (n+2))
+            C = frac(4, (n+1) * (n+2))
 
             data = [
                 (B, rd(n+1, [(1, 1)])),
-                (C, rd(n+1, [(fr(1, 2), 2)])),
+                (C, rd(n+1, [(frac(1, 2), 2)])),
                 ]
         elif degree == 3:
 
-            B = fr(n**2-4*n+6, (n+1) * (n+2) * (n+3))
-            C = fr(27-9*n, 2 * (n+1) * (n+2) * (n+3))
-            D = fr(27, (n+1) * (n+2) * (n+3))
+            B = frac(n**2-4*n+6, (n+1) * (n+2) * (n+3))
+            C = frac(27-9*n, 2 * (n+1) * (n+2) * (n+3))
+            D = frac(27, (n+1) * (n+2) * (n+3))
 
-            r = fr(1, 3)
-            s = fr(2, 3)
+            r = frac(1, 3)
+            s = frac(2, 3)
 
             data = [
                 (B, rd(n+1, [(1, 1)])),
@@ -46,15 +50,15 @@ class Lauffer(object):
             assert n >= 3
 
             nprod = (n+1) * (n+2) * (n+3) * (n+4)
-            B1 = fr(-3*n**3 + 17*n**2 - 58*n + 72, 3*nprod)
-            B2 = fr(16 * (n**2 - 5*n + 12), 3*nprod)
-            B3 = fr(4 * (n**2 - 9*n + 12), nprod)
-            B4 = fr(64 * (4-n), 2*nprod)
-            B5 = fr(256, nprod)
+            B1 = frac(-3*n**3 + 17*n**2 - 58*n + 72, 3*nprod)
+            B2 = frac(16 * (n**2 - 5*n + 12), 3*nprod)
+            B3 = frac(4 * (n**2 - 9*n + 12), nprod)
+            B4 = frac(64 * (4-n), 2*nprod)
+            B5 = frac(256, nprod)
 
-            r = fr(1, 4)
-            s = fr(3, 4)
-            t = fr(1, 2)
+            r = frac(1, 4)
+            s = frac(3, 4)
+            t = frac(1, 2)
 
             data = [
                 (B1, rd(n+1, [(1, 1)])),
@@ -71,18 +75,18 @@ class Lauffer(object):
             nprod = (n+1) * (n+2) * (n+3) * (n+4) * (n+5)
 
             # ERR Stroud is missing the factor 1/12 in B1.
-            B1 = fr((12*n**4 - 82*n**3 + 477*n**2 - 1277*n + 1440), 12*nprod)
-            B2 = fr(5**2 * (-3*n**3 + 19*n**2 - 96*n + 170), 12*nprod)
-            B3 = fr(5**2 * (-n**3 + 13*n**2 - 47*n + 65), 6*nprod)
-            B4 = fr(5**3 * (n**2 - 6*n + 20), 3*nprod)
-            B5 = fr(5**3 * (n**2 - 11*n + 20), 4*nprod)
-            B6 = fr(5**4 * (5 - n), 2*nprod)
-            B7 = fr(5**5, nprod)
+            B1 = frac((12*n**4 - 82*n**3 + 477*n**2 - 1277*n + 1440), 12*nprod)
+            B2 = frac(5**2 * (-3*n**3 + 19*n**2 - 96*n + 170), 12*nprod)
+            B3 = frac(5**2 * (-n**3 + 13*n**2 - 47*n + 65), 6*nprod)
+            B4 = frac(5**3 * (n**2 - 6*n + 20), 3*nprod)
+            B5 = frac(5**3 * (n**2 - 11*n + 20), 4*nprod)
+            B6 = frac(5**4 * (5 - n), 2*nprod)
+            B7 = frac(5**5, nprod)
 
-            r = fr(1, 5)
-            s = fr(4, 5)
-            u = fr(2, 5)
-            v = fr(3, 5)
+            r = frac(1, 5)
+            s = frac(4, 5)
+            u = frac(2, 5)
+            v = frac(3, 5)
 
             data = [
                 (B1, rd(n+1, [(1, 1)])),
