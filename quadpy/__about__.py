4321ac170b6facdd0f99075309a8672ed32d89f0
Nico Schlömer <nico.schloemer@gmail.com>
2018-01-14 22:45:45 +0100

    version bump

--- quadpy/__about__.py
+++ quadpy/__about__.py
@@ -4,9 +4,9 @@
 __author__ = u'Nico Schlömer'
 __email__ = 'nico.schloemer@gmail.com'
 __copyright__ = \
-    'Copyright (c) 2016-2018, {} <{}>'.format(__author__, __email__)
+    u'Copyright (c) 2016-2018, {} <{}>'.format(__author__, __email__)
 __credits__ = []
 __license__ = 'License :: OSI Approved :: MIT License'
-__version__ = '0.11.1'
+__version__ = '0.11.2'
 __maintainer__ = 'Nico Schlömer'
 __status__ = 'Development Status :: 4 - Beta'
