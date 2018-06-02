bda4844db562726d7724871cb198c89726d9d681
Nico Schlömer <nico.schloemer@gmail.com>
2018-06-02 13:22:49 +0200

    simplify hexhedron::stroud

--- quadpy/hexahedron/stroud.py
+++ quadpy/hexahedron/stroud.py
@@ -28,57 +28,47 @@ class Stroud(object):
     """
 
     def __init__(self, index, symbolic=False):
-        frac = sympy.Rational if symbolic else lambda x, y: x / y
-        sqrt = numpy.vectorize(sympy.sqrt) if symbolic else numpy.sqrt
 
-        reference_volume = 8
-        if index == "C3 3-1":
-            self.set_data(Tyler(1))
-        elif index == "C3 3-2":
-            # product Gauss
-            self.degree = 3
-            data = [(frac(1, 8), pm_rrr(sqrt(frac(1, 3))))]
-            self.points, self.weights = untangle(data)
-            self.weights *= reference_volume
-        elif index == "C3 3-3":
-            self.set_data(Ewing(3, symbolic))
-        elif index == "C3 3-4":
-            self.set_data(MustardLynessBlatt(1, symbolic))
-        elif index == "C3 3-5":
-            self.set_data(MustardLynessBlatt(2, symbolic))
-        elif index == "C3 3-6":
-            self.set_data(AlbrechtCollatz(symbolic))
-        elif index == "C3 3-7":
-            self.set_data(MustardLynessBlatt(3, symbolic))
-        elif index == "C3 5-1":
-            self.set_data(Stroud1967(symbolic))
-        elif index == "C3 5-2":
-            self.set_data(HammerStroud("2-3", symbolic))
-        elif index == "C3 5-3":
-            self.set_data(Tyler(2, symbolic))
-        elif index == "C3 5-4":
-            self.set_data(MustardLynessBlatt(4, symbolic))
-        elif index == "C3 5-5":
-            self.set_data(MustardLynessBlatt(5, symbolic))
-        elif index == "C3 5-6":
-            self.set_data(MustardLynessBlatt(6, symbolic))
-        elif index == "C3 5-7":
-            self.set_data(MustardLynessBlatt(7, symbolic))
-        elif index == "C3 5-8":
-            self.set_data(Sadowsky(symbolic))
-        elif index == "C3 7-1a":
-            self.set_data(HammerStroud("5-3a", symbolic))
-        elif index == "C3 7-1b":
-            self.set_data(HammerStroud("5-3b", symbolic))
-        elif index == "C3 7-2":
-            self.set_data(HammerWymore(symbolic=symbolic))
-        else:
-            assert index == "C3 7-3"
-            self.set_data(SarmaStroud(symbolic))
-        return
+        d = {
+            "C3 3-1": (Tyler, [1]),
+            "C3 3-2": (ProductGauss, [symbolic]),
+            "C3 3-3": (Ewing, [3, symbolic]),
+            "C3 3-4": (MustardLynessBlatt, [1, symbolic]),
+            "C3 3-5": (MustardLynessBlatt, [2, symbolic]),
+            "C3 3-6": (AlbrechtCollatz, [symbolic]),
+            "C3 3-7": (MustardLynessBlatt, [3, symbolic]),
+            "C3 5-1": (Stroud1967, [symbolic]),
+            "C3 5-2": (HammerStroud, ["2-3", symbolic]),
+            "C3 5-3": (Tyler, [2, symbolic]),
+            "C3 5-4": (MustardLynessBlatt, [4, symbolic]),
+            "C3 5-5": (MustardLynessBlatt, [5, symbolic]),
+            "C3 5-6": (MustardLynessBlatt, [6, symbolic]),
+            "C3 5-7": (MustardLynessBlatt, [7, symbolic]),
+            "C3 5-8": (Sadowsky, [symbolic]),
+            "C3 7-1a": (HammerStroud, ["5-3a", symbolic]),
+            "C3 7-1b": (HammerStroud, ["5-3b", symbolic]),
+            "C3 7-2": (HammerWymore, [symbolic]),
+            "C3 7-3": (SarmaStroud, [symbolic]),
+        }
 
-    def set_data(self, scheme):
+        fun, args = d[index]
+        scheme = fun(args)
+
         self.degree = scheme.degree
         self.weights = scheme.weights
         self.points = scheme.points
+        return
+
+
+class ProductGauss(object):
+    def __init__(self, symbolic):
+        reference_volume = 8
+
+        frac = sympy.Rational if symbolic else lambda x, y: x / y
+        sqrt = numpy.vectorize(sympy.sqrt) if symbolic else numpy.sqrt
+
+        self.degree = 3
+        data = [(frac(1, 8), pm_rrr(sqrt(frac(1, 3))))]
+        self.points, self.weights = untangle(data)
+        self.weights *= reference_volume
         return
