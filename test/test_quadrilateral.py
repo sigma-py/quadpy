9f86d7fc176bd2503b0b3fd65743749e7f7502f1
Nico Schlömer <nico.schloemer@gmail.com>
2019-06-11 00:49:43 +0200

    add waldron schemes

--- test/test_quadrilateral.py
+++ test/test_quadrilateral.py
@@ -114,6 +114,7 @@ def _integrate_exact2(k, x0, x1, y0, y1):
     + [(quadpy.quadrilateral.Schmid(k), 1.0e-10) for k in [2, 4, 6]]
     + [(quadpy.quadrilateral.Sommariva(k), 1.0e-13) for k in range(1, 56)]
     + [(quadpy.quadrilateral.StroudN(k), 1.0e-8) for k in ["Cn 7-1"]]
+    + [(quadpy.quadrilateral.Waldron(0.6, numpy.pi / 7), 1.0e-14)]
     + [
         (quadpy.quadrilateral.WissmannBecker(k), 1.0e-14)
         for k in ["4-1", "4-2", "6-1", "6-2", "8-1", "8-2"]
