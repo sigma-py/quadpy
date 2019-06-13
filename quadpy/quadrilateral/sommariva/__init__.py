94d4931450dd76ccb1660cf6710e2d744bb452be
Nico Schlömer <nico.schloemer@gmail.com>
2019-06-13 17:43:19 +0200

    more quad modernization

--- quadpy/quadrilateral/sommariva/__init__.py
+++ quadpy/quadrilateral/sommariva/__init__.py
@@ -1,28 +1,29 @@
 # -*- coding: utf-8 -*-
 #
+"""
+Quadrilateral quadrature by Alvise Sommariva,
+<http://www.math.unipd.it/~alvise/sets.html>.
+"""
 import json
 import os
 
 import numpy
 
+from ..helpers import QuadrilateralScheme
 
-class Sommariva(object):
-    """
-    Quad quadrature by Alvise Sommariva, <http://www.math.unipd.it/~alvise/sets.html>.
-    """
 
-    def __init__(self, index):
-        self.name = "Sommariva({})".format(index)
+def sommariva(index):
+    this_dir = os.path.dirname(os.path.realpath(__file__))
+    filename = "sommariva_{:02d}.json".format(index)
+    with open(os.path.join(this_dir, filename), "r") as f:
+        data = json.load(f)
 
-        this_dir = os.path.dirname(os.path.realpath(__file__))
+    degree = data.pop("degree")
 
-        filename = "sommariva_{:02d}.json".format(index)
-        with open(os.path.join(this_dir, filename), "r") as f:
-            data = json.load(f)
+    data = numpy.array(data["data"])
+    points = data[:, :2]
+    weights = data[:, 2]
+    return QuadrilateralScheme("Sommariva {}".format(index), degree, weights, points)
 
-        self.degree = data.pop("degree")
 
-        data = numpy.array(data["data"])
-        self.points = data[:, :2]
-        self.weights = data[:, 2]
-        return
+Sommariva = {k: lambda: sommariva(k) for k in range(1, 56)}
