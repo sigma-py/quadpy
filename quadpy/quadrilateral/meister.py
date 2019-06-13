59c57734efbc31bfd9a837b46959c48d0f769659
Nico Schlömer <nico.schloemer@gmail.com>
2019-06-13 16:26:35 +0200

    modernize a few quad schemes

--- quadpy/quadrilateral/meister.py
+++ quadpy/quadrilateral/meister.py
@@ -1,35 +1,33 @@
 # -*- coding: utf-8 -*-
 #
+"""
+Bernd Meister,
+On a Family of Cubature Formulae,
+Comput J (1966) 8 (4): 368-371,
+<https://doi.org/10.1093/comjnl/8.4.368>.
+"""
 from __future__ import division
 
 import sympy
 
-from .helpers import concat, zero, symm_s, symm_r0, symm_s_t
+from .helpers import concat, zero, symm_s, symm_r0, symm_s_t, QuadrilateralScheme
 
 
-class Meister(object):
-    """
-    Bernd Meister,
-    On a Family of Cubature Formulae,
-    Comput J (1966) 8 (4): 368-371,
-    <https://doi.org/10.1093/comjnl/8.4.368>.
-    """
+def Meister(symbolic=False):
+    frac = sympy.Rational if symbolic else lambda x, y: x / y
 
-    def __init__(self, symbolic=False):
-        frac = sympy.Rational if symbolic else lambda x, y: x / y
+    name = "Meister"
+    degree = 7
 
-        self.name = "Meister"
-        self.degree = 7
+    r = frac(2, 3)
+    s = frac(1, 3)
 
-        r = frac(2, 3)
-        s = frac(1, 3)
+    weights, points = concat(
+        zero(frac(1024, 6720)),
+        symm_s([frac(576, 6720), r], [-frac(9, 6720), s], [frac(47, 6720), 1]),
+        symm_r0([frac(576, 6720), r]),
+        symm_s_t([frac(117, 6720), 1, s]),
+    )
 
-        self.weights, self.points = concat(
-            zero(frac(1024, 6720)),
-            symm_s([frac(576, 6720), r], [-frac(9, 6720), s], [frac(47, 6720), 1]),
-            symm_r0([frac(576, 6720), r]),
-            symm_s_t([frac(117, 6720), 1, s]),
-        )
-
-        self.weights *= 4
-        return
+    weights *= 4
+    return QuadrilateralScheme(name, degree, weights, points)
--- quadpy/quadrilateral/miller.py
+++ quadpy/quadrilateral/miller.py
@@ -1,32 +1,30 @@
 # -*- coding: utf-8 -*-
 #
+"""
+J.C.P. Miller,
+Numerical Quadrature Over a Rectangular Domain in Two or More Dimensions.
+Part 3: Quadrature of a Harmonic Integrand,
+Mathematics of Computation,
+Vol. 14, No. 71 (Jul., 1960), pp. 240-248,
+<https://doi.org/10.2307/2003163>.
+
+This scheme is exact for harmonic integrands of degree <= 11.
+"""
 from __future__ import division
 
 import sympy
 
-from .helpers import concat, zero, symm_r0, symm_s
+from .helpers import concat, zero, symm_r0, symm_s, QuadrilateralScheme
 
 
-class Miller(object):
-    """
-    J.C.P. Miller,
-    Numerical Quadrature Over a Rectangular Domain in Two or More Dimensions.
-    Part 3: Quadrature of a Harmonic Integrand,
-    Mathematics of Computation,
-    Vol. 14, No. 71 (Jul., 1960), pp. 240-248,
-    <https://doi.org/10.2307/2003163>.
+def Miller(symbolic=False):
+    frac = sympy.Rational if symbolic else lambda x, y: x / y
 
-    This scheme is exact for harmonic integrands of degree <= 11.
-    """
+    name = "Miller"
+    degree = 1
 
-    def __init__(self, symbolic=False):
-        frac = sympy.Rational if symbolic else lambda x, y: x / y
-
-        self.name = "Miller"
-        self.degree = 1
-
-        self.weights, self.points = concat(
-            zero(frac(250, 225)), symm_r0([-frac(8, 225), 1]), symm_s([frac(7, 900), 1])
-        )
-        self.weights *= 4
-        return
+    weights, points = concat(
+        zero(frac(250, 225)), symm_r0([-frac(8, 225), 1]), symm_s([frac(7, 900), 1])
+    )
+    weights *= 4
+    return QuadrilateralScheme(name, degree, weights, points)
