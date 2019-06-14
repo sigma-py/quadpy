8cbe7b057e538beb9b4815ed1a4f6611401d8bb7
Nico Schlömer <nico.schloemer@gmail.com>
2019-06-14 22:21:22 +0200

    modernize e2r2 schemes

--- quadpy/e2r2/stroud.py
+++ quadpy/e2r2/stroud.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 #
+"""
+Arthur Stroud,
+Approximate Calculation of Multiple Integrals,
+Prentice Hall, 1971.
+"""
 from __future__ import division
 
 import warnings
@@ -10,90 +15,81 @@ import sympy
 from .rabinowitz_richter import RabinowitzRichter
 from .stroud_secrest import StroudSecrest
 
+from .helpers import E2r2Scheme
 from ..helpers import untangle, fsd, pm
 
 
-class Stroud(object):
-    """
-    Arthur Stroud,
-    Approximate Calculation of Multiple Integrals,
-    Prentice Hall, 1971.
-    """
+def stroud_4_1(symbolic=False):
+    frac = sympy.Rational if symbolic else lambda x, y: x / y
+    sqrt = numpy.vectorize(sympy.sqrt) if symbolic else numpy.sqrt
+    cos = numpy.vectorize(sympy.cos) if symbolic else numpy.cos
+    sin = numpy.vectorize(sympy.sin) if symbolic else numpy.sin
+    pi = sympy.pi if symbolic else numpy.pi
 
-    def __init__(self, index, symbolic=False):
-        frac = sympy.Rational if symbolic else lambda x, y: x / y
-        sqrt = numpy.vectorize(sympy.sqrt) if symbolic else numpy.sqrt
-        cos = numpy.vectorize(sympy.cos) if symbolic else numpy.cos
-        sin = numpy.vectorize(sympy.sin) if symbolic else numpy.sin
-        pi = sympy.pi if symbolic else numpy.pi
-
-        self.name = "Stroud_E2r2({})".format(index)
-        if index == "4-1":
-            self.degree = 4
-
-            pts = (
-                sqrt(2)
-                * numpy.array(
-                    [
-                        [cos(2 * i * numpy.pi / 5) for i in range(5)],
-                        [sin(2 * i * numpy.pi / 5) for i in range(5)],
-                    ]
-                ).T
-            )
-            data = [(frac(1, 2), numpy.array([[0, 0]])), (frac(1, 10), pts)]
-
-            self.points, self.weights = untangle(data)
-            self.weights *= pi
-        elif index == "5-1":
-            self.set_data(StroudSecrest("V", symbolic=symbolic))
-        elif index == "5-2":
-            # Cartesian product Gauss formula
-            self.degree = 5
-
-            r = sqrt(frac(3, 2))
-            data = [
-                (frac(4, 9), numpy.array([[0, 0]])),
-                (frac(1, 9), fsd(2, (r, 1))),
-                (frac(1, 36), pm(2, r)),
+    pts = (
+        sqrt(2)
+        * numpy.array(
+            [
+                [cos(2 * i * numpy.pi / 5) for i in range(5)],
+                [sin(2 * i * numpy.pi / 5) for i in range(5)],
             ]
+        ).T
+    )
+    data = [(frac(1, 2), numpy.array([[0, 0]])), (frac(1, 10), pts)]
 
-            self.points, self.weights = untangle(data)
-            self.weights *= pi
-        elif index == "7-1":
-            self.set_data(StroudSecrest("VI", symbolic=symbolic))
-        elif index == "7-2":
-            # Cartesian product Gauss formula
-            warnings.warn("Stroud's Gauss product formula has degree 1, not 7.")
-            self.degree = 1
+    points, weights = untangle(data)
+    weights *= pi
+    return E2r2Scheme("Stroud 4-1", 4, weights, points)
 
-            sqrt6 = sqrt(6)
-            r, s = [sqrt((3 + p_m * sqrt6) / 2) for p_m in [+1, -1]]
-            A, B = [(5 - p_m * 2 * sqrt6) / 48 for p_m in [+1, -1]]
-            C = frac(1, 48)
 
-            data = [
-                (A, fsd(2, (r, 1))),
-                (B, fsd(2, (s, 1))),
-                (C, fsd(2, (r, 1), (s, 1))),
-            ]
+def stroud_5_2(symbolic=False):
+    frac = sympy.Rational if symbolic else lambda x, y: x / y
+    sqrt = numpy.vectorize(sympy.sqrt) if symbolic else numpy.sqrt
+    pi = sympy.pi if symbolic else numpy.pi
 
-            self.points, self.weights = untangle(data)
-            self.weights *= pi
-        elif index == "9-1":
-            self.set_data(RabinowitzRichter(1))
-        elif index == "11-1":
-            self.set_data(RabinowitzRichter(2))
-        elif index == "11-2":
-            self.set_data(RabinowitzRichter(3))
-        elif index == "13-1":
-            self.set_data(RabinowitzRichter(4))
-        else:
-            assert index == "15-1"
-            self.set_data(RabinowitzRichter(5))
-        return
+    # Cartesian product Gauss formula
+    r = sqrt(frac(3, 2))
+    data = [
+        (frac(4, 9), numpy.array([[0, 0]])),
+        (frac(1, 9), fsd(2, (r, 1))),
+        (frac(1, 36), pm(2, r)),
+    ]
 
-    def set_data(self, scheme):
-        self.degree = scheme.degree
-        self.weights = scheme.weights
-        self.points = scheme.points
-        return
+    points, weights = untangle(data)
+    weights *= pi
+    return E2r2Scheme("Stroud 5-2", 5, weights, points)
+
+
+def stroud_7_2(symbolic=False):
+    frac = sympy.Rational if symbolic else lambda x, y: x / y
+    sqrt = numpy.vectorize(sympy.sqrt) if symbolic else numpy.sqrt
+    pi = sympy.pi if symbolic else numpy.pi
+
+    # Cartesian product Gauss formula
+    sqrt6 = sqrt(6)
+    r, s = [sqrt((3 + p_m * sqrt6) / 2) for p_m in [+1, -1]]
+    A, B = [(5 - p_m * 2 * sqrt6) / 48 for p_m in [+1, -1]]
+    C = frac(1, 48)
+
+    data = [(A, fsd(2, (r, 1))), (B, fsd(2, (s, 1))), (C, fsd(2, (r, 1), (s, 1)))]
+
+    points, weights = untangle(data)
+    weights *= pi
+
+    # TODO find what's wrong
+    warnings.warn("Stroud's Gauss product formula has degree 1, not 7.")
+    return E2r2Scheme("Stroud 7-2", 1, weights, points)
+
+
+Stroud = {
+    "4-1": stroud_4_1,
+    "5-1": StroudSecrest["V"],
+    "5-2": stroud_5_2,
+    "7-1": StroudSecrest["VI"],
+    "7-2": stroud_7_2,
+    "9-1": RabinowitzRichter[1],
+    "11-1": RabinowitzRichter[2],
+    "11-2": RabinowitzRichter[3],
+    "13-1": RabinowitzRichter[4],
+    "15-1": RabinowitzRichter[5],
+}
--- quadpy/e2r2/stroud_secrest.py
+++ quadpy/e2r2/stroud_secrest.py
@@ -1,52 +1,58 @@
 # -*- coding: utf-8 -*-
 #
+"""
+A.H. Stroud and D. Secrest,
+Approximate integration formulas for certain spherically symmetric regions,
+Math. Comp. 17 (1963), 105-135,
+<https://doi.org/10.1090/S0025-5718-1963-0161473-0>.
+"""
 from __future__ import division
 
 import numpy
 import sympy
 
+from .helpers import E2r2Scheme
 from ..helpers import untangle, pm_array, pm_array0, fsd, pm
 
 
-class StroudSecrest(object):
-    """
-    A.H. Stroud and D. Secrest,
-    Approximate integration formulas for certain spherically symmetric regions,
-    Math. Comp. 17 (1963), 105-135,
-    <https://doi.org/10.1090/S0025-5718-1963-0161473-0>.
-    """
+def stroud_secrest_v(symbolic=False):
+    frac = sympy.Rational if symbolic else lambda x, y: x / y
+    sqrt = numpy.vectorize(sympy.sqrt) if symbolic else numpy.sqrt
+    pi = sympy.pi if symbolic else numpy.pi
 
-    def __init__(self, index, symbolic=False):
-        frac = sympy.Rational if symbolic else lambda x, y: x / y
-        sqrt = numpy.vectorize(sympy.sqrt) if symbolic else numpy.sqrt
-        pi = sympy.pi if symbolic else numpy.pi
+    nu = sqrt(2)
+    xi = nu / 2
+    eta = sqrt(6) / 2
+    A = frac(1, 2)
+    B = frac(1, 12)
 
-        if index == "V":
-            self.degree = 5
+    data = [
+        (A, numpy.array([[0, 0]])),
+        (B, pm_array0(2, [nu], [0])),
+        (B, pm_array([xi, eta])),
+    ]
 
-            nu = sqrt(2)
-            xi = nu / 2
-            eta = sqrt(6) / 2
-            A = frac(1, 2)
-            B = frac(1, 12)
+    points, weights = untangle(data)
+    weights *= pi
+    return E2r2Scheme("Stroud-Secrest V", 5, weights, points)
 
-            data = [
-                (A, numpy.array([[0, 0]])),
-                (B, pm_array0(2, [nu], [0])),
-                (B, pm_array([xi, eta])),
-            ]
-        else:
-            assert index == "VI"
-            self.degree = 7
 
-            sqrt5 = sqrt(5)
-            nu = sqrt(3)
-            xi, eta = [sqrt((9 - p_m * 3 * sqrt5) / 8) for p_m in [+1, -1]]
-            A = frac(1, 36)
-            B, C = [(5 + p_m * 2 * sqrt5) / 45 for p_m in [+1, -1]]
+def stroud_secrest_vi(symbolic=False):
+    frac = sympy.Rational if symbolic else lambda x, y: x / y
+    sqrt = numpy.vectorize(sympy.sqrt) if symbolic else numpy.sqrt
+    pi = sympy.pi if symbolic else numpy.pi
 
-            data = [(A, fsd(2, (nu, 1))), (B, pm(2, xi)), (C, pm(2, eta))]
+    sqrt5 = sqrt(5)
+    nu = sqrt(3)
+    xi, eta = [sqrt((9 - p_m * 3 * sqrt5) / 8) for p_m in [+1, -1]]
+    A = frac(1, 36)
+    B, C = [(5 + p_m * 2 * sqrt5) / 45 for p_m in [+1, -1]]
 
-        self.points, self.weights = untangle(data)
-        self.weights *= pi
-        return
+    data = [(A, fsd(2, (nu, 1))), (B, pm(2, xi)), (C, pm(2, eta))]
+
+    points, weights = untangle(data)
+    weights *= pi
+    return E2r2Scheme("Stroud-Secrest VI", 7, weights, points)
+
+
+StroudSecrest = {"V": stroud_secrest_v, "VI": stroud_secrest_vi}
