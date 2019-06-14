ff9fee072e9399dbf1fa2bd7c13640fa617c9fe3
Nico Schlömer <nico.schloemer@gmail.com>
2019-06-14 13:32:26 +0200

    modernize krylov

---
+++ quadpy/circle/helpers.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+#
+import collections
+
+CircleScheme = collections.namedtuple(
+    "CircleScheme", ["name", "degree", "weights", "points"]
+)
--- quadpy/circle/krylov.py
+++ quadpy/circle/krylov.py
@@ -1,29 +1,28 @@
 # -*- coding: utf-8 -*-
 #
+"""
+Pages 73-74 in
+
+V.I. Krylov,
+Approximate Calculation of Integrals,
+Macmillan, New York, 1962.
+(Translated from 1st Russian ed., 1959, by A.H. Stroud.)
+<https://books.google.de/books/about/Approximate_Calculation_of_Integrals.html?id=ELeRwR27IRIC&redir_esc=y>
+"""
 from __future__ import division
 
 import numpy
 import sympy
 
+from .helpers import CircleScheme
 
-class Krylov(object):
-    """
-    Pages 73-74 in
 
-    V.I. Krylov,
-    Approximate Calculation of Integrals,
-    Macmillan, New York, 1962.
-    (Translated from 1st Russian ed., 1959, by A.H. Stroud.)
-    <https://books.google.de/books/about/Approximate_Calculation_of_Integrals.html?id=ELeRwR27IRIC&redir_esc=y>
-    """
+def Krylov(n, symbolic=False):
+    cos = numpy.vectorize(sympy.cos) if symbolic else numpy.cos
+    sin = numpy.vectorize(sympy.sin) if symbolic else numpy.sin
+    pi = sympy.pi if symbolic else numpy.pi
 
-    def __init__(self, n, symbolic=False):
-        cos = numpy.vectorize(sympy.cos) if symbolic else numpy.cos
-        sin = numpy.vectorize(sympy.sin) if symbolic else numpy.sin
-        pi = sympy.pi if symbolic else numpy.pi
-
-        self.weights = numpy.full(n, 2 * pi / n)
-        alpha = 2 * numpy.arange(n) * pi / n
-        self.points = numpy.column_stack([cos(alpha), sin(alpha)])
-        self.degree = n - 1
-        return
+    weights = numpy.full(n, 2 * pi / n)
+    alpha = 2 * numpy.arange(n) * pi / n
+    points = numpy.column_stack([cos(alpha), sin(alpha)])
+    return CircleScheme("Krylov", n - 1, weights, points)
