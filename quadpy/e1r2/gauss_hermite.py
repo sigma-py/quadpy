6fa71af77070f6af4fad79d85db03a77aba355a1
Nico Schlömer <nico.schloemer@gmail.com>
2019-06-14 21:54:18 +0200

    modernize various schemes

--- quadpy/e1r2/gauss_hermite.py
+++ quadpy/e1r2/gauss_hermite.py
@@ -3,31 +3,28 @@
 import numpy
 import orthopy
 
+from .helpers import E1r2Scheme
 from ..tools import scheme_from_rc
 
 
-class GaussHermite(object):
+def GaussHermite(n, mode="numpy", decimal_places=None):
     """
     Gauss-Hermite quadrature for integrals of the form
 
         int_{-inf}^{+inf} exp(-x^2) f(x) dx.
     """
+    if mode == "numpy":
+        points, weights = numpy.polynomial.hermite.hermgauss(n)
+    else:
+        _, _, alpha, beta = orthopy.e1r2.recurrence_coefficients(
+            n, "monic", symbolic=True
+        )
 
-    def __init__(self, n, mode="numpy", decimal_places=None):
-        self.degree = 2 * n - 1
+        # For some reason, the parameters have to be adapted here.
+        # TODO find out why
+        beta[1:] /= 2
 
-        if mode == "numpy":
-            self.points, self.weights = numpy.polynomial.hermite.hermgauss(n)
-        else:
-            _, _, alpha, beta = orthopy.e1r2.recurrence_coefficients(
-                n, "monic", symbolic=True
-            )
-
-            # For some reason, the parameters have to be adapted here.
-            # TODO find out why
-            beta[1:] /= 2
-
-            self.points, self.weights = scheme_from_rc(
-                alpha, beta, mode=mode, decimal_places=decimal_places
-            )
-        return
+        points, weights = scheme_from_rc(
+            alpha, beta, mode=mode, decimal_places=decimal_places
+        )
+    return E1r2Scheme("Gauss-Hermite", 2 * n - 1, weights, points)
--- quadpy/e2r/stroud.py
+++ quadpy/e2r/stroud.py
@@ -10,12 +10,13 @@ from __future__ import division
 import numpy
 import sympy
 
-from . import rabinowitz_richter
-from . import stroud_secrest
+from .rabinowitz_richter import RabinowitzRichter
+from .stroud_secrest import StroudSecrest
+from .helpers import E2rScheme
 from ..helpers import untangle
 
 
-def _gen4_1(symbolic):
+def stroud_4_1(symbolic=False):
     frac = sympy.Rational if symbolic else lambda x, y: x / y
     sqrt = numpy.vectorize(sympy.sqrt) if symbolic else numpy.sqrt
     cos = numpy.vectorize(sympy.cos) if symbolic else numpy.cos
@@ -33,33 +34,33 @@ def _gen4_1(symbolic):
         ).T
     )
     data = [(frac(7, 10), numpy.array([[0, 0]])), (frac(3, 50), pts)]
-    return 4, data
+
+    points, weights = untangle(data)
+    weights *= 2 * pi
+    return E2rScheme("Stroud 4-1", 4, weights, points)
 
 
-# The boolean tells whether the factor 2*pi is already in the weights
-_gen = {
-    "4-1": (_gen4_1, False),
-    "5-1": (stroud_secrest.v, False),
-    "7-1": (stroud_secrest.vi, False),
-    "9-1": (rabinowitz_richter.gen1, True),
-    "11-1": (rabinowitz_richter.gen2, True),
-    "11-2": (rabinowitz_richter.gen3, True),
+# # The boolean tells whether the factor 2*pi is already in the weights
+# _gen = {
+#     "4-1": (_gen4_1, False),
+#     "5-1": (stroud_secrest.v, False),
+#     "7-1": (stroud_secrest.vi, False),
+#     "9-1": (rabinowitz_richter.gen1, True),
+#     "11-1": (rabinowitz_richter.gen2, True),
+#     "11-2": (rabinowitz_richter.gen3, True),
+#     # ERR misprint in Stroud copied from original article
+#     # '13-1': (rabinowitz_richter.gen4,
+#     "15-1": (rabinowitz_richter.gen5, True),
+# }
+
+Stroud = {
+    "4-1": stroud_4_1,
+    "5-1": StroudSecrest["V"],
+    "7-1": StroudSecrest["VI"],
+    "9-1": RabinowitzRichter[1],
+    "11-1": RabinowitzRichter[2],
+    "11-2": RabinowitzRichter[3],
     # ERR misprint in Stroud copied from original article
-    # '13-1': (rabinowitz_richter.gen4,
-    "15-1": (rabinowitz_richter.gen5, True),
+    # '13-1': RabinowitzRichter[4],
+    "15-1": RabinowitzRichter[5],
 }
-
-
-class Stroud(object):
-    keys = _gen.keys()
-
-    def __init__(self, key, symbolic=False):
-        self.name = "Stroud_E2r({})".format(key)
-        self.degree, data = _gen[key][0](symbolic)
-        weights_contain_2pi = _gen[key][1]
-        self.points, self.weights = untangle(data)
-
-        pi = sympy.pi if symbolic else numpy.pi
-        if not weights_contain_2pi:
-            self.weights *= 2 * pi
-        return
