19cd6b86e58294eb0418318a2993ca36014dff11
Nico Schlömer <nico.schloemer@gmail.com>
2019-06-15 17:36:04 +0200

    add missing files

---
+++ quadpy/e3r/helpers.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+#
+import collections
+
+E3rScheme = collections.namedtuple("E3rScheme", ["name", "degree", "weights", "points"])
---
+++ quadpy/e3r2/helpers.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+#
+import collections
+
+E3r2Scheme = collections.namedtuple(
+    "E3r2Scheme", ["name", "degree", "weights", "points"]
+)
