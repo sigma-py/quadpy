a60e6d36ee2c297d08ab06dfc8dfbe19b6a7740e
Nico Schlömer <nico.schloemer@gmail.com>
2019-06-15 17:36:04 +0200

    add missing files

---
+++ quadpy/e1r/helpers.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+#
+import collections
+
+E1rScheme = collections.namedtuple("E1rScheme", ["name", "degree", "weights", "points"])
---
+++ quadpy/e1r2/helpers.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+#
+import collections
+
+E1r2Scheme = collections.namedtuple(
+    "E1r2Scheme", ["name", "degree", "weights", "points"]
+)
