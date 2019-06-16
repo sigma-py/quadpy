55148284a289cd56aa6201e0fa66f9c76cf0e0e3
Nico Schlömer <nico.schloemer@gmail.com>
2019-06-16 18:31:10 +0200

    modernize quad::schmid

--- quadpy/quadrilateral/schmid.py
+++ quadpy/quadrilateral/schmid.py
@@ -20,86 +20,84 @@ from __future__ import division
 import numpy
 import sympy
 
-
-class Schmid2(object):
-    def __init__(self, symbolic=False):
-        frac = sympy.Rational if symbolic else lambda x, y: x / y
-        sqrt = sympy.sqrt if symbolic else numpy.sqrt
-        self.degree = 2
-        self.points = numpy.array(
-            [
-                [-sqrt(frac(1, 3)), +sqrt(frac(2, 3))],
-                [-sqrt(frac(1, 3)), -sqrt(frac(2, 3))],
-                [+sqrt(frac(1, 3)), 0],
-            ]
-        )
-        self.weights = numpy.array([frac(1, 4), frac(1, 4), frac(1, 2)])
-        self.weights *= 4
-        return
+from .helpers import QuadrilateralScheme
 
 
-class Schmid4(object):
-    def __init__(self, symbolic=False):
-        frac = sympy.Rational if symbolic else lambda x, y: x / y
-        sqrt = sympy.sqrt if symbolic else numpy.sqrt
-        self.degree = 4
-        self.points = numpy.array(
-            [
-                [0, (sqrt(3) + sqrt(15)) / 6],
-                [0, (sqrt(3) - sqrt(15)) / 6],
-                [+sqrt(15) / 5, (+sqrt(87) - 2 * sqrt(3)) / 15],
-                [-sqrt(15) / 5, (+sqrt(87) - 2 * sqrt(3)) / 15],
-                [+sqrt(15) / 5, (-sqrt(87) - 2 * sqrt(3)) / 15],
-                [-sqrt(15) / 5, (-sqrt(87) - 2 * sqrt(3)) / 15],
-            ]
-        )
-        self.weights = numpy.array(
-            [
-                frac(2, 9) - 2 * sqrt(5) / 45,
-                frac(2, 9) + 2 * sqrt(5) / 45,
-                frac(5, 36) + 5 * sqrt(29) / 18 / 29,
-                frac(5, 36) + 5 * sqrt(29) / 18 / 29,
-                frac(5, 36) - 5 * sqrt(29) / 18 / 29,
-                frac(5, 36) - 5 * sqrt(29) / 18 / 29,
-            ]
-        )
-        self.weights *= 4
-        return
+def schmid_2(symbolic=False):
+    frac = sympy.Rational if symbolic else lambda x, y: x / y
+    sqrt = sympy.sqrt if symbolic else numpy.sqrt
+
+    points = numpy.array(
+        [
+            [-sqrt(frac(1, 3)), +sqrt(frac(2, 3))],
+            [-sqrt(frac(1, 3)), -sqrt(frac(2, 3))],
+            [+sqrt(frac(1, 3)), 0],
+        ]
+    )
+    weights = numpy.array([frac(1, 4), frac(1, 4), frac(1, 2)])
+    weights *= 4
+    return QuadrilateralScheme("Schmid 2", 2, weights, points)
 
 
-class Schmid6(object):
-    def __init__(self):
-        self.degree = 6
-        self.points = numpy.array(
-            [
-                [+0.000000000000, +0.774596669241],
-                [+0.563604836881, -0.795508520349],
-                [+0.838331011044, +0.845091361153],
-                [+0.651030930900, +0.166755021097],
-                [-0.484792881050, -0.927694708202],
-                [-0.914603935097, -0.520771886130],
-                [-0.135220856964, -0.279191827433],
-                [-0.731697727745, +0.417391901524],
-                [-0.887824220291, +1.075479856096],
-                [+1.101172842321, -0.485302501018],
-            ]
-        )
-        self.weights = numpy.array(
-            [
-                0.140845070423,
-                0.113931725656,
-                0.049023075184,
-                0.168918151204,
-                0.063463914536,
-                0.066611011696,
-                0.214897708035,
-                0.145149421990,
-                0.014704280797,
-                0.022455640481,
-            ]
-        )
-        self.weights *= 4
-        return
+def schmid_4(symbolic=False):
+    frac = sympy.Rational if symbolic else lambda x, y: x / y
+    sqrt = sympy.sqrt if symbolic else numpy.sqrt
+
+    points = numpy.array(
+        [
+            [0, (sqrt(3) + sqrt(15)) / 6],
+            [0, (sqrt(3) - sqrt(15)) / 6],
+            [+sqrt(15) / 5, (+sqrt(87) - 2 * sqrt(3)) / 15],
+            [-sqrt(15) / 5, (+sqrt(87) - 2 * sqrt(3)) / 15],
+            [+sqrt(15) / 5, (-sqrt(87) - 2 * sqrt(3)) / 15],
+            [-sqrt(15) / 5, (-sqrt(87) - 2 * sqrt(3)) / 15],
+        ]
+    )
+    weights = numpy.array(
+        [
+            frac(2, 9) - 2 * sqrt(5) / 45,
+            frac(2, 9) + 2 * sqrt(5) / 45,
+            frac(5, 36) + 5 * sqrt(29) / 18 / 29,
+            frac(5, 36) + 5 * sqrt(29) / 18 / 29,
+            frac(5, 36) - 5 * sqrt(29) / 18 / 29,
+            frac(5, 36) - 5 * sqrt(29) / 18 / 29,
+        ]
+    )
+    weights *= 4
+    return QuadrilateralScheme("Schmid 4", 4, weights, points)
 
 
-Schmid = {2: Schmid2, 4: Schmid4, 6: Schmid6}
+def schmid_6(symbolic=False):
+    points = numpy.array(
+        [
+            [+0.000000000000, +0.774596669241],
+            [+0.563604836881, -0.795508520349],
+            [+0.838331011044, +0.845091361153],
+            [+0.651030930900, +0.166755021097],
+            [-0.484792881050, -0.927694708202],
+            [-0.914603935097, -0.520771886130],
+            [-0.135220856964, -0.279191827433],
+            [-0.731697727745, +0.417391901524],
+            [-0.887824220291, +1.075479856096],
+            [+1.101172842321, -0.485302501018],
+        ]
+    )
+    weights = numpy.array(
+        [
+            0.140845070423,
+            0.113931725656,
+            0.049023075184,
+            0.168918151204,
+            0.063463914536,
+            0.066611011696,
+            0.214897708035,
+            0.145149421990,
+            0.014704280797,
+            0.022455640481,
+        ]
+    )
+    weights *= 4
+    return QuadrilateralScheme("Schmid 6", 6, weights, points)
+
+
+Schmid = {2: schmid_2, 4: schmid_4, 6: schmid_6}
