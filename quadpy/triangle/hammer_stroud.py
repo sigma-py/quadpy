91fbf9f99e06143a49aa74b4df958e2c3ce85b51
Nico Schlömer <nico.schloemer@gmail.com>
2019-06-16 23:16:07 +0200

    more triangle modernization

--- quadpy/triangle/hammer_stroud.py
+++ quadpy/triangle/hammer_stroud.py
@@ -1,31 +1,30 @@
 # -*- coding: utf-8 -*-
 #
+"""
+Preston C. Hammer and Arthur H. Stroud,
+Numerical Evaluation of Multiple Integrals II,
+Math. Comp. 12 (1958), 272-280,
+<https://doi.org/10.1090/S0025-5718-1958-0102176-6>.
+"""
 from __future__ import division
 
 import sympy
 
-from .helpers import untangle2
+from .helpers import s2, TriangleScheme, s3, concat
 
 
-class HammerStroud(object):
-    """
-    Preston C. Hammer and Arthur H. Stroud,
-    Numerical Evaluation of Multiple Integrals II,
-    Math. Comp. 12 (1958), 272-280,
-    <https://doi.org/10.1090/S0025-5718-1958-0102176-6>.
-    """
+def hammer_stroud_2(symbolic=False):
+    frac = sympy.Rational if symbolic else lambda x, y: x / y
 
-    def __init__(self, degree, symbolic=False):
-        frac = sympy.Rational if symbolic else lambda x, y: x / y
+    weights, bary = s2([frac(1, 3), frac(1, 6)])
+    return TriangleScheme("Hammer-Stroud 2", 2, weights, bary)
 
-        self.name = "Hammer-Stroud"
-        self.degree = degree
-        if degree == 2:
-            data = {"s2": [[frac(1, 3), frac(1, 6)]]}
-        else:
-            assert degree == 3
-            data = {"s3": [[-frac(27, 48)]], "s2": [[+frac(25, 48), frac(1, 5)]]}
 
-        self.bary, self.weights = untangle2(data)
-        self.points = self.bary[:, 1:]
-        return
+def hammer_stroud_3(symbolic=False):
+    frac = sympy.Rational if symbolic else lambda x, y: x / y
+
+    weights, bary = concat(s3(-frac(27, 48)), s2([+frac(25, 48), frac(1, 5)]))
+    return TriangleScheme("Hammer-Stroud 3", 3, weights, bary)
+
+
+HammerStroud = {2: hammer_stroud_2, 3: hammer_stroud_3}
