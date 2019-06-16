9903037094bdb09a19da4b49fda6661dda796fc6
Nico Schlömer <nico.schloemer@gmail.com>
2019-06-16 14:47:29 +0200

    modernize some line segment schemes

--- quadpy/line_segment/gauss_legendre.py
+++ quadpy/line_segment/gauss_legendre.py
@@ -4,24 +4,22 @@ import numpy
 import orthopy
 
 from ..tools import scheme_from_rc
+from .helpers import LineSegmentScheme
 
 
-class GaussLegendre(object):
+def GaussLegendre(n, mode="numpy", decimal_places=None):
     """
     Gauss-Legendre quadrature.
     """
+    degree = 2 * n - 1
 
-    def __init__(self, n, mode="numpy", decimal_places=None):
-
-        self.degree = 2 * n - 1
-
-        if mode == "numpy":
-            self.points, self.weights = numpy.polynomial.legendre.leggauss(n)
-        else:
-            _, _, alpha, beta = orthopy.line_segment.recurrence_coefficients.legendre(
-                n, "monic", symbolic=True
-            )
-            self.points, self.weights = scheme_from_rc(
-                alpha, beta, mode=mode, decimal_places=decimal_places
-            )
-        return
+    if mode == "numpy":
+        points, weights = numpy.polynomial.legendre.leggauss(n)
+    else:
+        _, _, alpha, beta = orthopy.line_segment.recurrence_coefficients.legendre(
+            n, "monic", symbolic=True
+        )
+        points, weights = scheme_from_rc(
+            alpha, beta, mode=mode, decimal_places=decimal_places
+        )
+    return LineSegmentScheme("Gauss-Legendre", degree, weights, points)
