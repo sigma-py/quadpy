57aa4ead941420e9fd0e8db8d052504564c3cd5e
Nico Schlömer <nico.schloemer@gmail.com>
2019-06-16 14:47:40 +0200

    some hex fixes

--- quadpy/hexahedron/__init__.py
+++ quadpy/hexahedron/__init__.py
@@ -5,7 +5,7 @@ from .hammer_wymore import HammerWymore
 from .mustard_lyness_blatt import MustardLynessBlatt
 from .sadowsky import Sadowsky
 from .stroud import Stroud
-from .stroud1967 import Stroud1967
+from .stroud_1967 import Stroud_1967
 from .stroudn import StroudN
 from .tyler import Tyler
 
@@ -22,7 +22,7 @@ __all__ = [
     "MustardLynessBlatt",
     "Sadowsky",
     "Stroud",
-    "Stroud1967",
+    "Stroud_1967",
     "StroudN",
     "Tyler",
     "Product",
