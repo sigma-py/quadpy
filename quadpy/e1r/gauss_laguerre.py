4f984ab8e047d35568a6679acaed1beaedc8d621
Nico Schlömer <nico.schloemer@gmail.com>
2019-06-18 16:08:45 +0200

    clean up gauss-laguerre

--- quadpy/e1r/gauss_laguerre.py
+++ quadpy/e1r/gauss_laguerre.py
@@ -6,7 +6,7 @@ from .helpers import E1rScheme
 from ..tools import scheme_from_rc
 
 
-def GaussLaguerre(n, alpha=0, mode="numpy", decimal_places=None):
+def gauss_laguerre(n, alpha=0, mode="numpy", decimal_places=None):
     """
     Gauss-Laguerre quadrature for integrals of the form
 
@@ -14,4 +14,4 @@ def GaussLaguerre(n, alpha=0, mode="numpy", decimal_places=None):
     """
     _, _, a, b = orthopy.e1r.recurrence_coefficients(n, alpha, "monic", symbolic=True)
     points, weights = scheme_from_rc(a, b, mode=mode, decimal_places=decimal_places)
-    return E1rScheme("Gauss-Laguerre", 2 * n - 1, weights, points)
+    return E1rScheme("Gauss-Laguerre", weights, points, 2 * n - 1)
