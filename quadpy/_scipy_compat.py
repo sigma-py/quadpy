16c1ad458375fdb5cead9147d5b4f176403ba1a3
Nico Schlömer <nico.schloemer@gmail.com>
2019-11-04 13:13:01 +0100

    make format

--- quadpy/_scipy_compat.py
+++ quadpy/_scipy_compat.py
@@ -6,4 +6,5 @@ from .line_segment import integrate_adaptive
 def quad(f, a, b, args=(), epsabs=1.49e-08):
     def g(x):
         return f(x, *args)
+
     return integrate_adaptive(g, [a, b], eps=epsabs)
