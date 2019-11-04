a9ea17d274133491b4082ebc30c58f3cdff217dd
Nico Schlömer <nico.schloemer@gmail.com>
2019-11-04 13:04:40 +0100

    quad: vector-valued function

--- test/test_scipy.py
+++ test/test_scipy.py
@@ -2,8 +2,15 @@ import numpy
 import quadpy
 
 
-def test_scipy():
+def test_simple():
     val, err = quadpy.quad(lambda x: numpy.sin(x) - x, [0.0, 6.0])
 
     ref = -17.960170286650353
     assert abs(val - ref) < 1.0e-10 * abs(ref)
+
+
+def test_vector():
+    val, err = quadpy.quad(lambda x: [numpy.sin(x) - x, x], [0.0, 6.0])
+
+    ref = [-17.960170286650353, 18]
+    assert numpy.all(numpy.abs(val - ref) < 1.0e-10 * numpy.abs(ref))
