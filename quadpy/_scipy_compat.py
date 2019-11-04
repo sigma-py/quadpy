90b10410367af05aeb05ecd167aca947d317d215
Nico Schlömer <nico.schloemer@gmail.com>
2019-11-04 13:12:34 +0100

    better scipy compat

--- quadpy/_scipy_compat.py
+++ quadpy/_scipy_compat.py
@@ -1,5 +1,9 @@
 from .line_segment import integrate_adaptive
 
 
-def quad(f, interval):
-    return integrate_adaptive(f, interval, eps=1.0e-10)
+# compatibility for scipy.quad
+# https://docs.scipy.org/doc/scipy/reference/generated/scipy.integrate.quad.html
+def quad(f, a, b, args=(), epsabs=1.49e-08):
+    def g(x):
+        return f(x, *args)
+    return integrate_adaptive(g, [a, b], eps=epsabs)
