6e00579a762e49eb117c9907f1d8e28a60586d67
Nico Schlömer <nico.schloemer@gmail.com>
2020-04-27 18:54:48 +0200

    add mcnamee-stenger 5

--- quadpy/ncube/__init__.py
+++ quadpy/ncube/__init__.py
@@ -3,7 +3,7 @@ from ._dobrodeev1978 import dobrodeev_1978
 from ._ewing import ewing
 from ._hammer_stroud import hammer_stroud_1n, hammer_stroud_2n
 from ._helpers import NCubeScheme, ncube_points, transform
-from ._mcnamee_stenger import mcnamee_stenger_3
+from ._mcnamee_stenger import mcnamee_stenger_3, mcnamee_stenger_5
 from ._mustard_lyness_blatt import mustard_lyness_blatt
 from ._phillips import phillips
 from ._stroud import (
@@ -40,6 +40,7 @@ __all__ = [
     "hammer_stroud_1n",
     "hammer_stroud_2n",
     "mcnamee_stenger_3",
+    "mcnamee_stenger_5",
     "mustard_lyness_blatt",
     "phillips",
     "stroud_1957_2",
--- quadpy/ncube/_mcnamee_stenger.py
+++ quadpy/ncube/_mcnamee_stenger.py
@@ -33,3 +33,25 @@ def mcnamee_stenger_3(n):
     points, weights = untangle(data)
     # weights *= 2 ** n
     return NCubeScheme("McNamee-Stenger 3", n, weights, points, 3, _citation)
+
+
+def mcnamee_stenger_5(n):
+    I0 = 2 ** n
+    I2 = frac(2 ** n, 3)
+    I4 = frac(2 ** n, 5)
+    I22 = frac(2 ** n, 9)
+
+    u = sqrt(frac(I4, I2))
+    A0 = I0 - n * (I2 / I4) ** 2 * (I4 - frac(n - 1, 2) * I22)
+    A1 = frac(1, 2) * frac(I2, I4) ** 2 * (I4 - (n - 1) * I22)
+    A11 = frac(1, 4) * frac(I2, I4) ** 2 * I22
+
+    data = [
+        (A0, z(n)),
+        (A1, fsd(n, (u, 1))),
+        (A11, fsd(n, (u, 2))),
+    ]
+
+    points, weights = untangle(data)
+    # weights *= 2 ** n
+    return NCubeScheme("McNamee-Stenger 5", n, weights, points, 5, _citation)
