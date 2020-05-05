9bdccf63f0619d5e7e690a1671707b299509d1e0
Nico Schlömer <nico.schloemer@gmail.com>
2020-05-05 12:01:58 +0200

    give up on cools-haegemans for the ncube

--- quadpy/ncube/__init__.py
+++ quadpy/ncube/__init__.py
@@ -1,7 +1,6 @@
 from ._cools_haegemans import (
     cools_haegemans_1,
     cools_haegemans_2,
-    cools_haegemans_3,
 )
 from ._dobrodeev1970 import dobrodeev_1970
 from ._dobrodeev1978 import dobrodeev_1978
@@ -48,7 +47,6 @@ from ._tyler import tyler
 __all__ = [
     "cools_haegemans_1",
     "cools_haegemans_2",
-    "cools_haegemans_3",
     "dobrodeev_1970",
     "dobrodeev_1978",
     "ewing",
