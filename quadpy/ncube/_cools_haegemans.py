3ef5236c2b94a5a6e87188f50ae6603ffdfdad9e
Nico Schlömer <nico.schloemer@gmail.com>
2020-05-09 20:16:49 +0200

    save some time on cools-haegemans

--- quadpy/ncube/_cools_haegemans.py
+++ quadpy/ncube/_cools_haegemans.py
@@ -1,8 +1,9 @@
-import numpy
-from sympy import Rational as frac
-from sympy import sqrt
+import math
 
-from ..helpers import article, fsd, pm, untangle, z
+import numpy
+import sympy
+
+from ..helpers import article, fsd, pm, untangle, z, prod
 from ._helpers import NCubeScheme
 
 _citation = article(
@@ -16,17 +17,20 @@ _citation = article(
 )
 
 
-def _mu(j):
+def _mu(j, symbolic):
     # 1/2 int_{-1}^1 x^j
+    frac = sympy.Rational if symbolic else lambda a, b: a / b
     return frac(1, j + 1) if j % 2 == 0 else 0
 
 
-def cools_haegemans_1(n, delta2=1):
+def cools_haegemans_1(n, delta2=1, symbolic=False):
+    frac = sympy.Rational if symbolic else lambda a, b: a / b
+    sqrt = sympy.sqrt if symbolic else math.sqrt
     assert frac(1, 3) <= delta2
     m = 1
 
     w0 = frac(3 * delta2 - 1, 3 * delta2)
-    w = frac(_mu(2) ** m * _mu(0) ** (n - m), 2 ** n * delta2 ** m)
+    w = frac(_mu(2, symbolic) ** m * _mu(0, symbolic) ** (n - m), 2 ** n * delta2 ** m)
 
     data = [
         (w0, z(n)),
@@ -38,11 +42,13 @@ def cools_haegemans_1(n, delta2=1):
     return NCubeScheme("Cools-Haegemans 1", n, weights, points, 3, _citation)
 
 
-def cools_haegemans_2(n, delta2=1):
+def cools_haegemans_2(n, delta2=1, symbolic=False):
+    frac = sympy.Rational if symbolic else lambda a, b: a / b
+    sqrt = sympy.sqrt if symbolic else math.sqrt
     # assert frac(5, 11) <= delta2 <= frac(5 * n + 4, 3 * (5 * n - 4))
     m = 2
 
-    lmbdas2 = _gener(delta2, 1, _mu)
+    lmbdas2 = _gener(delta2, 1, _mu, symbolic)
     w0 = frac(
         -(15 * delta2 * n - 12 * delta2 - 5 * n - 4) * (3 * delta2 - 1),
         36 * delta2 ** 2,
@@ -135,15 +141,7 @@ def cools_haegemans_2(n, delta2=1):
 #     return NCubeScheme("Cools-Haegemans 3", n, weights, points, 7, _citation)
 
 
-def _prod(iterable):
-    from functools import reduce
-    import operator
-
-    # https://stackoverflow.com/a/7948307/353337
-    return reduce(operator.mul, iterable, 1)
-
-
-def _gener(delta2, m, mu):
+def _gener(delta2, m, mu, symbolic):
     # Computes the lambda_q from the article, eq. (9).
     lmbdas2 = []
     for q in range(1, m + 1):
@@ -153,10 +151,10 @@ def _gener(delta2, m, mu):
         else:
             coeffs = numpy.poly(lmbdas2)
 
-        a0 = [c * mu(2 * (q - k) + 2) for k, c in enumerate(coeffs)]
-        a1 = [c * mu(2 * (q - k)) for k, c in enumerate(coeffs)]
-        prod = _prod([1 - lmbda2 / delta2 for lmbda2 in lmbdas2])
-        a = sum(a0) - mu(2) ** (q + 1) / mu(0) ** q * prod
-        b = sum(a1) - mu(2) ** (q + 1) / mu(0) ** q * prod / delta2
+        a0 = [c * mu(2 * (q - k) + 2, symbolic) for k, c in enumerate(coeffs)]
+        a1 = [c * mu(2 * (q - k), symbolic) for k, c in enumerate(coeffs)]
+        prod_ = prod([1 - lmbda2 / delta2 for lmbda2 in lmbdas2])
+        a = sum(a0) - mu(2, symbolic) ** (q + 1) / mu(0, symbolic) ** q * prod_
+        b = sum(a1) - mu(2, symbolic) ** (q + 1) / mu(0, symbolic) ** q * prod_ / delta2
         lmbdas2.append(a / b)
     return lmbdas2
