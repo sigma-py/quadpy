4da53771997c89295eac653d2a9dfa9b669dd4f4
Nico Schlömer <nico.schloemer@gmail.com>
2020-05-10 16:18:19 +0200

    add comment

--- quadpy/line_segment/_helpers.py
+++ quadpy/line_segment/_helpers.py
@@ -79,6 +79,8 @@ class LineSegmentScheme:
         import matplotlib.pyplot as plt
 
         self.plot(*args, **kwargs)
+        # mpl keeps a hidden background patch that renders bbox_inches ineffective.
+        # keep an eye out for https://stackoverflow.com/q/61712551/353337
         plt.savefig(filename, transparent=True, bbox_inches="tight")
 
     def show(self, *args, **kwargs):
