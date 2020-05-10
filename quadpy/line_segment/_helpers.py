e8756beaaad2a49d485fa6cae25969d2b961ac78
Nico Schlömer <nico.schloemer@gmail.com>
2020-05-10 16:17:01 +0200

    add savefig

--- quadpy/line_segment/_helpers.py
+++ quadpy/line_segment/_helpers.py
@@ -75,6 +75,12 @@ class LineSegmentScheme:
         # The factor 0.5 is from the length of the reference line [-1, 1].
         return 0.5 * len_intervals * dot(fx, self.weights)
 
+    def savefig(self, filename, *args, **kwargs):
+        import matplotlib.pyplot as plt
+
+        self.plot(*args, **kwargs)
+        plt.savefig(filename, transparent=True, bbox_inches="tight")
+
     def show(self, *args, **kwargs):
         import matplotlib.pyplot as plt
 
