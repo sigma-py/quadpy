0742c15b14ba621ee150e6e247d46f70d7f270bf
Nico Schlömer <nico.schloemer@gmail.com>
2020-07-09 23:26:03 +0200

    move some functions over to orthopy

--- quadpy/tools/__init__.py
+++ quadpy/tools/__init__.py
@@ -1,20 +1,10 @@
 from .main import (
-    chebyshev,
-    chebyshev_modified,
     check_coefficients,
     coefficients_from_gauss,
-    golub_welsch,
-    integrate,
     scheme_from_rc,
-    stieltjes,
 )
 
 __all__ = [
-    "golub_welsch",
-    "stieltjes",
-    "chebyshev",
-    "chebyshev_modified",
-    "integrate",
     "coefficients_from_gauss",
     "check_coefficients",
     "scheme_from_rc",
