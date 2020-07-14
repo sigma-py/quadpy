5257cf82863731ba2f317bbd34c2efb840eed97f
Nico Schlömer <nico.schloemer@gmail.com>
2020-07-14 10:06:21 +0200

    clean up bazant-oh

--- quadpy/u3/_bazant_oh/__init__.py
+++ quadpy/u3/_bazant_oh/__init__.py
@@ -1,6 +1,5 @@
 import json
-import os
-import re
+import pathlib
 
 from ...helpers import article
 from .._helpers import U3Scheme, cartesian_to_spherical, untangle2
@@ -17,17 +16,15 @@ source = article(
 )
 
 
-def _read(index, tol):
-    name = f"BazantOh({index})"
+def _read(filename):
+    this_dir = pathlib.Path(__file__).resolve().parent
 
-    this_dir = os.path.dirname(os.path.realpath(__file__))
-
-    m = re.match("([0-9]+)([a-z]*)", index)
-    filename = "bazant_oh_{:03d}{}.json".format(int(m.group(1)), m.group(2))
-    with open(os.path.join(this_dir, filename), "r") as f:
+    with open(this_dir / filename, "r") as f:
         data = json.load(f)
 
     degree = data.pop("degree")
+    name = data.pop("name")
+    tol = data.pop("test_tolerance")
 
     points, weights = untangle2(data)
     theta_phi = cartesian_to_spherical(points)
@@ -35,12 +32,12 @@ def _read(index, tol):
 
 
 def bazant_oh_09():
-    return _read("9", 5.808e-12)
+    return _read("bazant_oh_009.json")
 
 
 def bazant_oh_11():
-    return _read("11", 3.361e-12)
+    return _read("bazant_oh_011.json")
 
 
 def bazant_oh_13():
-    return _read("13", 5.033e-12)
+    return _read("bazant_oh_013.json")
