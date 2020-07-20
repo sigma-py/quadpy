0df67dcfc630cdf6ade7cf623282eb95d3ed9f95
Nico Schlömer <nico.schloemer@gmail.com>
2020-07-20 20:52:02 +0200

    t2: more json

--- quadpy/t2/_berntsen_espelid/__init__.py
+++ quadpy/t2/_berntsen_espelid/__init__.py
@@ -1,8 +1,7 @@
 import pathlib
-from mpmath import mp
 
 from ...helpers import article, techreport
-from .._helpers import T2Scheme, concat, s1, s2, s3, _read
+from .._helpers import _read
 
 source = techreport(
     authors=["J. Berntsen", "T.O. Espelid"],
@@ -33,58 +32,6 @@ def dcutri(mpmath=False):
 
 def berntsen_espelid_1(mpmath=False):
     return _read(this_dir / "berntsen_espelid_1.json", source)
-    flt = mp.mpf if mpmath else float
-
-    mp.dps = 30
-    weights, points = concat(
-        s3(flt("0.051739766065744133555179145422")),
-        s2(
-            [
-                flt("0.008007799555564801597804123460"),
-                flt("0.024862168537947217274823955239"),
-            ],
-            [
-                flt("0.046868898981821644823226732071"),
-                flt("0.414192542538082326221847602214"),
-            ],
-            [
-                flt("0.046590940183976487960361770070"),
-                flt("0.230293878161404779868453507244"),
-            ],
-            [
-                flt("0.031016943313796381407646220131"),
-                flt("0.113919981661733719124857214943"),
-            ],
-            [
-                flt("0.010791612736631273623178240136"),
-                flt("0.495457300025082323058213517632"),
-            ],
-            [
-                flt("0.032195534242431618819414482205"),
-                flt("0.468861354847056503251458179727"),
-            ],
-        ),
-        s1(
-            [
-                flt("0.015445834210701583817692900053"),
-                flt("0.022076289653624405142446876931"),
-                flt("0.851306504174348550389457672223"),
-            ],
-            [
-                flt("0.017822989923178661888748319485"),
-                flt("0.018620522802520968955913511549"),
-                flt("0.689441970728591295496647976487"),
-            ],
-            [
-                flt("0.037038683681384627918546472190"),
-                flt("0.096506481292159228736516560903"),
-                flt("0.635867859433372768286976979827"),
-            ],
-        ),
-    )
-    return T2Scheme(
-        "Berntsen-Espelid 1 (DCUTRI)", weights, points, 13, source, 1.516e-12
-    )
 
 
 def berntsen_espelid_2(mpmath=False):
---
+++ quadpy/t2/_cubtri/__init__.py
@@ -0,0 +1,23 @@
+import pathlib
+
+from ...helpers import article
+from .._helpers import _read
+
+source = article(
+    authors=["D.P. Laurie"],
+    title="Algorithm 584: CUBTRI: Automatic Cubature over a Triangle",
+    journal="ACM Trans. Math. Softw.",
+    month="jun",
+    year="1982",
+    url="https://doi.org/10.1145/355993.356001",
+)
+
+this_dir = pathlib.Path(__file__).resolve().parent
+
+
+def cubtri():
+    """
+    Se also
+    https://people.sc.fsu.edu/~jburkardt/datasets/quadrature_rules_tri/quadrature_rules_tri.html
+    """
+    return _read(this_dir / "cubtri.json", source)
