a9294079bb124bceb8226d4f8359b33e4349aad8
Nico Schlömer <nico.schloemer@gmail.com>
2020-07-23 17:58:22 +0200

    test fixes

--- test/test_e3r.py
+++ test/test_e3r.py
@@ -1,11 +1,11 @@
 import accupy
+import ndim
 import numpy
 import pytest
 from helpers import check_degree
 from matplotlib import pyplot as plt
 
 import quadpy
-from quadpy.enr._helpers import integrate_monomial_over_enr
 
 
 @pytest.mark.parametrize(
@@ -26,7 +26,7 @@ def test_scheme(scheme):
 
     degree, err = check_degree(
         lambda poly: scheme.integrate(poly, dot=accupy.fdot),
-        integrate_monomial_over_enr,
+        ndim.enr.integrate_monomial,
         3,
         scheme.degree + 1,
         tol=scheme.test_tolerance,
