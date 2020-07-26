b31549f82d4ec0a29316b744997a016bf96f4618
Nico Schlömer <nico.schloemer@gmail.com>
2020-07-26 23:26:51 +0200

    convert vioreanu-rokhlin to new format

--- quadpy/t2/_vioreanu_rokhlin/__init__.py
+++ quadpy/t2/_vioreanu_rokhlin/__init__.py
@@ -1,8 +1,7 @@
-import json
 import pathlib
 
 from ...helpers import article
-from .._helpers import T2Scheme, untangle2
+from .._helpers import _read
 
 source = article(
     authors=["B. Vioreanu", "V. Rokhlin"],
@@ -21,95 +20,81 @@ source = article(
 this_dir = pathlib.Path(__file__).resolve().parent
 
 
-def _read(index, tol=1.0e-14):
-    # Use JSON instead of YAML because
-    #  * a parse is in the Python standard library, and
-    #  * it's _much_ faster to parse <https://stackoverflow.com/a/50685946/353337>
-    filename = f"vr{index:02d}.json"
-    with open(this_dir / filename, "r") as f:
-        data = json.load(f)
-
-    degree = data["degree"]
-    points, weights = untangle2(data)
-    weights /= 2
-    return T2Scheme(f"Vioreanu-Rokhlin {index}", weights, points, degree, source, tol)
-
-
 def vioreanu_rokhlin_00():
-    return _read(0)
+    return _read(this_dir / "vr00.json", source)
 
 
 def vioreanu_rokhlin_01():
-    return _read(1)
+    return _read(this_dir / "vr01.json", source)
 
 
 def vioreanu_rokhlin_02():
-    return _read(2)
+    return _read(this_dir / "vr02.json", source)
 
 
 def vioreanu_rokhlin_03():
-    return _read(3)
+    return _read(this_dir / "vr03.json", source)
 
 
 def vioreanu_rokhlin_04():
-    return _read(4)
+    return _read(this_dir / "vr04.json", source)
 
 
 def vioreanu_rokhlin_05():
-    return _read(5)
+    return _read(this_dir / "vr05.json", source)
 
 
 def vioreanu_rokhlin_06():
-    return _read(6)
+    return _read(this_dir / "vr06.json", source)
 
 
 def vioreanu_rokhlin_07():
-    return _read(7)
+    return _read(this_dir / "vr07.json", source)
 
 
 def vioreanu_rokhlin_08():
-    return _read(8)
+    return _read(this_dir / "vr08.json", source)
 
 
 def vioreanu_rokhlin_09():
-    return _read(9)
+    return _read(this_dir / "vr09.json", source)
 
 
 def vioreanu_rokhlin_10():
-    return _read(10)
+    return _read(this_dir / "vr10.json", source)
 
 
 def vioreanu_rokhlin_11():
-    return _read(11)
+    return _read(this_dir / "vr11.json", source)
 
 
 def vioreanu_rokhlin_12():
-    return _read(12)
+    return _read(this_dir / "vr12.json", source)
 
 
 def vioreanu_rokhlin_13():
-    return _read(13, 3.045e-14)
+    return _read(this_dir / "vr13.json", source)
 
 
 def vioreanu_rokhlin_14():
-    return _read(14, 2.055e-14)
+    return _read(this_dir / "vr14.json", source)
 
 
 def vioreanu_rokhlin_15():
-    return _read(15, 2.112e-14)
+    return _read(this_dir / "vr15.json", source)
 
 
 def vioreanu_rokhlin_16():
-    return _read(16, 2.523e-13)
+    return _read(this_dir / "vr16.json", source)
 
 
 def vioreanu_rokhlin_17():
-    return _read(17)
+    return _read(this_dir / "vr17.json", source)
 
 
 def vioreanu_rokhlin_18():
-    return _read(18, 2.895e-14)
+    return _read(this_dir / "vr18.json", source)
 
 
 def vioreanu_rokhlin_19():
-    return _read(19, 4.085e-12)
+    return _read(this_dir / "vr19.json", source)
