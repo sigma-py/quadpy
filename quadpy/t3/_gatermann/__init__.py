f0842f19c7cb840a6377733ac2deb3138b1c5064
Nico Schlömer <nico.schloemer@gmail.com>
2020-07-29 17:27:05 +0200

    one more t3

---
+++ quadpy/t3/_gatermann/__init__.py
@@ -0,0 +1,19 @@
+import pathlib
+
+from ...helpers import article
+from .._helpers import _read
+
+source = article(
+    authors=["Karin Gatermann"],
+    title="Linear Representations of Finite Groups and The Ideal Theoretical Construction of G-Invariant Cubature Formulas",
+    journal="Numerical Integration",
+    pages="25-35",
+    note="Part of the NATO ASI Series book series (ASIC, volume 357)",
+)
+
+
+this_dir = pathlib.Path(__file__).resolve().parent
+
+
+def gatermann():
+    return _read(this_dir / "gatermann.json", source)
