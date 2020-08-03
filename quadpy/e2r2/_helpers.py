cae0b87f0365768f5a0524d28eaa50c05743bd13
Nico Schlömer <nico.schloemer@gmail.com>
2020-08-03 10:04:34 +0200

    more unused code

--- quadpy/e2r2/_helpers.py
+++ quadpy/e2r2/_helpers.py
@@ -46,24 +46,6 @@ class E2r2Scheme(QuadratureScheme):
         return ref_vol * dot(f(flt(self.points).T), flt(self.weights))
 
 
-def _z():
-    return numpy.array([[0.0, 0.0]])
-
-
-def _s8(a, b):
-    return numpy.array(
-        [[+a, +b], [-a, +b], [+a, -b], [-a, -b], [+b, +a], [-b, +a], [+b, -a], [-b, -a]]
-    )
-
-
-def _s4(a):
-    return numpy.array([[+a, +a], [-a, +a], [+a, -a], [-a, -a]])
-
-
-def _s40(a):
-    return numpy.array([[+a, 0.0], [-a, 0.0], [0.0, +a], [0.0, -a]])
-
-
 def _zero(data):
     return numpy.array([[0.0], [0.0]])
 
