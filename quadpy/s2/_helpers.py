7a3b4dcd5a715ba48e2b8e7465c5e5f8d53b89fd
Nico Schlömer <nico.schloemer@gmail.com>
2020-08-03 10:13:01 +0200

    remove more unused code

--- quadpy/s2/_helpers.py
+++ quadpy/s2/_helpers.py
@@ -37,36 +37,6 @@ class S2Scheme(QuadratureScheme):
         return numpy.pi * numpy.array(radius) ** 2 * dot(ff, self.weights)
 
 
-def _z():
-    return numpy.array([[0.0, 0.0]])
-
-
-def _s8(a, b):
-    return numpy.array(
-        [[+a, +b], [-a, +b], [+a, -b], [-a, -b], [+b, +a], [-b, +a], [+b, -a], [-b, -a]]
-    )
-
-
-def _s4(a):
-    return numpy.array([[+a, +a], [-a, +a], [+a, -a], [-a, -a]])
-
-
-def _s40(a):
-    return numpy.array([[+a, 0.0], [-a, 0.0], [0.0, +a], [0.0, -a]])
-
-
-def _pm(a, b):
-    return numpy.array([[+a, +b], [-a, +b], [+a, -b], [-a, -b]])
-
-
-def _pmx(x):
-    return numpy.array([[+x, 0], [-x, 0]])
-
-
-def _pmy(y):
-    return numpy.array([[0, +y], [0, -y]])
-
-
 def _pma_alt(data):
     a = numpy.asarray(data)
     points = numpy.array([[+a, +a], [-a, +a], [+a, -a], [-a, -a]])
