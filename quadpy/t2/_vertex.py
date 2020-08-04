ebd553a70213b1fabef2736b79a4b9b921414677
Nico Schlömer <nico.schloemer@gmail.com>
2020-08-04 17:32:01 +0200

    t2: vertex modernization

--- quadpy/t2/_vertex.py
+++ quadpy/t2/_vertex.py
@@ -1,8 +1,9 @@
 from sympy import Rational as frac
 
-from ._helpers import T2Scheme, s2
+from ._helpers import T2Scheme, expand_symmetries
 
 
 def vertex():
-    weights, points = s2([frac(1, 3), 0])
+    d = {"s2": [[frac(1, 3)], [0]]}
+    points, weights = expand_symmetries(d)
     return T2Scheme("Vertex scheme", weights, points, 1)
