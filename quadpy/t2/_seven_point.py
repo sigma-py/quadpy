9abf0d308a162096be86f9d59420369c5655ea41
Nico Schlömer <nico.schloemer@gmail.com>
2020-08-04 17:28:04 +0200

    modernize seven-point scheme

--- quadpy/t2/_seven_point.py
+++ quadpy/t2/_seven_point.py
@@ -1,10 +1,9 @@
 from sympy import Rational as frac
 
-from ._helpers import T2Scheme, concat, s2, s3
+from ._helpers import T2Scheme, expand_symmetries
 
 
 def seven_point():
-    weights, points = concat(
-        s3(frac(9, 20)), s2([frac(1, 20), 0], [frac(2, 15), frac(1, 2)])
-    )
+    d = {"s3": [[frac(9, 20)]], "s2": [[frac(1, 20), frac(2, 15)], [0, frac(1, 2)]]}
+    points, weights = expand_symmetries(d)
     return T2Scheme("Seven-point scheme", weights, points, 3)
