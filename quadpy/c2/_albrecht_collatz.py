ff3d2317d8fabd6b5a814fc7ced19451b92035d5
Nico Schlömer <nico.schloemer@gmail.com>
2020-08-11 19:28:32 +0200

    c2: modernize albrecht-collatz

--- quadpy/c2/_albrecht_collatz.py
+++ quadpy/c2/_albrecht_collatz.py
@@ -2,7 +2,7 @@ from sympy import Rational as frac
 from sympy import sqrt
 
 from ..helpers import article
-from ._helpers import C2Scheme, concat, pm, pm2, symm_r0, symm_s, zero
+from ._helpers import C2Scheme, expand_symmetries
 
 source = article(
     authors=["J. Albrecht", "L. Collatz"],
@@ -17,9 +17,12 @@ source = article(
 
 
 def albrecht_collatz_1():
-    weights, points = concat(
-        zero(frac(5, 12)), symm_r0([frac(1, 8), 1]), symm_s([frac(1, 48), 1])
-    )
+    d = {
+        "zero": [[frac(5, 12)]],
+        "symm_r0": [[frac(1, 8)], [1]],
+        "symm_s": [[frac(1, 48)], [1]],
+    }
+    points, weights = expand_symmetries(d)
     return C2Scheme("Albrecht-Collatz 1", weights, points, 3, source, 4.442e-16)
 
 
@@ -27,26 +30,35 @@ def albrecht_collatz_2():
     r = sqrt(frac(3, 5))
     s = sqrt(frac(1, 3))
     t = sqrt(frac(14, 15))
-    weights, points = concat(
-        zero(frac(2, 7)), pm([frac(5, 63), 0, t]), pm2([frac(5, 36), r, s])
-    )
+    d = {
+        "zero": [[frac(2, 7)]],
+        "pm": [[frac(5, 63)], [0], [t]],
+        "pm2": [[frac(5, 36)], [r], [s]],
+    }
+    points, weights = expand_symmetries(d)
     return C2Scheme("Albrecht-Collatz 2", weights, points, 5, source, 4.627e-16)
 
 
 def albrecht_collatz_3():
     r = sqrt(frac(7, 15))
     s, t = [sqrt((7 + i * sqrt(24)) / 15) for i in [+1, -1]]
-    weights, points = concat(
-        zero(frac(2, 7)),
-        pm([frac(25, 168), r, r], [frac(5, 48), +s, -t], [frac(5, 48), +t, -s]),
-    )
+    d = {
+        "zero": [[frac(2, 7)]],
+        "pm": [
+            [frac(25, 168), frac(5, 48), frac(5, 48)],
+            [r, s, t],
+            [r, -t, -s]
+        ]
+    }
+    points, weights = expand_symmetries(d)
     return C2Scheme("Albrecht-Collatz 3", weights, points, 5, source, 4.442e-16)
 
 
 def albrecht_collatz_4():
-    weights, points = concat(
-        zero(frac(2, 45)),
-        symm_r0([frac(2, 45), 1]),
-        symm_s([frac(1, 60), 1], [frac(8, 45), frac(1, 2)]),
-    )
+    d = {
+        "zero": [[frac(2, 45)]],
+        "symm_r0": [[frac(2, 45)], [1]],
+        "symm_s": [[frac(1, 60), frac(8, 45)], [1, frac(1, 2)]],
+    }
+    points, weights = expand_symmetries(d)
     return C2Scheme("Albrecht-Collatz 4", weights, points, 5, source, 8.883e-16)
