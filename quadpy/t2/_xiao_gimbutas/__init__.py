a4474c43fc0c816039168d104297f18d2cc8d843
Nico Schlömer <nico.schloemer@gmail.com>
2020-08-11 18:27:13 +0200

    modermize xiao-gimbutas

--- quadpy/t2/_xiao_gimbutas/__init__.py
+++ quadpy/t2/_xiao_gimbutas/__init__.py
@@ -2,7 +2,7 @@ import json
 import pathlib
 
 from ...helpers import article
-from .._helpers import T2Scheme, untangle2
+from .._helpers import _read
 
 source = article(
     authors=["Hong Xiao", "Zydrunas Gimbutas"],
@@ -21,213 +21,201 @@ source = article(
 this_dir = pathlib.Path(__file__).resolve().parent
 
 
-# TODO mpmath
-def _read(degree):
-    filename = f"xg{degree:02d}.json"
-    with open(this_dir / filename, "r") as f:
-        data = json.load(f)
-
-    degree = data["degree"]
-    name = f"Xiao-Gimbutas {degree}"
-    points, weights = untangle2(data)
-    return T2Scheme(name, weights, points, degree, source)
-
-
 def xiao_gimbutas_01():
-    return _read(1)
+    return _read(this_dir / "xg01.json", source)
 
 
 def xiao_gimbutas_02():
-    return _read(2)
+    return _read(this_dir / "xg02.json", source)
 
 
 def xiao_gimbutas_03():
-    return _read(3)
+    return _read(this_dir / "xg03.json", source)
 
 
 def xiao_gimbutas_04():
-    return _read(4)
+    return _read(this_dir / "xg04.json", source)
 
 
 def xiao_gimbutas_05():
-    return _read(5)
+    return _read(this_dir / "xg05.json", source)
 
 
 def xiao_gimbutas_06():
-    return _read(6)
+    return _read(this_dir / "xg06.json", source)
 
 
 def xiao_gimbutas_07():
-    return _read(7)
+    return _read(this_dir / "xg07.json", source)
 
 
 def xiao_gimbutas_08():
-    return _read(8)
+    return _read(this_dir / "xg08.json", source)
 
 
 def xiao_gimbutas_09():
-    return _read(9)
+    return _read(this_dir / "xg09.json", source)
 
 
 def xiao_gimbutas_10():
-    return _read(10)
+    return _read(this_dir / "xg10.json", source)
 
 
 def xiao_gimbutas_11():
-    return _read(11)
+    return _read(this_dir / "xg11.json", source)
 
 
 def xiao_gimbutas_12():
-    return _read(12)
+    return _read(this_dir / "xg12.json", source)
 
 
 def xiao_gimbutas_13():
-    return _read(13)
+    return _read(this_dir / "xg13.json", source)
 
 
 def xiao_gimbutas_14():
-    return _read(14)
+    return _read(this_dir / "xg14.json", source)
 
 
 def xiao_gimbutas_15():
-    return _read(15)
+    return _read(this_dir / "xg15.json", source)
 
 
 def xiao_gimbutas_16():
-    return _read(16)
+    return _read(this_dir / "xg16.json", source)
 
 
 def xiao_gimbutas_17():
-    return _read(17)
+    return _read(this_dir / "xg17.json", source)
 
 
 def xiao_gimbutas_18():
-    return _read(18)
+    return _read(this_dir / "xg18.json", source)
 
 
 def xiao_gimbutas_19():
-    return _read(19)
+    return _read(this_dir / "xg19.json", source)
 
 
 def xiao_gimbutas_20():
-    return _read(20)
+    return _read(this_dir / "xg20.json", source)
 
 
 def xiao_gimbutas_21():
-    return _read(21)
+    return _read(this_dir / "xg21.json", source)
 
 
 def xiao_gimbutas_22():
-    return _read(22)
+    return _read(this_dir / "xg22.json", source)
 
 
 def xiao_gimbutas_23():
-    return _read(23)
+    return _read(this_dir / "xg23.json", source)
 
 
 def xiao_gimbutas_24():
-    return _read(24)
+    return _read(this_dir / "xg24.json", source)
 
 
 def xiao_gimbutas_25():
-    return _read(25)
+    return _read(this_dir / "xg25.json", source)
 
 
 def xiao_gimbutas_26():
-    return _read(26)
+    return _read(this_dir / "xg26.json", source)
 
 
 def xiao_gimbutas_27():
-    return _read(27)
+    return _read(this_dir / "xg27.json", source)
 
 
 def xiao_gimbutas_28():
-    return _read(28)
+    return _read(this_dir / "xg28.json", source)
 
 
 def xiao_gimbutas_29():
-    return _read(29)
+    return _read(this_dir / "xg29.json", source)
 
 
 def xiao_gimbutas_30():
-    return _read(30)
+    return _read(this_dir / "xg30.json", source)
 
 
 def xiao_gimbutas_31():
-    return _read(31)
+    return _read(this_dir / "xg31.json", source)
 
 
 def xiao_gimbutas_32():
-    return _read(32)
+    return _read(this_dir / "xg32.json", source)
 
 
 def xiao_gimbutas_33():
-    return _read(33)
+    return _read(this_dir / "xg33.json", source)
 
 
 def xiao_gimbutas_34():
-    return _read(34)
+    return _read(this_dir / "xg34.json", source)
 
 
 def xiao_gimbutas_35():
-    return _read(35)
+    return _read(this_dir / "xg35.json", source)
 
 
 def xiao_gimbutas_36():
-    return _read(36)
+    return _read(this_dir / "xg36.json", source)
 
 
 def xiao_gimbutas_37():
-    return _read(37)
+    return _read(this_dir / "xg37.json", source)
 
 
 def xiao_gimbutas_38():
-    return _read(38)
+    return _read(this_dir / "xg38.json", source)
 
 
 def xiao_gimbutas_39():
-    return _read(39)
+    return _read(this_dir / "xg39.json", source)
 
 
 def xiao_gimbutas_40():
-    return _read(40)
+    return _read(this_dir / "xg40.json", source)
 
 
 def xiao_gimbutas_41():
-    return _read(41)
+    return _read(this_dir / "xg41.json", source)
 
 
 def xiao_gimbutas_42():
-    return _read(42)
+    return _read(this_dir / "xg42.json", source)
 
 
 def xiao_gimbutas_43():
-    return _read(43)
+    return _read(this_dir / "xg43.json", source)
 
 
 def xiao_gimbutas_44():
-    return _read(44)
+    return _read(this_dir / "xg44.json", source)
 
 
 def xiao_gimbutas_45():
-    return _read(45)
+    return _read(this_dir / "xg45.json", source)
 
 
 def xiao_gimbutas_46():
-    return _read(46)
+    return _read(this_dir / "xg46.json", source)
 
 
 def xiao_gimbutas_47():
-    return _read(47)
+    return _read(this_dir / "xg47.json", source)
 
 
 def xiao_gimbutas_48():
-    return _read(48)
+    return _read(this_dir / "xg48.json", source)
 
 
 def xiao_gimbutas_49():
-    return _read(49)
+    return _read(this_dir / "xg49.json", source)
 
 
 def xiao_gimbutas_50():
-    return _read(50)
+    return _read(this_dir / "xg50.json", source)
