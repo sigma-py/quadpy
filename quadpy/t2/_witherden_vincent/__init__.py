80b90fb52b24d62d7c052d485a88057fd5f4a371
Nico Schlömer <nico.schloemer@gmail.com>
2020-08-11 18:17:47 +0200

    t2: modernize witherden-vincent

--- quadpy/t2/_witherden_vincent/__init__.py
+++ quadpy/t2/_witherden_vincent/__init__.py
@@ -1,8 +1,7 @@
-import json
 import pathlib
 
 from ...helpers import article
-from .._helpers import T2Scheme, untangle2
+from .._helpers import _read
 
 source = article(
     authors=["F.D. Witherden", "P.E. Vincent"],
@@ -19,86 +18,77 @@ source = article(
 this_dir = pathlib.Path(__file__).resolve().parent
 
 
-def _read(filename):
-    with open(this_dir / filename, "r") as f:
-        data = json.load(f)
-
-    degree = data["degree"]
-    points, weights = untangle2(data)
-    return weights, points, degree, source
-
-
 def witherden_vincent_01():
-    return T2Scheme("Witherden-Vincent 1", *_read("wv01.json"))
+    return _read(this_dir / "wv01.json", source)
 
 
 def witherden_vincent_02():
-    return T2Scheme("Witherden-Vincent 2", *_read("wv02.json"))
+    return _read(this_dir / "wv02.json", source)
 
 
 def witherden_vincent_04():
-    return T2Scheme("Witherden-Vincent 4", *_read("wv04.json"))
+    return _read(this_dir / "wv04.json", source)
 
 
 def witherden_vincent_05():
-    return T2Scheme("Witherden-Vincent 5", *_read("wv05.json"))
+    return _read(this_dir / "wv05.json", source)
 
 
 def witherden_vincent_06():
-    return T2Scheme("Witherden-Vincent 6", *_read("wv06.json"))
+    return _read(this_dir / "wv06.json", source)
 
 
 def witherden_vincent_07():
-    return T2Scheme("Witherden-Vincent 7", *_read("wv07.json"))
+    return _read(this_dir / "wv07.json", source)
 
 
 def witherden_vincent_08():
-    return T2Scheme("Witherden-Vincent 8", *_read("wv08.json"))
+    return _read(this_dir / "wv08.json", source)
 
 
 def witherden_vincent_09():
-    return T2Scheme("Witherden-Vincent 9", *_read("wv09.json"))
+    return _read(this_dir / "wv09.json", source)
 
 
 def witherden_vincent_10():
-    return T2Scheme("Witherden-Vincent 10", *_read("wv10.json"))
+    return _read(this_dir / "wv10.json", source)
 
 
 def witherden_vincent_11():
-    return T2Scheme("Witherden-Vincent 11", *_read("wv11.json"))
+    return _read(this_dir / "wv11.json", source)
 
 
 def witherden_vincent_12():
-    return T2Scheme("Witherden-Vincent 12", *_read("wv12.json"))
+    return _read(this_dir / "wv12.json", source)
 
 
 def witherden_vincent_13():
-    return T2Scheme("Witherden-Vincent 13", *_read("wv13.json"))
+    return _read(this_dir / "wv13.json", source)
 
 
 def witherden_vincent_14():
-    return T2Scheme("Witherden-Vincent 14", *_read("wv14.json"))
+    return _read(this_dir / "wv14.json", source)
 
 
 def witherden_vincent_15():
-    return T2Scheme("Witherden-Vincent 15", *_read("wv15.json"))
+    return _read(this_dir / "wv15.json", source)
 
 
 def witherden_vincent_16():
-    return T2Scheme("Witherden-Vincent 16", *_read("wv16.json"))
+    return _read(this_dir / "wv16.json", source)
 
 
 def witherden_vincent_17():
-    return T2Scheme("Witherden-Vincent 17", *_read("wv17.json"))
+    return _read(this_dir / "wv17.json", source)
 
 
 def witherden_vincent_18():
-    return T2Scheme("Witherden-Vincent 18", *_read("wv18.json"))
+    return _read(this_dir / "wv18.json", source)
 
 
 def witherden_vincent_19():
-    return T2Scheme("Witherden-Vincent 19", *_read("wv19.json"))
+    return _read(this_dir / "wv19.json", source)
 
 
 def witherden_vincent_20():
-    return T2Scheme("Witherden-Vincent 20", *_read("wv20.json"))
+    return _read(this_dir / "wv20.json", source)
