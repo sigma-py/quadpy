010e4965bf09c7a53f7e548de970ed761210c158
Nico Schlömer <nico.schloemer@gmail.com>
2020-08-18 19:48:03 +0200

    add get_good_scheme to t3

--- test/test_t3.py
+++ test/test_t3.py
@@ -86,11 +86,78 @@ def test_show(scheme):
     scheme.show(tet, render=False)
 
 
-if __name__ == "__main__":
-    # scheme_ = quadpy.t3.Stroud("T3 7-1")
-    # test_scheme(scheme_)
-    # # test_show(scheme_)
-    # quadpy.t3.show(scheme_, backend="vtk")
-    from helpers import find_equal
+@pytest.mark.skip()
+def test_get_good_scheme():
+    for degree in range(51):
+        best = None
+        for scheme in quadpy.t3.schemes.values():
+            scheme = scheme()  # initialize
 
-    find_equal(quadpy.t3.schemes)
+            # filter schemes for eligibility
+            if scheme.degree < degree:
+                continue
+
+            # allow only positive weights
+            if any(scheme.weights < 0):
+                continue
+
+            # disallow points outside of the domain
+            if numpy.any(scheme.points < 0):
+                continue
+
+            if scheme.test_tolerance > 1.0e-13:
+                continue
+
+            try:
+                keys = set(scheme.symmetry_data.keys())
+            except AttributeError:
+                continue
+
+            if len(keys - set(["s4", "s31", "s22", "s211", "s1111"])) > 0:
+                continue
+
+            # okay, now compare the scheme with `best`
+            if best is None:
+                best = scheme
+                continue
+
+            if len(scheme.weights) > len(best.weights):
+                continue
+            elif len(scheme.weights) < len(best.weights):
+                best = scheme
+                continue
+            else:  # len(scheme.weights) == len(best.weights):
+                abs_weights = numpy.abs(scheme.weights)
+                ratio = max(abs_weights) / min(abs_weights)
+                bratio = max(numpy.abs(best.weights)) / min(numpy.abs(best.weights))
+                if ratio < bratio:
+                    best = scheme
+                    continue
+                elif ratio > bratio:
+                    continue
+                else:  # ratio == bratio
+                    # # check if it's actually the same scheme
+                    # if numpy.all(numpy.abs(scheme.points - best.points) < 1.0e-12):
+                    #     print("DUP", best.name, scheme.name)
+                    #     # pick the older one
+
+                    # for all intents and purposes, the schemes are equal; take the
+                    # older one
+                    scheme_year = "0" if scheme.source is None else scheme.source.year
+                    best_year = "0" if best.source is None else best.source.year
+                    if scheme_year < best_year:
+                        best = scheme
+                        continue
+                    elif scheme_year > best_year:
+                        continue
+                    else:  # years are equal
+                        pass
+
+        print(degree, best.name)
+
+        # print(best)
+    return
+
+
+if __name__ == "__main__":
+    test_get_good_scheme()
